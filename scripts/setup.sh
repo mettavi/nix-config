4cfa1552b10148a2e7677a156893fcc31f61f1a5
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 00:14:54 +1000

    Add reminder about prerequisite of patch command

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,5 @@
 
 git clone git@github.com:junegunn/fzf-git.sh.git ~/
 
+# This has to be applied after brew has installed cask logi-options-plus
 patch  /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/app_permissions.json < ./app_permissions.json.diff
