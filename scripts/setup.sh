af077f86b8fc8a3f32dea4bf74d5b3c391a233b3
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 00:40:38 +1000

    Download theme for bat

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
+# Download the git script for fzf
 git clone git@github.com:junegunn/fzf-git.sh.git ~/
 
 # This has to be applied after brew has installed cask logi-options-plus
 patch  /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/app_permissions.json < ./app_permissions.json.diff
+
+# Download theme after brew has installed bat
+curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme --create-dirs --output $(bat --config-dir)/themes
