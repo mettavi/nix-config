d5c37c28f9962e0c3669d3f84f0a129b354139aa
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 22:51:27 +1000

    Remove unnecessary --force option

--- scripts/vscode/install_vscode_exts.sh
+++ scripts/vscode/install_vscode_exts.sh
@@ -1,4 +1,4 @@
-code --install-extension chrmarti.regex --force &
-code --install-extension dbaeumer.vscode-eslint --force &
-code --install-extension formulahendry.code-runner --force &
-code --install-extension ritwickdey.liveserver --force
+code --install-extension chrmarti.regex &
+code --install-extension dbaeumer.vscode-eslint &
+code --install-extension formulahendry.code-runner &
+code --install-extension ritwickdey.liveserver
--- scripts/vscode/save_vscode_exts.sh
+++ scripts/vscode/save_vscode_exts.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 code --list-extensions |
 xargs -L 1 echo code --install-extension |
-sed "s/$/ --force/" |
 sed "\$!s/$/ \&/" > $HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh
 chmod +x $HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh
