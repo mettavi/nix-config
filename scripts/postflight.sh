50880b31907843ccab3cbc98fa9a4a8464b9fb4b
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 21:47:41 +1000

    Add script to generate list of installed vscode extensions

--- scripts/postflight.sh
+++ scripts/postflight.sh
@@ -8,3 +8,9 @@ patch  /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/app_perm
 
 # Download theme after brew has installed bat
 curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme --create-dirs --output $(bat --config-dir)/themes
+
+# Copy after vscode is installed
+./vscode/save_vscode_exts.sh
+./vscode/install_vscode_exts.sh # ensure these two lines are first to prevent git hook overwriting them!
+chmod +x ./dotfiles_precommit_hook
+cp ./dotfiles_precommit_hook ../.git/hooks/pre-commit
