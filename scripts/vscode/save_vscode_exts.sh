203e916bd4b52cf0a0e209631a2d566d89ea72a1
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 22:36:05 +1000

    Use full path to output directory

--- scripts/vscode/save_vscode_exts.sh
+++ scripts/vscode/save_vscode_exts.sh
@@ -3,4 +3,4 @@ code --list-extensions |
 xargs -L 1 echo code --install-extension |
 sed "s/$/ --force/" |
 sed "\$!s/$/ \&/" > $HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh
-chmod +x ./install_vscode_exts.sh
+chmod +x $HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh
