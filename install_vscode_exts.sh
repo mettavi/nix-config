1c83649e8dbc0933edee88733bf161f3ca26f152
Mettavihari <timotheos.allen@gmail.com>
2024-05-04 22:02:55 +1000

    Remove .vscode from dotfiles

--- install_vscode_exts.sh
+++
@@ -1,4 +0,0 @@
-code --install-extension chrmarti.regex --force &
-code --install-extension dbaeumer.vscode-eslint --force &
-code --install-extension formulahendry.code-runner --force &
-code --install-extension ritwickdey.liveserver --force
