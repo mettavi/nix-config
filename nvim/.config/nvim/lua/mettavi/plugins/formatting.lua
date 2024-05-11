1cc3f2569cd02e35713fbc46c3c09c16af16058e
Mettavihari <timotheos.allen@gmail.com>
2024-05-10 23:53:23 -0700

    Add shfmt for formatting bash scripts

--- nvim/.config/nvim/lua/mettavi/plugins/formatting.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/formatting.lua
@@ -19,6 +19,7 @@ return {
         markdown = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
+        sh = { "shfmt" },
       },
       format_on_save = {
         lsp_fallback = true,
