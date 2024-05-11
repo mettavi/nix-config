18cbd20dfa02f45f52da1439f6d90fed088b9e18
Mettavihari <timotheos.allen@gmail.com>
2024-05-11 00:39:20 -0700

    Add comment for bash-language-server

--- nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
@@ -38,7 +38,7 @@ return {
         "lua_ls",
         "emmet_ls",
         "pyright",
-        "bashls",
+        "bashls", -- uses shellcheck linter installed with brew
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
