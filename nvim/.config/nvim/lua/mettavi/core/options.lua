baec04957ace57f11d08b9f5f9dc9d22b43af370
Mettavihari <timotheos.allen@gmail.com>
2024-05-10 21:04:29 -0700

    Comment out autocommand to save when leaving buffer

--- nvim/.config/nvim/lua/mettavi/core/options.lua
+++ nvim/.config/nvim/lua/mettavi/core/options.lua
@@ -71,11 +71,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Autosave buffer when leaving or losing focus
-vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
-  callback = function()
-    if not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
-      vim.api.nvim_command("silent update")
-    end
-  end,
-})
+-- -- Autosave buffer when leaving or losing focus
+-- vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
+--   callback = function()
+--     if not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+--       vim.api.nvim_command("silent update")
+--     end
+--   end,
+-- })
