246afe8ee178af19e4d012e13f66970e387b2f65
Mettavihari <timotheos.allen@gmail.com>
2024-05-11 00:36:52 -0700

    Format on save by shfmt

--- scripts/postflight.sh
+++ scripts/postflight.sh
@@ -7,7 +7,7 @@ cd $scriptDir
 git clone git@github.com:junegunn/fzf-git.sh.git ~/
 
 # This has to be applied after brew has installed cask logi-options-plus
-patch  /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/app_permissions.json < ./app_permissions.json.diff
+patch /Library/Application\ Support/Logitech.localized/LogiOptionsPlus/app_permissions.json <./app_permissions.json.diff
 
 # Download theme after brew has installed bat
 curl -O https://raw.githubusercontent.com/folke/tokyonight.nvim/main/extras/sublime/tokyonight_night.tmTheme --create-dirs --output $(bat --config-dir)/themes
@@ -17,7 +17,7 @@ ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /us
 
 # Copy after vscode is installed
 ./vscode/save_vscode_exts.sh
-./vscode/install_vscode_exts.sh # must execute this before enabling git hooks in next command 
+./vscode/install_vscode_exts.sh # must execute this before enabling git hooks in next command
 
 # Point git to config file within repo
 git config --local include.path ../.gitconfig
