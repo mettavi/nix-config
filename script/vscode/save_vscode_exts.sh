9c49fb96bf89349b34dca978cc27d3590e31fdae
Mettavihari <timotheos.allen@gmail.com>
2024-05-15 16:09:41 -0700

    Replace tab character with spaces

--- script/vscode/save_vscode_exts.sh
+++ script/vscode/save_vscode_exts.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 code --list-extensions |
-	xargs -L 1 echo code --install-extension |
-	sed "\$!s/$/ \&/" >"$HOME/.dotfiles/script/vscode/install_vscode_exts.sh"
+  xargs -L 1 echo code --install-extension |
+  sed "\$!s/$/ \&/" >"$HOME/.dotfiles/script/vscode/install_vscode_exts.sh"
 chmod +x "$HOME/.dotfiles/script/vscode/install_vscode_exts.sh"
