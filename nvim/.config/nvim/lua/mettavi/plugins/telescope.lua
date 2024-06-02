3c3c1da3052f6df5f9dea3fa7de60b86e5218c04
Mettavihari <timotheos.allen@gmail.com>
2024-06-03 09:43:40 +1000

    Remove unnecessary require statement for trouble.nvim

--- nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
@@ -12,7 +12,6 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
-    local trouble = require("trouble")
     local open_with_trouble = require("trouble.sources.telescope").open
     -- Use this to add more results without clearing the trouble list
     local add_to_trouble = require("trouble.sources.telescope").add
