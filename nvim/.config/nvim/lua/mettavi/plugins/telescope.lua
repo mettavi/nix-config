13934e37a0fc4c718208a24b55a919bfae62985e
Mettavihari <timotheos.allen@gmail.com>
2024-06-03 09:38:19 +1000

    Update code for C-q mapping for sending selected items to qflist

--- nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
@@ -17,13 +17,6 @@ return {
     -- Use this to add more results without clearing the trouble list
     local add_to_trouble = require("trouble.sources.telescope").add
 
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
     telescope.setup({
       defaults = {
         path_display = { "smart" },
@@ -31,7 +24,7 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
             ["<C-t>"] = open_with_trouble,
           },
           n = { ["<c-t>"] = open_with_trouble },
