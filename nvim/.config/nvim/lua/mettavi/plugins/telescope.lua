a904f6b421528cda9f610434dbe8b9de666a7127
Mettavihari <timotheos.allen@gmail.com>
2024-06-02 13:31:04 +1000

    Update deprecated code to open search results in trouble.nvim

--- nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/telescope.lua
@@ -13,7 +13,9 @@ return {
     local transform_mod = require("telescope.actions.mt").transform_mod
 
     local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
+    local open_with_trouble = require("trouble.sources.telescope").open
+    -- Use this to add more results without clearing the trouble list
+    local add_to_trouble = require("trouble.sources.telescope").add
 
     -- or create your custom action
     local custom_actions = transform_mod({
@@ -30,7 +32,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-t>"] = open_with_trouble,
           },
         },
       },
