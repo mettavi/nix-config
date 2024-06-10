ba4bdaca7ce677a01e3de1c96732071853c473e8
Mettavihari <timotheos.allen@gmail.com>
2024-06-10 16:34:00 +0800

    Disable intrusive diagnostic message from lua_ls

--- nvim/.config/nvim/lua/mettavi/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/lsp/lspconfig.lua
@@ -130,6 +130,7 @@ return {
               -- make the language server recognize "vim" global
               diagnostics = {
                 globals = { "vim" },
+                disable = { "missing-fields" },
               },
               completion = {
                 callSnippet = "Replace",
