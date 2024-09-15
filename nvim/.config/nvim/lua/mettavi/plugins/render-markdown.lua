41a4519426de088507ba285e93fc423ead908c49
Mettavihari <timotheos.allen@gmail.com>
2024-09-15 23:29:57 +1000

    Add comment to describe render-markdown plugin

--- nvim/.config/nvim/lua/mettavi/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/render-markdown.lua
@@ -1,5 +1,6 @@
+-- Plugin to improve viewing Markdown files in Neovim
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {},
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 }
