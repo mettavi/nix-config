7933758b4e72f07d953864c5e6846f49abc8dce7
Mettavihari <timotheos.allen@gmail.com>
2024-10-10 23:04:42 +1100

    Add initial boilerplate nix config for home-manager

---
+++ nix-darwin/home.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+in
+{
+  programs.home-manager.enable = true;
+
+  home.username = "timotheos";
+  home.homeDirectory = "/Users/timotheos";
+  xdg.enable = true;
+
+  # xdg.configFile.nvim.source = mkOutOfStoreSymlink "/Users/timotheos/.dotfiles/.config/nvim";
+
+  home.stateVersion = "23.11";
+
+  programs = {
+    # tmux = import ../home/tmux.nix { inherit pkgs; };
+    # zsh = import ../home/zsh.nix { inherit config pkgs; };
+    # #zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
+    # fzf = import ../home/fzf.nix { inherit pkgs; };
+  };
+}
