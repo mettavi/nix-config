0be80ac5e0552614f4c175311220f22ff8eb5f73
Mettavihari <timotheos.allen@gmail.com>
2024-10-10 22:31:08 +1100

    Add stable channel for nix-darwin packages and rename old package to nixpkgs-unstable

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,7 +2,8 @@
   description = "Metta's Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
