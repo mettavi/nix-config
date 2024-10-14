be252e4e2dde71fd5788a9103e5bee0c22bcba77
Mettavihari <timotheos.allen@gmail.com>
2024-10-14 20:42:02 +1100

    Fix input for nix-darwin to follow nixpkgs-unstable for consistency with other inputs.

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,7 +5,7 @@
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
