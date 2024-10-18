4aaf2bfd3e9e41758b815c0d23c8c985eaef3d59
Mettavihari <timotheos.allen@gmail.com>
2024-10-18 20:24:05 +1100

    Comment out lines to enable incremental testing

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -16,44 +16,45 @@ in
   # home.packages = with pkgs; [ ];
 
   programs = {
-    aria2.enable = true;
-    atuin.enable = true;
-    bat.enable = true;
-    fd.enable = true;
-    fzf.enable = true;
-    git.enable = true;
-    git.delta.enable = true;
-    jq.enable = true;
-    keychain.enable = true;
-    lazygit.enable = true;
-    neovim.enable = true;
-    pyenv.enable = true;
-    qt.enable = true;
-    rbenv.enable = true;
-    thefuck.enable = true;
-    tmux.enable = true;
-    tmux.tmuxp.enable = true;
-    vscode = {
-      enable = true;
-      # disallow extensions being installed or updated manually by vscode
-      mutableExtensionsDir = false;
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
-      extensions = with pkgs.vscode-marketplace; [
-        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
-        dbaeumer.vscode-eslint
-        formulahendry.code-runner
-        ms-vscode-remote.remote-containers
-        ritwickdey.liveserver
-      ];
-        # ++ (with pkgs.open-vsx; [
-        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/open-vsx-latest.json
-        # ]);
-    yt-dlp.enable = true;
+    # aria2.enable = true;
+    # atuin.enable = true;
+    # bat.enable = true;
+    # fd.enable = true;
+    # fzf.enable = true;
+    # git.enable = true;
+    # git.delta.enable = true;
+    # jq.enable = true;
+    # keychain.enable = true;
+    # lazygit.enable = true;
+    # neovim.enable = true;
+    # pyenv.enable = true;
+    # qt.enable = true;
+    # rbenv.enable = true;
+    # thefuck.enable = true;
+    # tmux.enable = true;
+    # tmux.tmuxp.enable = true;
+    # vscode = {
+    #   enable = true;
+    #   # disallow extensions being installed or updated manually by vscode
+    #   mutableExtensionsDir = false;
+    #   enableUpdateCheck = false;
+    #   enableExtensionUpdateCheck = false;
+    #   extensions = with pkgs.vscode-marketplace; [
+    #     # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
+    #     dbaeumer.vscode-eslint
+    #     formulahendry.code-runner
+    #     ms-vscode-remote.remote-containers
+    #     ritwickdey.liveserver
+    #   ];
+    #     # ++ (with pkgs.open-vsx; [
+    #     # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/open-vsx-latest.json
+    #     # ]);
+    # };
+    # yt-dlp.enable = true;
     zsh.antidote.enable = true;
-    zsh.zsh-abbr.enable = true;
-    zoxide.enable = true;
-    zsh-syntax-highlighting = true;
+    # zsh.zsh-abbr.enable = true;
+    # zoxide.enable = true;
+    # zsh-syntax-highlighting = true;
     # tmux = import ../home/tmux.nix { inherit pkgs; };
     # zsh = import ../home/zsh.nix { inherit config pkgs; };
     # #zoxide = (import ../home/zoxide.nix { inherit config pkgs; });
