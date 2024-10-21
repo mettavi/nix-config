02dd054ee41e6251576c6aa56aebf87b94861001
Mettavihari <timotheos.allen@gmail.com>
2024-10-21 21:10:25 +1100

    Refactor nix-homebrew module
    Move nix-homebrew module from flake.nix to separate file homebrew.nix
    (Unrelated: update flake.lock database)

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -51,21 +51,6 @@
         modules = [
           ./configuration.nix
           nix-homebrew.darwinModules.nix-homebrew
-          {
-            nix-homebrew = {
-              # Install Homebrew under the default prefix
-              enable = true;
-
-              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-              # enableRosetta = true;
-
-              # User owning the Homebrew prefix
-              user = "timotheos";
-
-              # Automatically migrate existing Homebrew installations
-              autoMigrate = true;
-            };
-          }
           home-manager.darwinModules.home-manager
           {
             # `home-manager` config
