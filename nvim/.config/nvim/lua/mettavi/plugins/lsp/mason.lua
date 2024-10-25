99516ab9a490036a2aa22556f99969055d7c5d56
Mettavihari <timotheos.allen@gmail.com>
2024-10-25 21:41:42 +1100

    Enable inlay hints for svelte lsp

--- nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
@@ -107,6 +107,18 @@ return {
               end,
             })
           end,
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "all" },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+            },
+          },
         })
       end,
       ["bashls"] = function()
