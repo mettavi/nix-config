ace3053c8b469e5e37e7da3066a952c159c1f5ca
Mettavihari <timotheos.allen@gmail.com>
2024-11-14 17:01:08 +1100

    Two changes
    Set iterm2 to write user preferences to custom file
    Update the flake database

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -102,6 +102,13 @@ in
   system.defaults = {
     dock.autohide = true;
     NSGlobalDomain.KeyRepeat = 2;
+    CustomUserPreferences = {
+      # set iterm2 to write user prefs to custom file
+      "com.googlecode.iterm2" = {
+        "com.googlecode.iterm2.PrefsCustomFolder" = "$DOTFILES/iterm2";
+        "com.googlecode.iterm2.LoadPrefsFromCustomFolder" = true;
+      };
+    };
   };
 
   # List of directories to be symlinked in /run/current-system/sw
