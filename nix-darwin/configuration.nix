959ffa8bc95b52a59ee7949cda3bfe8cf89ebbac
Mettavihari <timotheos.allen@gmail.com>
2024-11-14 12:52:47 +1100

    Add config to source powerlevel10k script

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -329,7 +329,7 @@ in
     zsh = {
       enable = true;
       # autosuggestions.enable = true;
-      # promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     };
   };
 }
