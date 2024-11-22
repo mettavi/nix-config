94e4dfab97c82b2cb1d2d10093929d40b7ef22b6
Mettavihari <timotheos.allen@gmail.com>
2024-11-23 00:33:27 +1100

    Update flake for multiple subpackages (no default defined)

--- npm_globals/flake.nix
+++ npm_globals/flake.nix
@@ -12,39 +12,33 @@
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs = {
-    self,
-    dream2nix,
-    nixpkgs,
-  }: let
-    # A helper that helps us define the attributes below for
-    # all systems we care about.
-    eachSystem = nixpkgs.lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-  in {
-    packages = eachSystem (system: {
-      # For each system, we define our default package
-      # by passing in our desired nixpkgs revision plus
-      # any dream2nix modules needed by it.
-      default = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
-        modules = [
-          # Import our actual package definiton as a dream2nix module from ./default.nix
-          ./default.nix
-          {
-            # Aid dream2nix to find the project root. This setup should also works for mono
-            # repos. If you only have a single project, the defaults should be good enough.
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./packages;
-          }
-        ];
-      };
-    });
-  };
+  outputs =
+    {
+      self,
+      dream2nix,
+      nixpkgs,
+    }:
+    let
+      # A helper that helps us define the attributes below for
+      # all systems we care about.
+      eachSystem = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      packages = eachSystem (system: 
+            dream2nix.lib.importPackages
+            {
+              # All packages defined in ./packages/<name> are automatically added to the flake outputs
+              # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
+              projectRoot = ./.;
+              # can be changed to ".git" or "flake.nix" to get rid of .project-root
+              projectRootFile = "flake.nix";
+              packagesDir = ./packages;
+              packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+            });
+    };
 }
