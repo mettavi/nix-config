701faf4887370e4782d12d8d8dcb3600987f93ef
Mettavihari <timotheos.allen@gmail.com>
2024-11-30 13:21:22 +1100

    Add first draft of userActivationScript for karabiner-driverkit package

---
+++ myPkgs/karabiner-driverkit/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Finish installing Karabiner Driver Kit from nix-store
+
+NIXPATH=${karabiner-driver}
+NIXLIB="$NIXPATH/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice" 
+LIBPATH="/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice"
+
+if [[ ! -e /Applications/.Karabiner-VirtualHIDDevice-Manager.app ]]; then
+  cp -r "$NIXPATH/Apps/.Karabiner-VirtualHIDDevice-Manager.app" /Applications
+fi
+
+if [[ ! -e "$LIBPATH/Applications/Karabiner-VirtualHIDDevice-Daemon.app" ]]; then
+  mkdir -p "$LIBPATH/Applications" 
+  cp -r "$NIXLIB/Applications/Karabiner-VirtualHIDDevice-Daemon.app" "$LIBPATH/Applications/"
+fi
+
+if [[ ! -d "$LIBPATH/scripts" ]]; then
+  mkdir -p "$LIBPATH/scripts/uninstall"
+  cp -r "$NIXLIB/scripts" "$LIBPATH"
+fi
