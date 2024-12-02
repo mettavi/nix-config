3a788e82ed589a36b7025f223991c3f1ecc4f9d8
Mettavihari <timotheos.allen@gmail.com>
2024-12-02 22:06:52 +1100

    Remove code that is no longer used

--- myPkgs/karabiner-driverkit/default.nix
+++ myPkgs/karabiner-driverkit/default.nix
@@ -2,8 +2,6 @@
   lib,
   stdenv,
   fetchurl,
-  # xar,
-  # cpio,
 }:
 
 let
@@ -34,16 +32,6 @@ stdenv.mkDerivation {
     hash = hashes.darwin;
   };
 
-  # nativeBuildInputs = [
-  #   xar
-  #   cpio
-  # ];
-
-  # unpackPhase = ''
-  #   xar -xf $src
-  #   zcat < Payload | cpio -i
-  # '';
-
   # unpack does not create a folder, so start from the current directory
   sourceRoot = ".";
   dontUnpack = true;
@@ -53,19 +41,8 @@ stdenv.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-    # mkdir -p $out/Library/Application\ Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/{Applications,scripts/uninstall}
     mkdir $out
-    # cp -R "./Applications/.Karabiner-VirtualHIDDevice-Manager.app" "$out/Apps"
-    # mkdir -p "$out/${libRoot}/Applications"
-    # mkdir -p "$out/${libRoot}/scripts/uninstall"
-    # cp -R "./${libRoot}/Applications/Karabiner-VirtualHIDDevice-Daemon.app" "$out/${libRoot}/Applications"
-    # cp -R "./${libRoot}/scripts" "$out/${libRoot}"
     cp $src $out/Karabiner-DriverKit-VirtualHIDDevice-${versions.darwin}.pkg
     runHook postInstall
   '';
-
-  # postInstall = ''
-    # "/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager activate"
-    # sudo '/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/Applications/Karabiner-VirtualHIDDevice-Daemon.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Daemon'
-  # '';
 }
