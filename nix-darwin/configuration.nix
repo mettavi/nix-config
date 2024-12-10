a5f22a80a12fe4336650c3f8e8475cf0f484b192
Mettavihari <timotheos.allen@gmail.com>
2024-12-10 19:31:04 +1100

    Move comment next to code it explains

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -52,9 +52,9 @@ in
   nixpkgs.config.allowUnfree = true;
   # nixpkgs.config.allowBroken = true;
 
-  # install global npm packages that are not available in nixpkgs repo
   nixpkgs.overlays = [
     (final: prev: {
+      # install global npm packages that are not available in nixpkgs repo
       npmGlobals = final.callPackage ../npm_globals/node-packages-v18.nix {
         nodeEnv = final.callPackage ../npm_globals/node-env.nix {
           libtool = if final.stdenv.isDarwin then final.darwin.cctools else null;
