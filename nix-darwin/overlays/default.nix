44a72106cc5002e62f78d96bfc67e305cc6b5f07
Mettavihari <timotheos.allen@gmail.com>
2024-12-15 22:47:52 +1100

    Remove overlay which has been returned to top level module configuration.nix

--- nix-darwin/overlays/default.nix
+++ nix-darwin/overlays/default.nix
@@ -6,14 +6,6 @@
 }:
 {
   nixpkgs.overlays = [
-    (final: prev: {
-      # install global npm packages that are not available in nixpkgs repo
-      npmGlobals = final.callPackage ../npm_globals/node-packages-v18.nix {
-        nodeEnv = final.callPackage ../npm_globals/node-env.nix {
-          libtool = if final.stdenv.isDarwin then final.darwin.cctools else null;
-        };
-      };
-    })
     (import ./overlay-qtwebengine.nix)
   ];
 }
