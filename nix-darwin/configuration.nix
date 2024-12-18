fb4e0d403c5677e8a68281cfc5db2479345a2b6e
Mettavihari <timotheos.allen@gmail.com>
2024-12-18 22:34:32 +1100

    Add extra binary caches from cachix.org

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -40,9 +40,13 @@ in
       ];
       substituters = [
         "https://nix-community.cachix.org"
+        "https://cachix.cachix.org"
+        "https://nixpkgs.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
       ];
       trusted-users = [ "root" ];
     };
