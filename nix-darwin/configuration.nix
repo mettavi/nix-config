f24eebbbfb5cd79dc75125254af2757333fc0bab
Mettavihari <timotheos.allen@gmail.com>
2024-12-21 21:36:12 +1100

    Move nixpkgs.cachix.org to top of the list of substituters

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -52,13 +52,13 @@ in
       ];
       # substituters are always enabled, trusted-substituters can be enabled on demand by untrusted users
       substituters = [
-        "https://nix-community.cachix.org"
         "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
         "https://cachix.cachix.org"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       ];
       trusted-users = [ "root" ];
