75f456e31041c1269d25999bd387486244b0709d
Mettavihari <timotheos.allen@gmail.com>
2024-12-23 01:38:47 +1100

    Add yazi cachix binary cache to substituters list

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -55,11 +55,13 @@ in
         "https://nixpkgs.cachix.org"
         "https://nix-community.cachix.org"
         "https://cachix.cachix.org"
+        "https://yazi.cachix.org"
       ];
       trusted-public-keys = [
         "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       ];
       trusted-users = [ "root" ];
     };
