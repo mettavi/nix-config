959b2e78a1adb4e12ff47685c0092ee6c99beb29
Mettavihari <timotheos.allen@gmail.com>
2024-12-25 22:24:17 +1100

    Lint yaml files with yamllint

--- nvim/.config/nvim/lua/mettavi/plugins/linting.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/linting.lua
@@ -13,6 +13,7 @@ return {
       svelte = { "eslint_d" },
       python = { "pylint" },
       nix = { "nix" },
+      yaml = { "yamllint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
