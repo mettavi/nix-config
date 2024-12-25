ded5eeece326493d8a63710d33f435d8c513d238
Mettavihari <timotheos.allen@gmail.com>
2024-12-25 21:05:57 +1100

    Comment out opening/closing lines of config block to prevent lsp errors

--- nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/mettavi/plugins/lsp/mason.lua
@@ -65,7 +65,7 @@ return {
       },
     })
 
-    mason_lspconfig.setup({ -- use mason_tool_installer instead to also autoupdate
+    -- mason_lspconfig.setup({ -- use mason_tool_installer instead to also autoupdate
       -- list of servers for mason to install
       -- ensure_installed = {
       --   "ts_ls",
@@ -80,7 +80,7 @@ return {
       -- },
       -- auto-install configured servers (with lspconfig)
       -- automatic_installation = true, -- not the same as ensure_installed
-    })
+    -- })
 
     mason_tool_installer.setup({
       ensure_installed = {
