8b57717f5f714c0f63f0bf499bc8406841792c5f
Mettavihari <timotheos.allen@gmail.com>
2024-12-27 01:58:17 +1100

    Change sops-nix home-manager config to use the same secrets file as the system module

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -24,7 +24,7 @@ in
   ];
 
   sops = {
-    defaultSopsFile = ./secrets_home.yaml; # must have no password!
+    defaultSopsFile = ./secrets.yaml; # must have no password!
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
     # It's also possible to use a ssh key, but only when it has no password:
     #age.sshKeyPaths = [ "/home/user/path-to-ssh-key" ];
