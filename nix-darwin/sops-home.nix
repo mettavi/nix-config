5aaf5992aaf73d5f1cec34a58d2d1afc01a670b6
Mettavihari <timotheos.allen@gmail.com>
2024-12-27 13:00:06 +1100

    Add nix file to setup and configure the sops-nix home-manager module

---
+++ nix-darwin/sops-home.nix
@@ -0,0 +1,29 @@
+{
+  inputs,
+  config,
+  ...
+}:
+{
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml; # must have no password!
+    age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+    # It's also possible to use a ssh key, but only when it has no password:
+    #age.sshKeyPaths = [ "/home/user/path-to-ssh-key" ];
+
+    secrets = {
+      "private_keys/timotheos" = {
+        path = "/Users/timotheos/.ssh/id_ed25519";
+      };
+    };
+    # secrets.test = {
+    #   # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
+    #
+    #   # %r gets replaced with a runtime directory, use %% to specify a '%'
+    #   # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
+    #   # DARWIN_USER_TEMP_DIR) on darwin.
+    #   path = "%r/test.txt";
+    # };
+  };
+}
