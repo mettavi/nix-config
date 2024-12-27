b46a4565b07a35487d30623490f79739bf481b5e
Mettavihari <timotheos.allen@gmail.com>
2024-12-27 18:33:34 +1100

    Add comments from EmergentMind/nix-config github repo

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -86,12 +86,18 @@ in
       # This will generate a new key if the key specified above does not exist
       generateKey = true;
     };
-    gnupg.sshKeyPaths = [];
+    gnupg.sshKeyPaths = [ ];
     # secrets will be output to /run/secrets
     # e.g. /run/secrets/msmtp-password
     # secrets required for user creation are handled in respective ./users/<username>.nix files
     # because they will be output to /run/secrets-for-users and only when the user is assigned to a host.
     secrets = {
+      # For home-manager a separate age key is used to decrypt secrets and must be placed onto the host. This is because
+      # the user doesn't have read permission for the ssh service private key. However, we can bootstrap the age key from
+      # the secrets decrypted by the host key, which allows home-manager secrets to work without manually copying over
+      # the age key.
+      # These age keys are unique for the user on each host and are generated on their own (i.e. they are not derived
+      # from an ssh key).
       github_token = {
         owner = "${user}";
       };
