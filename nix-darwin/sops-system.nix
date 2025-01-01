aa307ee91fa7f61a7dc1ee80fedbebe7da00dc84
Mettavihari <timotheos.allen@gmail.com>
2025-01-01 17:28:08 +1100

    Add workaround to bypass bug

--- nix-darwin/sops-system.nix
+++ nix-darwin/sops-system.nix
@@ -18,7 +18,10 @@
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       # This will generate a new key if the key specified above does not exist
-      generateKey = true;
+      # BUG: setting this to true will break mounting of secrets on startup
+      # see https://github.com/Mic92/sops-nix/issues/700 for details
+      # workaround: run once and then set to false
+      generateKey = false;
     };
     gnupg.sshKeyPaths = [ ];
     # secrets will be output to /run/secrets
