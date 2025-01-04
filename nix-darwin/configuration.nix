83f529273f4e1c2588eb1581f46dc9cb0e8aedd7
Mettavihari <timotheos.allen@gmail.com>
2025-01-04 19:59:46 +1100

    Update path to new location of npm_globals directory

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -86,8 +86,8 @@ in
   nixpkgs.overlays = [
     (final: prev: {
       # install global npm packages that are not available in nixpkgs repo
-      npmGlobals = final.callPackage ../npm_globals/node-packages-v18.nix {
-        nodeEnv = final.callPackage ../npm_globals/node-env.nix {
+      npmGlobals = final.callPackage ../modules/npm_globals/node-packages-v18.nix {
+        nodeEnv = final.callPackage ../modules/npm_globals/node-env.nix {
           libtool = if final.stdenv.isDarwin then final.darwin.cctools else null;
         };
       };
