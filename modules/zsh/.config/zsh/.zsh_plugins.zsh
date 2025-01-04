55461fd089318cdf08ca25192a338de1ca80e23f
Mettavihari <timotheos.allen@gmail.com>
2025-01-04 22:03:37 +1100

    Nix-darwin refactor: move zsh module to modules directory

---
+++ modules/zsh/.config/zsh/.zsh_plugins.zsh
@@ -0,0 +1,5 @@
+fpath+=( /Users/timotheos/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /Users/timotheos/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
+fpath+=( /Users/timotheos/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use )
+source /Users/timotheos/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/you-should-use.plugin.zsh
+source /Users/timotheos/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-you-should-use/zsh-you-should-use.plugin.zsh
