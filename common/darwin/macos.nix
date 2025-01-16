4412c8a93fc4ff42d9968b380e51be44c3a99a86
Mettavihari <timotheos.allen@gmail.com>
2025-01-16 14:26:36 +1100

    Add and update macOS key repeat settings

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -1,9 +1,13 @@
 {
   system.defaults = {
     dock.autohide = true;
+    # check current setting with "defaults read NSGlobalDomain '<insert setting here>'"
     NSGlobalDomain = {
-      KeyRepeat = 2;
       ApplePressAndHoldEnabled = false;
+      # GUI steps (L to R): InitialKeyRepeat: 120, 94, 68, 35, 25, 15 / KeyRepeat: 120, 90, 60, 30, 12, 6, 2
+      # (See https://apple.stackexchange.com/questions/261163/default-value-for-nsglobaldomain-initialkeyrepeat)
+      InitialKeyRepeat = 35; # default 25 
+      KeyRepeat = 6; # default 6
     };
   };
 }
