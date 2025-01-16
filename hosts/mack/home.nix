111573b6ad1ae2efab75d3dc7e78de88f63185af
Mettavihari <timotheos.allen@gmail.com>
2025-01-17 09:23:36 +1100

    Move vscode config for user timotheos from host config to separate user module

--- hosts/mack/home.nix
+++ hosts/mack/home.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   config,
   pkgs,
   ...
@@ -23,6 +22,7 @@ in
     # sops config  for home
     ../../modules/sops/sops-home.nix
     # packages
+    ../../common/users/timotheos/vscode.nix
     ../../modules/yazi.nix
   ];
 
@@ -97,57 +97,6 @@ in
       sensibleOnTop = false;
       terminal = "tmux-256color";
       tmuxp.enable = true;
-    };
-    vscode = {
-      enable = true;
-      # disallow extensions being installed or updated by vscode
-      mutableExtensionsDir = false;
-      enableUpdateCheck = true;
-      enableExtensionUpdateCheck = true;
-      extensions = with pkgs.vscode-marketplace; [
-        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
-        dbaeumer.vscode-eslint
-        formulahendry.code-runner
-        ms-vscode-remote.remote-containers
-        ritwickdey.liveserver
-      ];
-      # ++ (with pkgs.open-vsx; [
-      # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/open-vsx-latest.json
-      # ]);
-      keybindings = [
-        {
-          "key" = "ctrl+`";
-          "command" = "workbench.action.terminal.focus";
-          "when" = "editorTextFocus";
-        }
-        {
-          "key" = "ctrl+`";
-          "command" = "workbench.action.focusActiveEditorGroup";
-          "when" = "terminalFocus";
-        }
-        {
-          "key" = "ctrl+'";
-          "command" = "workbench.action.terminal.toggleTerminal";
-        }
-      ];
-      userSettings = {
-        "editor.insertSpaces" = true;
-        "editor.tabSize" = 2;
-        "editor.autoIndent" = "advanced";
-        "diffEditor.ignoreTrimWhitespace" = false;
-        "editor.detectIndentation" = false;
-        "extensions.autoUpdate" = false;
-        "window.zoomLevel" = 1;
-        "eslint.codeActionsOnSave.rules" = null;
-        "editor.fontLigatures" = false;
-        "terminal.integrated.fontFamily" = "MesloLGS Nerd Font Mono";
-        "editor.fontFamily" = "MesloLGS Nerd Font Mono; Monaco; 'Courier New'; monospace";
-        "update.mode" = "manual";
-        "telemetry.telemetryLevel" = "off";
-        "files.autoSave" = "afterDelay";
-        "terminal.integrated.macOptionIsMeta" = true;
-        "application.shellEnvironmentResolutionTimeout" = 30;
-      };
     };
     yt-dlp.enable = true;
     zoxide = {
