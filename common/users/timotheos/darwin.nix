81b964102ceb0657cc20917db38e037a1f7fbe61
Mettavihari <timotheos.allen@gmail.com>
2025-02-01 14:37:11 +1100

    Define persistent apps in the dock for user1/timotheos

---
+++ common/users/timotheos/darwin.nix
@@ -0,0 +1,14 @@
+{ user1, ... }:
+{
+  system.defaults.dock = {
+    persistent-apps = [
+      "/System/Applications/Calendar.app"
+      "/System/Applications/App Store.app"
+      "/Applications/Bitwarden.app"
+      "/Applications/Google Chrome.app"
+      "/Applications/iTerm.app"
+      "/Applications/Microsoft Word.app"
+      "/Users/${user1}/.dotfiles/common/users/${user1}/bin/CaliSync.app"
+    ];
+  };
+}
