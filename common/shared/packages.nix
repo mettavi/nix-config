fe8219b80999e185ff0d7fbfaa3dd36e8ae2a9b0
Mettavihari <timotheos.allen@gmail.com>
2025-02-02 12:05:11 +1100

    Move config for restic package to a separate file

--- common/shared/packages.nix
+++ common/shared/packages.nix
@@ -50,7 +50,6 @@ in
     # pnpm
     # poppler
     # python-launcher
-    restic # Backup with delta transfers to cloud storage via rclone
     rclone # sync files and directories to and from major cloud storage
     ruby
     rustc
---
+++ common/shared/restic.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  restic_1drive = pkgs.restic.overrideAttrs (oldAttrs: {
+    postInstall =
+      (oldAttrs.postInstall or "")
+      + ''
+        export RESTIC_REPOSITORY="rclone:onedrive:calibre";
+        # initialise calibre repo if necessary
+        if ! restic cat config > /dev/null 2>&1; then
+          restic -r rclone:ondedrive:calibre init;
+      '';
+  });
+
+  environment.systemPackages = with pkgs; [
+    restic # Backup with delta transfers (eg. to cloud storage via rclone)
+  ];
+}
