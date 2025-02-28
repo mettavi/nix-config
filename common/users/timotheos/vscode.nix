17e11f99087e7f0bcc5b70e7dafd46f75b808578
Mettavihari <timotheos.allen@gmail.com>
2025-02-28 16:45:04 +1100

    Remove dev containers extension and move user options to new config profiles key

--- common/users/timotheos/vscode.nix
+++ common/users/timotheos/vscode.nix
@@ -4,51 +4,52 @@
     enable = true;
     # disallow extensions being installed or updated by vscode
     mutableExtensionsDir = false;
-    enableUpdateCheck = true;
-    enableExtensionUpdateCheck = true;
-    extensions = with pkgs.vscode-marketplace; [
-      # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
-      dbaeumer.vscode-eslint
-      formulahendry.code-runner
-      ms-vscode-remote.remote-containers
-      ritwickdey.liveserver
-    ];
     # ++ (with pkgs.open-vsx; [
     # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/open-vsx-latest.json
     # ]);
-    keybindings = [
-      {
-        "key" = "ctrl+`";
-        "command" = "workbench.action.terminal.focus";
-        "when" = "editorTextFocus";
-      }
-      {
-        "key" = "ctrl+`";
-        "command" = "workbench.action.focusActiveEditorGroup";
-        "when" = "terminalFocus";
-      }
-      {
-        "key" = "ctrl+'";
-        "command" = "workbench.action.terminal.toggleTerminal";
-      }
-    ];
-    userSettings = {
-      "editor.insertSpaces" = true;
-      "editor.tabSize" = 2;
-      "editor.autoIndent" = "advanced";
-      "diffEditor.ignoreTrimWhitespace" = false;
-      "editor.detectIndentation" = false;
-      "extensions.autoUpdate" = false;
-      "window.zoomLevel" = 1;
-      "eslint.codeActionsOnSave.rules" = null;
-      "editor.fontLigatures" = false;
-      "terminal.integrated.fontFamily" = "MesloLGS Nerd Font Mono";
-      "editor.fontFamily" = "MesloLGS Nerd Font Mono; Monaco; 'Courier New'; monospace";
-      "update.mode" = "manual";
-      "telemetry.telemetryLevel" = "off";
-      "files.autoSave" = "afterDelay";
-      "terminal.integrated.macOptionIsMeta" = true;
-      "application.shellEnvironmentResolutionTimeout" = 30;
+    profiles.default = {
+      userSettings = {
+        "editor.insertSpaces" = true;
+        "editor.tabSize" = 2;
+        "editor.autoIndent" = "advanced";
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.detectIndentation" = false;
+        "extensions.autoUpdate" = false;
+        "window.zoomLevel" = 1;
+        "eslint.codeActionsOnSave.rules" = null;
+        "editor.fontLigatures" = false;
+        "terminal.integrated.fontFamily" = "MesloLGS Nerd Font Mono";
+        "editor.fontFamily" = "MesloLGS Nerd Font Mono; Monaco; 'Courier New'; monospace";
+        "update.mode" = "manual";
+        "telemetry.telemetryLevel" = "off";
+        "files.autoSave" = "afterDelay";
+        "terminal.integrated.macOptionIsMeta" = true;
+        "application.shellEnvironmentResolutionTimeout" = 30;
+      };
+      extensions = with pkgs.vscode-marketplace; [
+        # https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/master/data/cache/vscode-marketplace-latest.json
+        dbaeumer.vscode-eslint
+        formulahendry.code-runner
+        ritwickdey.liveserver
+      ];
+      keybindings = [
+        {
+          "key" = "ctrl+`";
+          "command" = "workbench.action.terminal.focus";
+          "when" = "editorTextFocus";
+        }
+        {
+          "key" = "ctrl+`";
+          "command" = "workbench.action.focusActiveEditorGroup";
+          "when" = "terminalFocus";
+        }
+        {
+          "key" = "ctrl+'";
+          "command" = "workbench.action.terminal.toggleTerminal";
+        }
+      ];
+      enableExtensionUpdateCheck = true;
+      enableUpdateCheck = true;
     };
   };
 }
