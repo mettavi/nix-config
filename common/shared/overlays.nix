be035c220debe75b2e6615e26d31af563e513a47
Mettavihari <timotheos.allen@gmail.com>
2025-03-17 13:54:49 +1100

    Add example for how to override ffmpeg with special options

--- common/shared/overlays.nix
+++ common/shared/overlays.nix
@@ -8,6 +8,11 @@
           libtool = if final.stdenv.isDarwin then final.darwin.cctools else null;
         };
       };
+      # Example for how to override the base version of ffmpeg
+      # install ffmpeg with the non-free libfdk_aac codec
+      # ffmpeg = prev.ffmpeg.override {
+      #   withFdkAac =true;
+      # };
     })
   ];
 }
