92af630110864685fef5972f11822d058ebb960e
Mettavihari <timotheos.allen@gmail.com>
2025-03-20 18:38:10 +1100

    Add comment about purpose of custom installation of libation package

--- common/darwin/pkgs/default.nix
+++ common/darwin/pkgs/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   karabiner-driverkit = ( pkgs.callPackage ./karabiner-driverkit { } );
+  # install more recent libation version than in nixpkgs
   libation-gh = ( pkgs.callPackage ./libation-gh { } );
   goldendictng-gh = ( pkgs.callPackage ./goldendictng-gh { } );
 }
