f909de8ac413776423a682c5d4138d3a3c158434
Mettavihari <timotheos.allen@gmail.com>
2025-03-22 21:35:54 +1100

    Revert to previous overlays code for local packages added to nixpkgs

--- common/darwin/pkgs/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  karabiner-driverkit = (pkgs.callPackage ./karabiner-driverkit { });
-  # install more recent libation version than in nixpkgs
-  libation-gh = (pkgs.callPackage ./libation-gh { });
-  goldendictng-gh = (pkgs.callPackage ./goldendictng-gh { });
-}
