6623671f5183a89ba97b9c79d22161f8da5a258b
Mettavihari <timotheos.allen@gmail.com>
2025-03-25 19:49:59 +1100

    Remove custom modules to install kanata which are no longer needed

--- mypkgs/default.nix
+++
@@ -1,7 +0,0 @@
-# Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
-{ pkgs, ... }:
-{
-  kanata = pkgs.callPackage ./kanata { };
-  karabiner-driverkit = pkgs.callPackage ./karabiner-driverkit { };
-}
