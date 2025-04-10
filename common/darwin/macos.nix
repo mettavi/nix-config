7bbafd10e17cd8c86eff96d478623a292d8f807a
Mettavihari <timotheos.allen@gmail.com>
2025-04-10 20:01:08 +1000

    Show setting for macos but comment out until bug is fixed

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -4,6 +4,9 @@
     dock.autohide = true;
     # check current setting with "defaults read NSGlobalDomain '<insert setting here>'"
     NSGlobalDomain = {
+      # This setting is currently ineffective as it should be value 2, see https://github.com/nix-darwin/nix-darwin/issues/1378
+      # Mode 3 (2?) enables full keyboard control
+      # AppleKeyboardUIMode = 3;
       ApplePressAndHoldEnabled = false;
       # GUI steps (L to R): InitialKeyRepeat: 120, 94, 68, 35, 25, 15 / KeyRepeat: 120, 90, 60, 30, 12, 6, 2
       # (See https://apple.stackexchange.com/questions/261163/default-value-for-nsglobaldomain-initialkeyrepeat)
