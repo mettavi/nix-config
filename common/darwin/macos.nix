baf0177a801d7ea71e5054aeb4fe208823a92df4
Mettavihari <timotheos.allen@gmail.com>
2025-04-11 19:26:25 +1000

    Disable option which is not currently working due to change in recent macOS versions

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -5,7 +5,8 @@
     # check current setting with "defaults read NSGlobalDomain '<insert setting here>'"
     NSGlobalDomain = {
       # This setting is currently ineffective as it should be value 2, see https://github.com/nix-darwin/nix-darwin/issues/1378
-      # Mode 3 (2?) enables full keyboard control
+      # WORKAROUND: set manually in activation.nix
+      # Mode 3 (should be 2?) enables keyboard navigation to focus between controls
       # AppleKeyboardUIMode = 3;
       ApplePressAndHoldEnabled = false;
       # GUI steps (L to R): InitialKeyRepeat: 120, 94, 68, 35, 25, 15 / KeyRepeat: 120, 90, 60, 30, 12, 6, 2
