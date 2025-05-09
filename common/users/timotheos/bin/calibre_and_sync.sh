583e484cba9e8e5a462a4ed421a9d7888f5dc31d
Mettavihari <timotheos.allen@gmail.com>
2025-05-09 21:38:48 +1000

    Add --fast-list flag to improve efficiency of rclone sync with onedrive

--- common/users/timotheos/bin/calibre_and_sync.sh
+++ common/users/timotheos/bin/calibre_and_sync.sh
@@ -7,6 +7,8 @@ calibre
 wait $! # optional really, but a bit safer
 
 # Setup and run sync
+# The --fast-list flag must be used with the --onedrive-delta flag (or delta = true in the config file)
+# as it can cause performance degradation
 rclone --log-level NOTICE --log-file="${XDG_CONFIG_HOME}/rclone/log.txt" \
-  --stats 2s --progress --check-first --retries 1 --max-backlog 999999 --buffer-size 256M \
+  --stats 2s --progress --check-first --retries 1 --max-backlog 999999 --buffer-size 256M --fast-list \
   sync ~/Documents/calibre/ --filter-from ~/.config/rclone/filter.txt onedrive:calibre/
