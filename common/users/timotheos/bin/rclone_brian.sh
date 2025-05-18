a730fe92d290ec09af0bcf366bd9634674a7b4c6
Mettavihari <timotheos.allen@gmail.com>
2025-05-18 23:16:15 +1000

    Change names of log files and fix syntax error with --verbose flag

--- common/users/timotheos/bin/rclone_brian.sh
+++ common/users/timotheos/bin/rclone_brian.sh
@@ -2,5 +2,5 @@
 #
 # This script will tar, compress, stream and copy a archive of Brian's home directory to AWS S3 Glacier Deep Archive using rclone
 tar -zcvf - /Volumes/Brian/Users/ibrianallen --exclude-from="$DOTFILES/common/users/timotheos/conf/rclone/exclude-brian.txt" |
-  rclone --dry-run --progress --stats 2s --verbose 2 --log-file="$HOME/Library/Logs/rclone-testing.log" --fast-list \
-  rcat --dump filters --transfers 100 --checkers 200 aws_gda-crypt:/brian_macos_10.11.6.tar.gz | tee ~/Library/Logs/rclone-stdout.log
+  rclone --dry-run --progress --stats 2s --verbose=2 --log-file="$HOME/Library/Logs/rclone-teststderr.log" --fast-list \
+  rcat --dump filters --transfers 100 --checkers 200 aws_gda-crypt:/brian_macos_10.11.6.tar.gz | tee ~/Library/Logs/rclone-teststdout.log
