0f4c0193c7e22a540b38c43c1414ca2b4cca37f0
Mettavihari <timotheos.allen@gmail.com>
2025-05-20 19:58:27 +1000

    Move sops.templates config to separate files, starting with the rclone template

--- common/users/timotheos/sops-home.nix
+++ common/users/timotheos/sops-home.nix
@@ -4,6 +4,10 @@
   ...
 }:
 {
+  imports = [
+    ./sops-templates
+  ];
+
   sops = {
     defaultSopsFile = ../../../modules/secrets/secrets.yaml; # must have no password!
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
@@ -39,44 +43,44 @@
       "users/${user1}/restic_b2_mack-timotheos" = {
       };
     };
-    templates = {
-      # rclone config file with secrets
-      # only "backend flags" can be added to the rclone.conf
-      "rclone.conf" = {
-        content = ''
-          [onedrive]
-          type = onedrive
-          token = ${config.sops.placeholder."users/${user1}/rclone_1d_token"}
-          drive_id = 0F811515C935D85C
-          drive_type = personal
-          hard_delete = true
-          # improves performance, except when used from the root (root/directory in onedrive:root/directory)
-          delta = true
-          
-          [b2]
-          type = b2
-          account = 004471da6ad00020000000001
-          key = ${config.sops.placeholder."users/${user1}/rclone_b2_appkey"}
-
-          [aws_gda]
-          type = s3
-          provider = AWS
-          access_key_id = AKIASR23L52NR2GRLUZ6
-          secret_access_key = ${config.sops.placeholder."users/${user1}/rclone_aws_gda_key"}
-          region = us-east-1
-          acl = private
-          storage_class = DEEP_ARCHIVE
-          
-          [aws_gda-crypt]
-          type = crypt
-          remote = aws_gda:mettavi-archive-useast1
-          # rclone obfuscated encryption password
-          password = ${config.sops.placeholder."users/${user1}/rclone_aws_gda_crypt"}
-          directory_name_encryption = false
-        '';
-        path = "${config.xdg.configHome}/rclone/rclone.conf";
-      };
-    };
+    # templates = {
+    #   # rclone config file with secrets
+    #   # only "backend flags" can be added to the rclone.conf
+    #   "rclone.conf" = {
+    #     content = ''
+    #       [onedrive]
+    #       type = onedrive
+    #       token = ${config.sops.placeholder."users/${user1}/rclone_1d_token"}
+    #       drive_id = 0F811515C935D85C
+    #       drive_type = personal
+    #       hard_delete = true
+    #       # improves performance, except when used from the root (root/directory in onedrive:root/directory)
+    #       delta = true
+    #       
+    #       [b2]
+    #       type = b2
+    #       account = 004471da6ad00020000000001
+    #       key = ${config.sops.placeholder."users/${user1}/rclone_b2_appkey"}
+    #
+    #       [aws_gda]
+    #       type = s3
+    #       provider = AWS
+    #       access_key_id = AKIASR23L52NR2GRLUZ6
+    #       secret_access_key = ${config.sops.placeholder."users/${user1}/rclone_aws_gda_key"}
+    #       region = us-east-1
+    #       acl = private
+    #       storage_class = DEEP_ARCHIVE
+    #       
+    #       [aws_gda-crypt]
+    #       type = crypt
+    #       remote = aws_gda:mettavi-archive-useast1
+    #       # rclone obfuscated encryption password
+    #       password = ${config.sops.placeholder."users/${user1}/rclone_aws_gda_crypt"}
+    #       directory_name_encryption = false
+    #     '';
+    #     path = "${config.xdg.configHome}/rclone/rclone.conf";
+    #   };
+    # };
     # secrets.test = {
     #   # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
     #
