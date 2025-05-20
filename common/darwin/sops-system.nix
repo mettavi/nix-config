e362c3063a3a11305fa9f87283723f49c6bfd9fe
Mettavihari <timotheos.allen@gmail.com>
2025-05-20 20:32:42 +1000

    Do not hardcode the user name

--- common/darwin/sops-system.nix
+++ common/darwin/sops-system.nix
@@ -28,7 +28,7 @@
       # the age key. These age keys are unique for the user on each host and are generated on their own (i.e. they are not derived
       # from an ssh key).
       "users/${user1}/encryption_key" = {
-        owner = "${config.users.users.timotheos.name}";
+        owner = "${config.users.users.${user1}.name}";
         # We need to ensure the entire directory structure is that of the user...
         path = "${config.users.users.${user1}.home}/.config/sops/age/keys.txt";
       };
