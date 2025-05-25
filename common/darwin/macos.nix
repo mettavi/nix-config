e3725e5fe0605acaa995d695270871e6e0519532
Mettavihari <timotheos.allen@gmail.com>
2025-05-25 22:17:44 +1000

    Remove old code and add setting for macOS trackpad to enable 3-finder drag

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -4,15 +4,14 @@
     dock.autohide = true;
     # check current setting with "defaults read NSGlobalDomain '<insert setting here>'"
     NSGlobalDomain = {
-      # This setting is currently ineffective as it should be value 2, see https://github.com/nix-darwin/nix-darwin/issues/1378
-      # WORKAROUND: set manually in activation.nix
-      # Mode 3 (should be 2?) enables keyboard navigation to focus between controls
-      # AppleKeyboardUIMode = 3;
       ApplePressAndHoldEnabled = false;
       # GUI steps (L to R): InitialKeyRepeat: 120, 94, 68, 35, 25, 15 / KeyRepeat: 120, 90, 60, 30, 12, 6, 2
       # (See https://apple.stackexchange.com/questions/261163/default-value-for-nsglobaldomain-initialkeyrepeat)
       InitialKeyRepeat = 35; # default 25
       KeyRepeat = 6; # default 6
+    };
+    trackpad = {
+      TrackpadThreeFingerDrag = true;
     };
     CustomUserPreferences = {
       NSGlobalDomain = {
