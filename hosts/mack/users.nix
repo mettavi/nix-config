c3ded46ee4489a788c04cbbd6c4c9b940d8f7af8
Mettavihari <timotheos.allen@gmail.com>
2025-05-25 20:20:39 +1000

    Change config to conform to new nix-darwin version requirements

--- hosts/mack/users.nix
+++ hosts/mack/users.nix
@@ -1,5 +1,6 @@
-{ user1, ...}:
-  { users.users.${user1} = {
+{ user1, ... }:
+{
+  users.users.${user1} = {
     name = "${user1}";
     home = "/Users/${user1}";
     # authorize remote login to host using personal ssh key
@@ -8,6 +9,19 @@
     ];
   };
 
-  imports = [ ../../common/users/${user1}/darwin.nix];
+  /*
+        Previously, some nix-darwin options applied to the user running
+    `   darwin-rebuild`. As part of a long‐term migration to make
+        nix-darwin focus on system‐wide activation and support first‐class
+        multi‐user setups, all system activation now runs as `root`, and
+        these options instead apply to the `system.primaryUser` user.
+        In the long run, this setting will be deprecated and removed after all the
+        functionality it is relevant for has been adjusted to allow
+        specifying the relevant user separately, moved under the
+        `users.users.*` namespace, or migrated to Home Manager.
+  */
+  system.primaryUser = "${user1}";
 
- }
+  imports = [ ../../common/users/${user1}/darwin.nix ];
+
+}
