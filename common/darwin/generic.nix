da1ff570130acadb40858fa6a3af31bd90f29be9
Mettavihari <timotheos.allen@gmail.com>
2025-06-01 21:07:11 +1000

    Enable automatic garbage collection and nix store optimisation in nix-darwin

--- common/darwin/generic.nix
+++ common/darwin/generic.nix
@@ -4,11 +4,21 @@
 }:
 {
   nix = {
+    # run nix-collect-garbage automatically 
+    # by default this will run each Sunday at (or after is the system is down) 3:15 am
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
     nixPath = [
       # this ensures $NIX_PATH is set to an immutable location in the nix-store
       # "nixpkgs-overlays=$HOME/.dotfiles/common/darwin/overlays"
       "darwin=${inputs.nix-darwin}"
     ];
+    # by default this will run each Sunday at (or after is the system is down) 4:15 am
+    optimise = {
+      automatic = true;
+    };
   };
   nixpkgs = {
     overlays = [ (import ../overlays/darwin/default.nix) ];
