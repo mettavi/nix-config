4bb2b9b785e25a776a95386d57d9c7a658a92f53
Mettavihari <timotheos.allen@gmail.com>
2025-06-01 21:07:51 +1000

    Enable automatic garbage collection of user profile in home-manager

--- common/users/timotheos/default.nix
+++ common/users/timotheos/default.nix
@@ -8,6 +8,14 @@
     preferXdgDirectories = true;
   };
 
+  nix = {
+    # by default this will run at midnight on Mondays (as per systemd.time(7))
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+  };
+
   # install the HTML manual and "home-manager-help" command
   manual.html.enable = true;
 
