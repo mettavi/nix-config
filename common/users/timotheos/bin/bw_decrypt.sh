6a8734844f5040e8e447fd2c06ae4ee93102adc8
Mettavihari <timotheos.allen@gmail.com>
2025-06-03 00:00:05 +1000

    Fix shebang to use env binary and apply shellcheck syntax tweaks

--- common/users/timotheos/bin/bw_decrypt.sh
+++ common/users/timotheos/bin/bw_decrypt.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-OUTNAME=$(basename $1 .enc).json
-openssl enc -aes-256-cbc -pbkdf2 -iter 1000000 -d -nopad -in $1 -out $OUTNAME
+OUTNAME=$(basename "$1" .enc).json
+openssl enc -aes-256-cbc -pbkdf2 -iter 1000000 -d -nopad -in "$1" -out "$OUTNAME"
