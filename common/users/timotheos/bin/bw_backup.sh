2cf26d2024d67731ced2826647cfb4635ab2ee47
Mettavihari <timotheos.allen@gmail.com>
2025-06-02 18:06:09 +1000

    Apply auto-formatting to script

--- common/users/timotheos/bin/bw_backup.sh
+++ common/users/timotheos/bin/bw_backup.sh
@@ -22,10 +22,10 @@ bw login --apikey
 BW_SESSION=$(bw unlock --passwordenv BW_PASSWORD --raw)
 
 if [ "$BW_SESSION" == "" ]; then
-    echo "$NOTIFICATION_EMAIL_BODY" | mail -s "$NOTIFICATION_EMAIL_SUBJECT" "$BW_NOTIFICATION_EMAIL"
-    bw logout
-    exit 1
-fi;
+  echo "$NOTIFICATION_EMAIL_BODY" | mail -s "$NOTIFICATION_EMAIL_SUBJECT" "$BW_NOTIFICATION_EMAIL"
+  bw logout
+  exit 1
+fi
 
 # Unencrypted export (not recommended)
 #bw --raw --session $BW_SESSION export --format json --output $EXPORT_PATH/$EXPORT_PLAIN_FILE
@@ -40,22 +40,21 @@ echo "Export encrypted json using openssl..."
 bw --raw --session "$BW_SESSION" export --format json | openssl enc -aes-256-cbc -pbkdf2 -iter 600000 -k "$OPENSSL_ENC_PASS" -out $EXPORT_PATH/"$EXPORT_OPENSSL_FILE"
 
 # ORGANIZATION
-if [[ ! -z "$BW_ORG_ID" ]]
-then
-    # Unencrypted export (not recommended)
-    #bw --raw --session $BW_SESSION export --organizationid $BW_ORG_ID --format json --output $EXPORT_PATH/$EXPORT_ORG_PLAIN_FILE
+if [[ ! -z "$BW_ORG_ID" ]]; then
+  # Unencrypted export (not recommended)
+  #bw --raw --session $BW_SESSION export --organizationid $BW_ORG_ID --format json --output $EXPORT_PATH/$EXPORT_ORG_PLAIN_FILE
 
-    # Encrypted export using encrypted_json from bitwarden
-    # echo "Export encrypted json using bitwarden format..."
-    # bw --raw --session $BW_SESSION export --organizationid $BW_ORG_ID --format encrypted_json --password $BW_PASSWORD --output $EXPORT_PATH/$EXPORT_ORG_ENCRYPTED_FILE
-    # chmod 644 $EXPORT_PATH/$EXPORT_ORG_ENCRYPTED_FILE
-    
-    # Encrypted export using openssl
-    echo "Export encrypted json using openssl..."
-    bw --raw --session "$BW_SESSION" export --organizationid "$BW_ORG_ID" --format json | openssl enc -aes-256-cbc -pbkdf2 -iter 600000 -k "$OPENSSL_ENC_PASS" -out $EXPORT_PATH/"$EXPORT_ORG_OPENSSL_FILE"
+  # Encrypted export using encrypted_json from bitwarden
+  # echo "Export encrypted json using bitwarden format..."
+  # bw --raw --session $BW_SESSION export --organizationid $BW_ORG_ID --format encrypted_json --password $BW_PASSWORD --output $EXPORT_PATH/$EXPORT_ORG_ENCRYPTED_FILE
+  # chmod 644 $EXPORT_PATH/$EXPORT_ORG_ENCRYPTED_FILE
+
+  # Encrypted export using openssl
+  echo "Export encrypted json using openssl..."
+  bw --raw --session "$BW_SESSION" export --organizationid "$BW_ORG_ID" --format json | openssl enc -aes-256-cbc -pbkdf2 -iter 600000 -k "$OPENSSL_ENC_PASS" -out $EXPORT_PATH/"$EXPORT_ORG_OPENSSL_FILE"
 else
-    echo
-    echo "No organizational vault defined."
+  echo
+  echo "No organizational vault defined."
 fi
 
 # Cleanup old files.
