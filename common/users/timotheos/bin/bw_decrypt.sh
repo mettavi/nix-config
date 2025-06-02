65fe9e7fa9963fc743c6ef26df581620bf0fa0d3
Mettavihari <timotheos.allen@gmail.com>
2025-06-03 00:47:41 +1000

    Add flag to openssl command to load the encryption/decryption password from an environment variable

--- common/users/timotheos/bin/bw_decrypt.sh
+++ common/users/timotheos/bin/bw_decrypt.sh
@@ -3,4 +3,4 @@
 source /Users/timotheos/.config/sops-nix/secrets/users/timotheos/bitwarden.env
 
 OUTNAME=$(basename "$1" .enc).json
-openssl enc -aes-256-cbc -pbkdf2 -iter 1000000 -d -nopad -in "$1" -out "$OUTNAME"
+openssl enc -aes-256-cbc -pbkdf2 -iter 600000 -k "$OPENSSL_ENC_PASS" -d -nopad -in "$1" -out "$OUTNAME"
