e156bc4ed3b36b42b61173eda1d8f1ca10a58070
Mettavihari <timotheos.allen@gmail.com>
2025-06-03 00:09:49 +1000

    Source the bitwarden.env file to access the openssl key

--- common/users/timotheos/bin/bw_decrypt.sh
+++ common/users/timotheos/bin/bw_decrypt.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+source /Users/timotheos/.config/sops-nix/secrets/users/timotheos/bitwarden.env
+
 OUTNAME=$(basename "$1" .enc).json
 openssl enc -aes-256-cbc -pbkdf2 -iter 1000000 -d -nopad -in "$1" -out "$OUTNAME"
