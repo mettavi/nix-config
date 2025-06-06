42755bc0a6c725c265b1f12fc747bf245e8af5a8
Mettavihari <timotheos.allen@gmail.com>
2025-06-06 15:04:34 +1000

    Combine all launch daemons into the same block and reorder them alphabetically

--- common/darwin/services.nix
+++ common/darwin/services.nix
@@ -25,6 +25,25 @@
 
   # NB: The daemon is not used in version 3.1.0 of karabiner-driverkit
   launchd.daemons = {
+    kanata = {
+      serviceConfig = {
+        Label = "com.github.jtroo-kanata";
+        ProgramArguments = [
+          "/run/current-system/sw/bin/kanata"
+          "-c"
+          "${config.users.users.timotheos.home}/.dotfiles/modules/kanata/kanata.lsp"
+        ];
+        RunAtLoad = false;
+        KeepAlive = {
+          # this keeps the kanata daemon alive when the karabiner daemon is alive
+          OtherJobEnabled = {
+            "com.github.pqrs-org-Karabiner-DriverKit-VirtualHIDDevice" = true;
+          };
+        };
+        StandardOutPath = "/var/log/kanata.log";
+        StandardErrorPath = "/var/log/kanata.log";
+      };
+    };
     karabiner = {
       serviceConfig = {
         Label = "com.github.pqrs-org-Karabiner-DriverKit-VirtualHIDDevice";
@@ -43,28 +62,6 @@
         AbandonProcessGroup = true;
         RunAtLoad = true;
         KeepAlive = true;
-      };
-    };
-  };
-
-  launchd.daemons = {
-    kanata = {
-      serviceConfig = {
-        Label = "com.github.jtroo-kanata";
-        ProgramArguments = [
-          "/run/current-system/sw/bin/kanata"
-          "-c"
-          "${config.users.users.timotheos.home}/.dotfiles/modules/kanata/kanata.lsp"
-        ];
-        RunAtLoad = false;
-        KeepAlive = {
-          # this keeps the kanata daemon alive when the karabiner daemon is alive
-          OtherJobEnabled = {
-            "com.github.pqrs-org-Karabiner-DriverKit-VirtualHIDDevice" = true;
-          };
-        };
-        StandardOutPath = "/var/log/kanata.log";
-        StandardErrorPath = "/var/log/kanata.log";
       };
     };
   };
