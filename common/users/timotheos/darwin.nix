5ddcb7b2f0d4de15a92fde08aed2fc986e84c131
Mettavihari <timotheos.allen@gmail.com>
2025-06-06 22:32:13 +1000

    Add launchd agent to run the bitwarden backup script weekly

--- common/users/timotheos/darwin.nix
+++ common/users/timotheos/darwin.nix
@@ -1,5 +1,8 @@
-{ user1, ... }:
+{ config, user1, ... }:
 {
+  # SYSTEM PREFERENCES FOR USER1 ON HOST "MACK" (DARWIN)
+
+  # NIX-DARWIN OPTIONS
   system.defaults.dock = {
     persistent-apps = [
       "/System/Applications/System Settings.app"
@@ -10,5 +13,34 @@
       "/Applications/Microsoft Word.app"
       "/Users/${user1}/.dotfiles/common/users/${user1}/bin/CaliSync.app"
     ];
+  };
+
+  # HOME_MANAGER OPTIONS
+  home-manager.users.${user1} = {
+    launchd.agents = {
+      # make an encrypted backup weekly
+      "org.bitwarden.backup" = {
+        enable = true;
+        config = {
+          Label = "org.bitwarden.backup";
+          ProgramArguments = [
+            "/usr/bin/env"
+            "zsh"
+            "-c"
+            "${config.users.users.${user1}.home}/.dotfiles/common/users/${user1}/bin/bw_backup.sh"
+          ];
+          # Run at midnight each Monday (will catch up if system is sleeping)
+          StartCalendarInterval = [
+            {
+              Hour = 0;
+              Minute = 0;
+              Weekday = 1;
+            }
+          ];
+          StandardErrorPath = /tmp/org.bitwarden.backup.err;
+          StandardOutPath = /tmp/org.bitwarden.backup.out;
+        };
+      };
+    };
   };
 }
