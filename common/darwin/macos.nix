ac4e5141005f9b793f2d861484b87cbbe81cc60d
Mettavihari <timotheos.allen@gmail.com>
2025-06-08 18:34:38 +1000

    Disable nix.* options and move them to /etc/nix/nix.custom.conf after changing to determinate nix/nixd system

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -18,10 +18,23 @@
     };
   };
 
-  # pam_reattach.so re-enables pam_tid.so in tmux
-  environment.etc."pam.d/sudo_local".text = /* bash */ ''
-    # Managed by Nix Darwin
-    auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
-    auth       sufficient     pam_tid.so
-  '';
+  environment.etc = {
+    "nix/nix.custom.conf".text = # bash
+      ''
+       # automatically detects files in the store that have identical contents, and replaces them with hard links to a single copy
+       auto-optimise-store = true
+       warn-dirty = false
+       lazy-trees = true
+       trusted-users = root
+       extra-substituters = https://nixpkgs.cachix.org https://nix-community.cachix.org https://cachix.cachix.org https://yazi.cachix.org https://mettavi.cachix.org  
+       extra-trusted-public-keys = nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k= mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU=
+      '';
+    # pam_reattach.so re-enables pam_tid.so in tmux
+    "pam.d/sudo_local".text = # bash
+      ''
+        # Managed by Nix Darwin
+        auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+        auth       sufficient     pam_tid.so
+      '';
+  };
 }
--- common/shared/nix.nix
+++ common/shared/nix.nix
@@ -1,46 +1,10 @@
 {
-  config,
   inputs,
-  pkgs,
-  user1,
   ...
 }:
 {
   nix = {
-    # auto upgrade nix package
-    package = pkgs.nix;
-    # this ensures $NIX_PATH is set to an immutable location in the nix-store
-    nixPath = [
-      "nixpkgs=${inputs.nixpkgs}"
-      "nixpkgs-overlays=${config.users.users.${user1}.home}/.dotfiles/common/overlays"
-      "home-manager=${inputs.home-manager}"
-    ];
-    optimise.automatic = true;
-    settings = {
-      # this setting is deprected, see https://bit.ly/3Cp2vYB
-      # auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      warn-dirty = false;
-      # substituters are always enabled, trusted-substituters can be enabled on demand by untrusted users
-      substituters = [
-        "https://nixpkgs.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://cachix.cachix.org"
-        "https://yazi.cachix.org"
-        "https://mettavi.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
-        "mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU="
-      ];
-      trusted-users = [ "root" ];
-    };
+    enable = false;
   };
 
   nixpkgs = {
