375ebc66f56d89a0e8b41c1ee1ca4787f0cd10ef
Mettavihari <timotheos.allen@gmail.com>
2025-06-09 13:39:50 +1000

    Add nixpkgs-overlays path to the nix-path config option to add local packages to nixpkgs set

--- common/darwin/macos.nix
+++ common/darwin/macos.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, user1, ... }:
 {
   imports = [ ./mac ];
 
@@ -28,6 +28,7 @@
        trusted-users = root
        extra-substituters = https://nixpkgs.cachix.org https://nix-community.cachix.org https://cachix.cachix.org https://yazi.cachix.org https://mettavi.cachix.org  
        extra-trusted-public-keys = nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k= mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU=
+       extra-nix-path =  nixpkgs-overlays=${config.users.users.${user1}.home}/.dotfiles/common/overlays
       '';
     # pam_reattach.so re-enables pam_tid.so in tmux
     "pam.d/sudo_local".text = # bash
