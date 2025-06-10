3e16f2a15d2c7bb1ae5483f7cfc1c906eae3e890
Mettavihari <timotheos.allen@gmail.com>
2025-06-10 21:45:39 +1000

    Remove code made unnecessary after recent changes

--- common/darwin/activation.nix
+++ common/darwin/activation.nix
@@ -1,9 +1,6 @@
-{ pkgs, config, ... }:
 {
   # check nix-darwin/modules/system/activation-scripts.nix for allowed values for activationScripts.<name>
   system.activationScripts = {
-    # "activationScripts.userScript"
-    # nix-darwin runs this first to ensure /run/current-system exists before the main script is executed
     postActivation.text = ''
       # Enable remote login for the host (macos ssh server)
       # WORKAROUND: `systemsetup -f -setremotelogin on` requires `Full Disk Access`
@@ -14,14 +11,5 @@
       # avoid a login/reboot to apply new settings after system activation (macOS)
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
-    # extraActivation.text = # bash
-    #   ''
-    #     # Enable remote login for the host (macos ssh server)
-    #     # WORKAROUND: `systemsetup -f -setremotelogin on` requires `Full Disk Access`
-    #     # permission for the Application calling it
-    #     if [[ "$(systemsetup -getremotelogin | sed 's/Remote Login: //')" == "Off" ]]; then
-    #       launchctl load -w /System/Library/LaunchDaemons/ssh.plist
-    #     fi
-    #   '';
   };
 }
