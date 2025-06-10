75797fcc7162fda11ca1f182a33807c079fe405c
Mettavihari <timotheos.allen@gmail.com>
2025-06-10 21:42:57 +1000

    Remove code for creating aliases for GUI apps which is now managed with mac-app-util module

--- common/darwin/activation.nix
+++ common/darwin/activation.nix
@@ -14,30 +14,6 @@
       # avoid a login/reboot to apply new settings after system activation (macOS)
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
-    # "activationScripts.script" (this is run after "userScript")
-    applications.text =
-      # install GUI apps with alias instead of symlink to show up in spotlight search
-      let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-      pkgs.lib.mkForce # bash
-        ''
-          # Set up applications.
-          echo "setting up /Applications..." >&2
-          rm -rf /Applications/Nix\ Apps
-          mkdir -p /Applications/Nix\ Apps
-          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-          while read -r src; do
-            app_name=$(basename "$src")
-            echo "copying $src" >&2
-            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-          done
-        '';
-
     # extraActivation.text = # bash
     #   ''
     #     # Enable remote login for the host (macos ssh server)
