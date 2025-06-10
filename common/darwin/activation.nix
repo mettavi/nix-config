a3335734b26a45686318232e1ff0d274f3c5a0bc
Mettavihari <timotheos.allen@gmail.com>
2025-06-10 21:40:48 +1000

    Move script from activationScripts.extraActivation to postActivation config block

--- common/darwin/activation.nix
+++ common/darwin/activation.nix
@@ -5,6 +5,12 @@
     # "activationScripts.userScript"
     # nix-darwin runs this first to ensure /run/current-system exists before the main script is executed
     postActivation.text = ''
+      # Enable remote login for the host (macos ssh server)
+      # WORKAROUND: `systemsetup -f -setremotelogin on` requires `Full Disk Access`
+      # permission for the Application calling it
+      if [[ "$(systemsetup -getremotelogin | sed 's/Remote Login: //')" == "Off" ]]; then
+      launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+      fi
       # avoid a login/reboot to apply new settings after system activation (macOS)
       /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     '';
@@ -32,14 +38,14 @@
           done
         '';
 
-    extraActivation.text = # bash
-      ''
-        # Enable remote login for the host (macos ssh server)
-        # WORKAROUND: `systemsetup -f -setremotelogin on` requires `Full Disk Access`
-        # permission for the Application calling it
-        if [[ "$(systemsetup -getremotelogin | sed 's/Remote Login: //')" == "Off" ]]; then
-          launchctl load -w /System/Library/LaunchDaemons/ssh.plist
-        fi
-      '';
+    # extraActivation.text = # bash
+    #   ''
+    #     # Enable remote login for the host (macos ssh server)
+    #     # WORKAROUND: `systemsetup -f -setremotelogin on` requires `Full Disk Access`
+    #     # permission for the Application calling it
+    #     if [[ "$(systemsetup -getremotelogin | sed 's/Remote Login: //')" == "Off" ]]; then
+    #       launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+    #     fi
+    #   '';
   };
 }
