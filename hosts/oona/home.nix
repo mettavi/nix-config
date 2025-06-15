3f396237cf6d9bc1e3e73e7191042aef4443bec1
Mettavihari <timotheos.allen@gmail.com>
2025-06-15 22:01:26 +1000

    Add home-manager module for new linux host "oona"

---
+++ hosts/oona/home.nix
@@ -0,0 +1,20 @@
+{ inputs, user1, system, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.${user1} = import ../../common/users/${user1};
+    # Optionally, use home-manager.extraSpecialArgs to pass
+    # arguments to home-manager modules
+    extraSpecialArgs = {
+      inherit inputs system user1;
+    };
+    # sharedModules = [
+    #   inputs.sops-nix.homeManagerModules.sops
+    #   inputs.mac-app-util.homeManagerModules.default
+    # ];
+  };
+}
