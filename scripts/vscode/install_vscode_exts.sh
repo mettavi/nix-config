824f041978f7362dd31474ef39b3864c98c1f1d0
Mettavihari <timotheos.allen@gmail.com>
2025-06-15 22:43:25 +1000

    Remove pre-nix dotfiles scripts from repo

--- scripts/vscode/install_vscode_exts.sh
+++
@@ -1,4 +0,0 @@
-code --install-extension chrmarti.regex &
-code --install-extension dbaeumer.vscode-eslint &
-code --install-extension formulahendry.code-runner &
-code --install-extension ritwickdey.liveserver
--- scripts/vscode/save_vscode_exts.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-code --list-extensions |
-  xargs -L 1 echo code --install-extension |
-  sed "\$!s/$/ \&/" >"$HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh"
-chmod +x "$HOME/.dotfiles/scripts/vscode/install_vscode_exts.sh"
