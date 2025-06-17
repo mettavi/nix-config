c94b423e964e69e346fce536262db7bae1728caa
Mettavihari <timotheos.allen@gmail.com>
2025-06-17 20:55:00 +1000

    Add conditional statement to define the location of the home directory depending on the host system

--- common/users/timotheos/default.nix
+++ common/users/timotheos/default.nix
@@ -1,17 +1,17 @@
 {
+  pkgs,
   username,
   ...
 }:
 {
   home = {
     username = "${username}";
-    homeDirectory = "/Users/${username}";
+    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "23.11";
     # make programs use XDG directories whenever supported
     preferXdgDirectories = true;
   };
-
 
   # install the HTML manual and "home-manager-help" command
   manual.html.enable = true;
