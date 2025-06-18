eb2ec9424136d3647483d6cc8ef37e5d2940b60a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-18 13:09:00 +1000

    Fix typo in file path

--- common/users/timotheos/dots.nix
+++ common/users/timotheos/dots.nix
@@ -28,7 +28,7 @@
     ".gitignore_global".source = ../../../modules/git/.gitignore_global;
     ".npmrc".source = ../../../modules/node/.npmrc;
     "Library/Preferences/com.plexapp.plexmediaserver.plist".source =
-      if pkgs.stdenv.isDarwin then ./../../modules/plex/com.plexapp.plexmediaserver.plist else null;
+      if pkgs.stdenv.isDarwin then ../../../modules/plex/com.plexapp.plexmediaserver.plist else null;
     ".vim/vimrc".source = ../../../modules/vim/.vim/vimrc;
     ".vim/colors/molokai.vim".source = ../../../modules/vim/.vim/colors/molokai.vim;
   };
