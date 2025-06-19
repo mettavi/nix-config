a0cab5e9060ecf771c645b0098ec758683827bd0
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-19 22:07:37 +1000

    Add unzip package to linux module

--- common/linux/packages.nix
+++ common/linux/packages.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # gcc and gnumake are required for nvim
+    # gcc, gnumake and unzip are required for nvim
     # GNU Compiler Collection
     gcc
     # Tool to control the generation of non-source files from sources
     gnumake
+    # Extraction utility
+    unzip
   ];
 }
