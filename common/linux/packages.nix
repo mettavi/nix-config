e7dd978b2530d543a63ac8efecc845980b27ed41
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-19 21:49:55 +1000

    Add gmumake package to linux module

--- common/linux/packages.nix
+++ common/linux/packages.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    # gcc and gnumake are required for nvim
     # GNU Compiler Collection
     gcc
+    # Tool to control the generation of non-source files from sources
+    gnumake
   ];
 }
