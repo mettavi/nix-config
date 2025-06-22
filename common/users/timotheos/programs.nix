b1dd07ebbaaac95d6ea287eb9b33eb087ae81f3a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-22 21:52:57 +1000

    Move tmux config to a separate file

--- common/users/timotheos/programs.nix
+++ common/users/timotheos/programs.nix
@@ -15,6 +15,8 @@
     fzf-git-sh
   ];
 
+  imports = [ ./programs ];
+
   programs = {
     # ensure home-manager itself is installed
     home-manager.enable = true;
@@ -108,76 +110,6 @@
       enableZshIntegration = true;
     };
 
-    tmux = {
-      enable = true;
-      baseIndex = 1;
-      customPaneNavigationAndResize = true;
-      escapeTime = 10;
-      extraConfig = # sh
-        ''
-          # Allow programs to bypass tmux using a terminal escape sequence (eg for image rendering)
-          set -g allow-passthrough on
-          # recommended by yazi to enable proper image rendering
-          set -ga update-environment TERM
-          set -ga update-environment TERM_PROGRAM
-          # Enable true-color/italics in a terminal in tmux
-          set -ga terminal-overrides ',xterm*:Tc:sitm=\E[3m'
-          set -g status-position top
-          # renumber open windows when one is closed
-          set -g renumber-windows on
-          # use semicolon to enter command mode
-          bind \; command-prompt
-          unbind %
-          bind | split-window -h -c "#{pane_current_path}" # opens in cwd rather than original
-          unbind '"'
-          bind - split-window -v -c "#{pane_current_path}" # opens in cwd rather than original
-          # open a new window in the cwd
-          bind c new-window -c "#{pane_current_path}"
-          # reload tmux config with <prefix>-r
-          unbind r
-          bind r source-file ~/.config/tmux/tmux.conf \; display-message "Tmux config reloaded!"
-          # maximise a pane (toggle)
-          bind -r m resize-pane -Z
-          # clear screen with <prefix>-C-l (for "CAPS-LOCK as CTRL" need to release key in between) 
-          bind C-l send-keys 'C-l' 
-          bind -T copy-mode-vi 'v' send -X begin-selection # select text by character/line, and V for full-line (no space necessary)
-          bind -T copy-mode-vi 'C-v' send -X rectangle-toggle \; send -X begin-selection # block-select text (with required space added)
-          bind -T copy-mode-vi 'y' send -X copy-selection # copy text with "y"
-          unbind -T copy-mode-vi MouseDragEnd1Pane # don't exit copy mode when dragging with mouse
-        '';
-      historyLimit = 50000;
-      keyMode = "vi";
-      mouse = true;
-      newSession = true;
-      plugins = with pkgs; [
-        {
-          plugin = tmuxPlugins.catppuccin;
-          extraConfig = ''
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-            set -g @catppuccin_status_modules_right "directory date_time"
-            set -g @catppuccin_status_modules_left "session"
-            set -g @catppuccin_status_left_separator  " "
-            set -g @catppuccin_status_right_separator " "
-            set -g @catppuccin_status_right_separator_inverse "no"
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-            set -g @catppuccin_directory_text "#{b:pane_current_path}"
-            set -g @catppuccin_date_time_text "%H:%M"
-          '';
-        }
-      ];
-      shell = "${pkgs.zsh}/bin/zsh";
-      shortcut = "a";
-      terminal = "tmux-256color";
-      tmuxp.enable = true;
-    };
     yt-dlp.enable = true;
     zoxide = {
       enable = true;
