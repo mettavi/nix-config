fb9b5e456fe93faa5d9ed0c74a3e43ca08bffd52
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-28 15:25:10 +1000

    Disable the latex function in render-markdown plugin to prevent unneecessary checks

--- modules/nvim/lua/mettavi/plugins/render-markdown.lua
+++ modules/nvim/lua/mettavi/plugins/render-markdown.lua
@@ -1,6 +1,6 @@
 -- Plugin to improve viewing Markdown files in Neovim
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  opts = {},
+  opts = { latex = { enabled = false } },
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 }
