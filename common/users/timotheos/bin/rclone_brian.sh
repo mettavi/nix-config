63c25a478710552383913e4d83055fc8663e549a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-06-29 22:36:21 +1000

    Add --flags to rclone command to preserve symlinks

--- common/users/timotheos/bin/rclone_brian.sh
+++ common/users/timotheos/bin/rclone_brian.sh
@@ -2,5 +2,5 @@
 #
 # This script will tar, compress, stream and copy a archive of Brian's home directory to AWS S3 Glacier Deep Archive using rclone
 tar -zcvf - "/Volumes/Brian T7/" --exclude-from="$NIXFILES/common/users/timotheos/conf/rclone/exclude-brian.txt" |
-  rclone --dry-run --progress --stats 2s --verbose=2 --log-file="$HOME/Library/Logs/rclone-teststderr.log" --fast-list \
+  rclone --dry-run --links --progress --stats 2s --verbose=2 --log-file="$HOME/Library/Logs/rclone-teststderr.log" --fast-list \
     rcat --dump filters --transfers 100 --checkers 200 aws_gda-crypt:/brian_macos_10.11.6.tar.gz | tee ~/Library/Logs/rclone-teststdout.log
