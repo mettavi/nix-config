c0f2484343a485f2609888f595be337f525c718e
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-04 15:42:54 +1000

    Pass the username variable through specialargs instead of a local assignment

--- hosts/salina/configuration.nix
+++ hosts/salina/configuration.nix
@@ -1,12 +1,10 @@
 {
   lib,
   pkgs,
+  username,
   ...
 }@args:
 
-let
-  username = "timotheos";
-in
 {
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
