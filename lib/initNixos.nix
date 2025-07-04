3298377fc63654b9a80c5ed0c005315544e97f19
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-04 15:40:41 +1000

    Move nixos-anywhere specific options from configuration.nix to initNixos.nix

--- lib/initNixos.nix
+++ lib/initNixos.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, modulesPath, ... }:
 {
   # Function for NixOS system configuration
   mkNixosConfiguration =
@@ -13,7 +13,10 @@
           ;
       };
       modules = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+        (modulesPath + "/profiles/qemu-guest.nix")
         inputs.disko.nixosModules.disko
+        ../hosts/${hostname}/.disk-config.nix
         ../hosts/${hostname}/configuration.nix
         {
           users.users.${username} = {
