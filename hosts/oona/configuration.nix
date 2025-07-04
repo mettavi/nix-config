82e3298e88d09e1b8d31accb29b64581f87a3e73
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-04 16:48:24 +1000

    Update system.stateVersion to current stable version of nixOS upon fresh install

--- hosts/oona/configuration.nix
+++ hosts/oona/configuration.nix
@@ -191,6 +191,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
