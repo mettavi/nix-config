82e15f5c35f575e6cfea16b8e48352e55719c022
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-07 23:52:29 +1000

    Configure NIX_PATH variable for Nixos

--- common/linux/generic.nix
+++ common/linux/generic.nix
@@ -1,12 +1,21 @@
 {
   nix = {
-    # enable automatic garbage collection and store optimisation 
+    # enable automatic garbage collection and store optimisation
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 30d"; # Delete generations older than 30 days
       persistent = true;
     };
+    # this ensures $NIX_PATH is set to an immutable location in the nix-store
+    nixPath = [
+      "nixpkgs=${inputs.nixos-pkgs}"
+      "nixpkgs-overlays=${config.users.users.${username}.home}/${nix_repo}/common/overlays/shared"
+      "nixos-config=${
+        config.users.users.${username}.home
+      }/${nix_repo}/hosts/${hostname}/configuration.nix"
+      "home-manager=${inputs.home-manager}"
+    ];
     optimise = {
       automatic = true;
       dates = "02:15";
