6ffa7adf3dcc67b21d61b1f9726295b29b5b4969
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-07 23:53:35 +1000

    Configure settings for binary caches on NixOS

--- common/linux/generic.nix
+++ common/linux/generic.nix
@@ -1,4 +1,12 @@
 {
+  config,
+  hostname,
+  inputs,
+  username,
+  nix_repo,
+  ...
+}:
+{
   nix = {
     # enable automatic garbage collection and store optimisation
     gc = {
@@ -19,6 +27,26 @@
     optimise = {
       automatic = true;
       dates = "02:15";
+    };
+    settings = {
+      extra-substituters = [
+        "https://nixpkgs.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cachix.cachix.org"
+        "https://yazi.cachix.org"
+        "https://mettavi.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+        "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+        "mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU="
+      ];
+      trusted-users = [
+        "@staff"
+        "root"
+      ];
     };
   };
 }
