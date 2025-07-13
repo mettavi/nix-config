b70f8ccd9af0afca997331d277e16af586681b70
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-13 15:07:21 +1000

    Add setting to autohide the xfce taskbar on host oona

--- common/users/timotheos/xfce.nix
+++ common/users/timotheos/xfce.nix
@@ -11,6 +11,11 @@
           "Gtk/FontName" = "Sans 12";
           "Gtk/MonospaceFontName" = "Monospace 12";
         };
+        xfce4-panel = {
+          # always autohide the top panel (taskbar)
+          # 2 = always, 1 = intelligent, 0 = never
+          "panels/panel-1/autohide-behavior" = 2;
+        };
         xfce4-screensaver = {
           "lock/saver-activation/enabled" = false;
         };
