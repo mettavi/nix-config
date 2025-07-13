4dcdff39e49f382d953d5ecb152c3a9cec8cd901
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-13 20:18:03 +1000

    Remove zotero package as unworkable

--- common/darwin/pkgs/default.nix
+++ common/darwin/pkgs/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
-  goldendictng-gh = ( pkgs.callPackage ./goldendictng-gh { } );
-  karabiner-driverkit = ( pkgs.callPackage ./karabiner-driverkit { } );
-  libation-gh = ( pkgs.callPackage ./libation-gh { } );
-  stacher = ( pkgs.callPackage ./stacher { } );
+  goldendictng-gh = (pkgs.callPackage ./goldendictng-gh { });
+  karabiner-driverkit = (pkgs.callPackage ./karabiner-driverkit { });
+  libation-gh = (pkgs.callPackage ./libation-gh { });
+  stacher = (pkgs.callPackage ./stacher { });
 }
