5b956accab2e5949a41f0868420b549ca2bc1a1f
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-15 17:51:50 +1000

    Change keyboard mapping for telescope send to quickfix list function to prevent conflict with PastePal app

--- dots/nvim/lua/mettavi/plugins/telescope.lua
+++ dots/nvim/lua/mettavi/plugins/telescope.lua
@@ -22,7 +22,7 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- for items selected with tab key
+            ["<S-C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- for items selected with tab key
             ["<C-q>"] = actions.send_to_qflist + actions.open_qflist, -- for all items found
             ["<C-t>"] = open_with_trouble,
           },
