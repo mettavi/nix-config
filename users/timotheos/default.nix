1ef46246b9ac49a05ea175c5fded2a2bce3f3f12
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-17 14:30:10 +1000

    Remove code which has been moved to mkHomeUser.nix and add enabled modules

--- users/timotheos/default.nix
+++ users/timotheos/default.nix
@@ -1,23 +1,14 @@
 {
-  pkgs,
-  username,
-  ...
-}:
-{
-  home = {
-    username = "${username}";
-    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${username}" else "/home/${username}";
-    # make programs use XDG directories whenever supported
-    preferXdgDirectories = true;
-  };
 
   # install the HTML manual and "home-manager-help" command
   manual.html.enable = true;
 
-  imports = [
-    ../../home/shared/dots.nix
-    ../../home/shared/programs.nix
-    ../../home/shared/services-home.nix
-    ../../home/shared/sops-home.nix
-  ];
+  nyx = {
+    modules = {
+      shell = {
+        bash.enable = true;
+        nh.enable = true;
+      };
+    };
+  };
 }
