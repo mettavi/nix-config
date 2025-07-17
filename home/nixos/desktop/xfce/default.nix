f1e7b0623af4f304a038393ab508576de6feeb06
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-17 14:26:53 +1000

    Add XFCE module for nixos home-manager

---
+++ home/nixos/desktop/xfce/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.nyx.modules.desktop.xfce;
+in
+{
+  options.nyx.modules.desktop.xfce = {
+    enable = lib.mkEnableOption "Configure XFCE Desktop";
+  };
+
+  config = lib.mkIf cfg.enable {
+    xfconf = {
+      enable = true;
+      settings = {
+        displays = {
+          "Default/Virtual-1/Scale" = 0.571426;
+        };
+        xsettings = {
+          "Gtk/FontName" = "Sans 12";
+          "Gtk/MonospaceFontName" = "Monospace 12";
+        };
+        xfce4-panel = {
+          # always autohide the top panel (taskbar)
+          # 2 = always, 1 = intelligent, 0 = never
+          "panels/panel-1/autohide-behavior" = 2;
+        };
+        xfce4-screensaver = {
+          "lock/saver-activation/enabled" = false;
+        };
+      };
+    };
+  };
+}
