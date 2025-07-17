30fbdae82483856c09a48d39b7f2cd849738fc90
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-17 12:58:23 +1000

    Add module for nh tool to home-manager shared folder

---
+++ home/shared/modules/shell/nh/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.nyx.modules.shell.nh;
+in
+{
+  options.nyx.modules.shell.nh = {
+    enable = lib.mkEnableOption "Install the nh (nix helper) tool";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nh = {
+      enable = true;
+    };
+    home.sessionVariables = lib.mkIf pkgs.stdenv.isDarwin {
+      # prevent nh from checking for flakes "experimental features" on darwin (which it can't read from determinate nix.conf)
+      NH_NO_CHECKS = "1";
+    };
+  };
+}
