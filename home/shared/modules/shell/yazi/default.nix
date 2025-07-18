887044585ffc02bc157fc023a03c436f615071ec
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-07-18 21:25:36 +1000

    Move settings for yazi binary cache from home-manager to nixos scope

--- home/shared/modules/shell/yazi/default.nix
+++ home/shared/modules/shell/yazi/default.nix
@@ -65,9 +65,5 @@ in
       };
       shellWrapperName = "y";
     };
-    nix.settings = {
-      extra-substituters = [ "https://yazi.cachix.org" ];
-      extra-trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
-    };
   };
 }
--- system/nixos/generic.nix
+++ system/nixos/generic.nix
@@ -2,6 +2,7 @@
   config,
   hostname,
   inputs,
+  lib,
   nix_repo,
   options,
   username,
@@ -31,18 +32,26 @@
       dates = "02:15";
     };
     settings = {
-      extra-substituters = [
-        "https://nixpkgs.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://cachix.cachix.org"
-        "https://mettavi.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
-        "mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU="
-      ];
+      extra-substituters =
+        [
+          "https://nixpkgs.cachix.org"
+          "https://nix-community.cachix.org"
+          "https://cachix.cachix.org"
+          "https://mettavi.cachix.org"
+        ]
+        ++ lib.optionals config.home-manager.users.${username}.nyx.modules.shell.yazi.enable [
+          "https://yazi.cachix.org"
+        ];
+      extra-trusted-public-keys =
+        [
+          "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+          "mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU="
+        ]
+        ++ lib.optionals config.home-manager.users.${username}.nyx.modules.shell.yazi.enable [
+          "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+        ];
       trusted-users = [
         "@staff"
         "root"
