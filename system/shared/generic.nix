c3e66e9e46c36e3f53d83bb6171c65e653d84b24
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-08-04 16:45:35 +1000

    Add timezone setting to shared generic configuration

--- system/shared/generic.nix
+++ system/shared/generic.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   # List of directories to be symlinked in /run/current-system/sw
   environment = {
@@ -11,4 +12,8 @@
       "/share/zsh"
     ];
   };
+
+  # Set your time zone.
+  time.timeZone = lib.mkDefault "Australia/Melbourne";
+
 }
