2ea1f993b59fdc35d306e766a1240db591d40b79
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-08-04 16:45:01 +1000

    Add locale settings to nixos host function

--- lib/mkNixos.nix
+++ lib/mkNixos.nix
@@ -37,6 +37,21 @@
               ];
             };
           };
+          # Select internationalisation properties.
+          i18n = {
+            defaultLocale = "en_GB.UTF-8";
+            extraLocaleSettings = {
+              LC_ADDRESS = "en_AU.UTF-8";
+              LC_IDENTIFICATION = "en_AU.UTF-8";
+              LC_MEASUREMENT = "en_AU.UTF-8";
+              LC_MONETARY = "en_AU.UTF-8";
+              LC_NAME = "en_AU.UTF-8";
+              LC_NUMERIC = "en_AU.UTF-8";
+              LC_PAPER = "en_AU.UTF-8";
+              LC_TELEPHONE = "en_AU.UTF-8";
+              LC_TIME = "en_AU.UTF-8";
+            };
+          };
           networking.hostName = "${hostname}";
           nix = {
             extraOptions = ''
