1988aadcb56e11053bc4ceba9ba53567c30b4c83
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-08-14 15:03:22 +1000

    Update keybinds for new flash.nvim utility

--- home/shared/dots/nvim/lua/mettavi/plugins/flash.lua
+++ home/shared/dots/nvim/lua/mettavi/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     modes = {
       search = {
-        -- use flash in search mode by default
+        -- use flash in search mode (/ or ?) by default
         enabled = true,
       },
       char = {
@@ -18,8 +18,8 @@ return {
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<leader>s", mode = "o", function() require("flash").remote() end, desc = "Flash Remote" },
+    { "<leader>S", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Flash Treesitter Remote" },
+    { "<S-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
