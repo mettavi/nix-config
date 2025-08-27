e0db8c8c33d6a3020ea1a3d2c8ca77d70901dff9
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-08-27 12:57:48 +1000

    Enable transparent background in lualine bar

--- home/shared/dots/nvim/lua/mettavi/plugins/lualine.lua
+++ home/shared/dots/nvim/lua/mettavi/plugins/lualine.lua
@@ -20,32 +20,32 @@ return {
       normal = {
         a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.none, fg = colors.fg },
       },
       insert = {
         a = { bg = colors.green, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.none, fg = colors.fg },
       },
       visual = {
         a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.none, fg = colors.fg },
       },
       command = {
         a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.none, fg = colors.fg },
       },
       replace = {
         a = { bg = colors.red, fg = colors.bg, gui = "bold" },
         b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
+        c = { bg = colors.none, fg = colors.fg },
       },
       inactive = {
         a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
         b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+        c = { bg = colors.none, fg = colors.semilightgray },
       },
     }
 
