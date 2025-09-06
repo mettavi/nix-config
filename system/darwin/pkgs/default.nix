43f756c2d166dbed8dad775c295583b523b0d270
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-09-06 15:49:07 +1000

    Do not install the libation-gh local package, which is no longer needed

--- system/darwin/pkgs/default.nix
+++ system/darwin/pkgs/default.nix
@@ -2,6 +2,6 @@
 {
   goldendictng-gh = (pkgs.callPackage ./goldendictng-gh { });
   karabiner-driverkit = (pkgs.callPackage ./karabiner-driverkit { });
-  libation-gh = (pkgs.callPackage ./libation-gh { });
+  # libation-gh = (pkgs.callPackage ./libation-gh { });
   stacher = (pkgs.callPackage ./stacher { });
 }
