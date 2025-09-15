f1a81638b9c01a2e555d762b0b4d090fbd84797d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-09-15 22:08:00 +1000

    Create new module for restic in the system configuration area

---
+++ system/shared/modules/system/restic/default.nix
@@ -0,0 +1,90 @@
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  username,
+  ...
+}:
+with lib;
+let
+  withRestic = (
+    builtins.any (config: config.nyx.modules.shell.restic.enable) (
+      builtins.attrValues config.home-manager.users
+    )
+  );
+  # cfg = config.nyx.modules.system.resticdaemon;
+in
+{
+  # options.nyx.modules.system.resticdaemon = {
+  #   enable = mkOption {
+  #     type = types.bool;
+  #     # enable the system daemon for any user that has the restic option enabled
+  #     default = withRestic;
+  #     description = "Install a system daemon to run restic tasks";
+  #   };
+  # };
+
+  launchd.daemons = mkIf (pkgs.stdenv.isDarwin && withRestic) {
+    resticprofile = {
+      serviceConfig = {
+        EnvironmentVariables = {
+          PATH = "/usr/local/sbin:/usr/local/bin:/etc/profiles/per-user/${username}/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/${username}/.local/bin";
+          RESTICPROFILE_SCHEDULE_ID = "${
+            config.users.users.${username}.home
+          }/.config/resticprofile/profiles.toml:backup@default";
+        };
+        Label = "${hostname}.resticprofile.${username}.default.backup";
+        LimitLoadToSessionType = "System";
+        LowPriorityBackgroundIO = false;
+        LowPriorityIO = false;
+        Nice = 0;
+        ProcessType = "Standard";
+        Program = "/etc/profiles/per-user/${username}/bin/resticprofile";
+        ProgramArguments = [
+          "/etc/profiles/per-user/${username}/bin/resticprofile"
+          "--no-prio"
+          "--no-ansi"
+          "--config"
+          "${config.users.users.${username}.home}/.config/resticprofile/profiles.toml"
+          "run-schedule"
+          "backup@default"
+        ];
+        StartCalendarInterval = [
+          {
+            Hour = 0;
+            Minute = 30;
+          }
+        ];
+        WorkingDirectory = "${config.users.users.${username}.home}/Library/Logs/restic";
+      };
+    };
+  };
+  home-manager.sharedModules = [
+    (
+      { config, ... }:
+      {
+        options.nyx.modules.shell.restic = {
+          enable = mkEnableOption "Install and configure the restic backup tool";
+          useRestProf = mkOption {
+            type = types.bool;
+            default = true;
+            description = "Use resticprofile to manage restic using config files";
+          };
+        };
+        config =
+          mkIf config.nyx.modules.shell.restic.enable {
+            home.packages = with pkgs; [
+              restic # Backup with delta transfers (eg. to cloud storage via rclone)
+            ];
+          }
+          // mkIf config.nyx.modules.shell.restic.useRestProf {
+            home.packages = with pkgs; [ resticprofile ]; # Configuration manager for restic
+            xdg.configFile = {
+              "resticprofile".source = ../../../../../home/shared/dots/resticprofile;
+            };
+          };
+      }
+    )
+  ];
+}
