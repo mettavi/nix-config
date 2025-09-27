7664a71a4d15e20dd8030a09f171585a5a3a5072
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-09-27 20:43:50 +1000

    Add user configuration block for the "myadmin" user, whose default user is "timotheos"

--- system/shared/modules/system/users/default.nix
+++ system/shared/modules/system/users/default.nix
@@ -113,4 +113,14 @@ in
           }
         ) cfg.users;
   };
+  myUserConfig.users = {
+    "myadmin" = {
+      # defaults to username "timotheos"
+      # pull encrupted "soft" secrets from private git rep
+      name = inputs.secrets.name;
+      description = "This is main admin account of the system, whose default username on most systems is \"timotheos\"";
+      email = inputs.secrets.email.gitHub;
+    };
+  };
+  _module.args.username = config.myUserConfig.users.myadmin.username;
 }
