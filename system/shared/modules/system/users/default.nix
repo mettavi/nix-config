8b0799e54609563a470d32ce8af5515fe6477eee
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-09-27 18:51:05 +1000

    Add module to set up users on the system

---
+++ system/shared/modules/system/users/default.nix
@@ -0,0 +1,98 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.myUserConfig; # Define a namespace for your options
+in
+{
+  options.myUserConfig = {
+    users = mkOption {
+      type = types.attrsOf (
+        types.submodule (
+          { name, ... }:
+          {
+            options = {
+              enable = mkOption {
+                type = types.bool;
+                default = false;
+                description = "Whether to enable this user on the current host.";
+              };
+              username = mkOption {
+                type = types.str;
+                default = "timotheos";
+                description = "The username on the system";
+              };
+              name = mkOption {
+                type = types.str;
+                default = name; # Default to the attribute name if not specified
+                description = "The display name of the user.";
+              };
+              email = mkOption {
+                type = types.str;
+                default = "";
+                description = "The email address of the user.";
+              };
+              passwordHash = mkOption {
+                type = types.str;
+                default = "";
+                description = "The hashed password for the user.";
+              };
+              sudo = mkOption {
+                type = types.bool;
+                default = false;
+                description = "Whether the user has sudo privileges.";
+              };
+              isNormalUser = mkOption {
+                type = types.bool;
+                default = true;
+                description = "Whether this user is a normal user.";
+              };
+              extraGroups = mkOption {
+                type = types.listOf types.str;
+                default = [ ];
+                description = "Additional groups for this user.";
+              };
+              # Add more user-specific options as needed
+              shell = mkOption {
+                type = types.package;
+                default = pkgs.bash;
+                description = "The default shell for this user.";
+              };
+            };
+          }
+        )
+      );
+      default = { };
+      description = "User configurations for the current host.";
+    };
+  };
+
+  # Use the declared options to configure actual users
+  config = mkIf (cfg.users != { }) {
+    users.users =
+      if pkgs.stdenv.isLinux then
+        mapAttrs' (
+          name: userCfg:
+          mkIf userCfg.enable {
+            name = name;
+            isNormalUser = userCfg.isNormalUser;
+            hashedPassword = userCfg.passwordHash;
+            extraGroups = userCfg.extraGroups;
+            shell = userCfg.shell;
+            # Map other options here
+          }
+        )
+      else
+        mapAttrs' (
+          name: userCfg:
+          mkIf userCfg.enable {
+            name = name;
+            shell = userCfg.shell;
+          }
+        ) cfg.users;
+  };
+}
