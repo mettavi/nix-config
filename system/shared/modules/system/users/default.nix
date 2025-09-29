c9267fa0eee35ad13a2c84e97d8e8d4c3283c3bf
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-09-29 15:30:22 +1000

    Wrap the code in a config block to create a proper module structure

--- system/shared/modules/system/users/default.nix
+++ system/shared/modules/system/users/default.nix
@@ -1,18 +1,100 @@
-{ config, inputs, ... }:
 {
-  imports = [ ./user_opts.nix ];
+  config,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+let
+  # Get all users defined in your darwin system
+  cfg = config.nyx.system;
+in
+{
+  # make the user module options available
+  imports = [
+    ./user_opts.nix
+    inputs.home-manager.darwinModules.home-manager
+  ];
 
-  # list all users managed by the nix system
-  nyx.system.userConfig.users = {
-    "myadmin" = {
-      # defaults to username "timotheos"
-      # pull encrypted "soft" secrets from private git rep
-      name = inputs.secrets.name;
-      description = "This is main admin account of the system, whose default username on most systems is \"timotheos\"";
-      email = inputs.secrets.email.gitHub;
+  config = {
+    # create a database of all users managed by the nix system
+    nyx.system.userConfig = {
+      myadmin = {
+        # pull encrypted "soft" secrets from private git rep
+        fullname = inputs.secrets.name;
+        description = "This is main admin account of the system, whose default username on most systems is \"timotheos\"";
+        email = inputs.secrets.email.gitHub;
+      };
     };
-  };
 
-  # pass the primary admin user's username to other modules
-  _module.args.username = config.nyx.system.userConfig.users.myadmin.username;
+    # Use the declared options and user data to set up users on the system
+    users.users =
+      if pkgs.stdenv.isLinux then
+        # nixos config
+        mapAttrs (
+          name: usrCfg:
+          mkIf usrCfg.enable {
+            name = usrCfg.username;
+            description = usrCfg.description;
+            home = "/home/${usrCfg.username}";
+            isNormalUser = usrCfg.isNormalUser;
+            # this is required to enable password login
+            # (create hash with "mkpasswd -m sha-512", or "mkpasswd" to use the stronger default "yes" encryption)
+            hashedPasswordFile = usrCfg.passwordHashFile;
+            extraGroups = usrCfg.extraGroups;
+            shell = usrCfg.shell;
+          }
+        )
+      else
+        # darwin config
+        mapAttrs (
+          name: usrCfg:
+          mkIf usrCfg.enable {
+            name = usrCfg.username;
+            description = usrCfg.description;
+            home = "/Users/${usrCfg.username}";
+            shell = usrCfg.shell;
+          }
+        ) cfg.userConfig;
+
+    # home-manager.users = mapAttrs' (
+    #   name: usrCfg:
+    #   nameValuePair (usrCfg.username) (
+    #     import ../../../../../lib/mkUserHome.nix {
+    #       inherit (usrCfg) username;
+    #       inherit pkgs;
+    #     }
+    #   )
+    # ) cfg.userConfig;
+
+    home-manager.users = mapAttrs' (
+      name: usrCfg:
+      nameValuePair (usrCfg.username) (
+        mkIf usrCfg.enable {
+          imports = [
+            ../../../../../home/shared/dots.nix
+            ../../../../../home/shared/programs.nix
+            ../../../../../home/shared/sops-home.nix
+            ../../../../../home/shared/modules
+            ../../../../../home/darwin
+            ../../../../../home/nixos
+            ../../../../../users/${usrCfg.username}
+          ];
+          home = {
+            # username and home are set automatically when using home-manager as a system module
+            # username = "some_user";
+            homeDirectory = mkForce (
+              if pkgs.stdenv.isDarwin then "/Users/${usrCfg.username}" else "/home/${usrCfg.username}"
+            );
+            # make programs use XDG directories whenever supported
+            preferXdgDirectories = true;
+          };
+        }
+      )
+    ) cfg.userConfig;
+
+    # pass the primary admin user's username to other system modules
+    _module.args.username = cfg.userConfig.myadmin.username;
+  };
 }
