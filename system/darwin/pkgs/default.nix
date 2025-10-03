53ba18c2ec80ecf74bc219adf94d3d46f176fc98
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-03 22:00:49 +1000

    Add the new local kanata package to default.nix callPackage index

--- system/darwin/pkgs/default.nix
+++ system/darwin/pkgs/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   goldendictng-gh = (pkgs.callPackage ./goldendictng-gh { });
+  # install from gh commit to get recent bugfix ASAP
+  kanata-head = (pkgs.callPackage ./kanata { });
   karabiner-driverkit = (pkgs.callPackage ./karabiner-driverkit { });
   # libation-gh = (pkgs.callPackage ./libation-gh { });
   stacher = (pkgs.callPackage ./stacher { });
