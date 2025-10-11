a5052dddcefcb8675e576e1573e89a07d50cfa89
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-11 17:15:20 +1100

    Remove configuration file for the now removed determinate-nixd

--- system/darwin/macos.nix
+++ system/darwin/macos.nix
@@ -37,22 +37,6 @@
   };
 
   environment.etc = {
-    "nix/nix.custom.conf".text = # bash
-      ''
-        # import this file into nix.conf (set "access-tokens" key to my GitHub token to get a higher API threshold for rate-limiting)
-        # see https://github.com/NixOS/nix/issues/6536 for more details
-        !include ${config.home-manager.users.${username}.sops.secrets."users/${username}/github_token".path}
-        # automatically detects files in the store that have identical contents, and replaces them with hard links to a single copy
-        auto-optimise-store = true
-        warn-dirty = false
-        lazy-trees = true
-        trusted-users = [ "@staff" "root" ]
-        extra-substituters = https://nixpkgs.cachix.org https://nix-community.cachix.org https://cachix.cachix.org https://yazi.cachix.org https://mettavi.cachix.org  
-        extra-trusted-public-keys = nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k= mettavi.cachix.org-1:rYvLGZOMT4z4hK7u5pzrbt8svJ/2JcUA/PTa1bQx4FU=
-        extra-nix-path =  nixpkgs-overlays=${
-          config.users.users.${username}.home
-        }/${nix_repo}/system/overlays
-      '';
     "newsyslog.d/restic.conf".text =
       lib.mkIf config.home-manager.users.${username}.mettavi.shell.restic.enable # bash
         ''
