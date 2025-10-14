e2c5c9117d4dc5615f222658843ae61dcb273163
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-15 02:04:02 +1100

    Add new nixos module for kanata

---
+++ system/shared/modules/system/kanata/default.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.mettavi.system.shell.kanata;
+in
+{
+  options.mettavi.system.shell.kanata = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install kanata, a tool to improve keyboard usability with advanced customization, on NixOS";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.kanata = {
+      enable = true;
+      keyboards = {
+        "MBP" = {
+          config = ''
+             (defsrc
+              esc f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12
+              caps a s d f h j k l ;
+            )
+
+            (defvar
+              tap-time 150
+              hold-time 200
+            )
+
+            (defalias
+              nav (layer-while-held navigation)
+              cw (caps-word 5000)
+              escnav (tap-hold 100 100 esc @nav)
+              ;; trigger a tap when rolling within the timeout (unless second key is released first)
+              ;; don't trigger hold for keys from same side ("bilateral combinations")
+              a (tap-hold-except-keys $tap-time $hold-time a lalt (q w e r t a s d f g z x c v b))
+              s (tap-hold-except-keys $tap-time $hold-time s lmet (q w e r t a s d f g z x c v b))
+              d (tap-hold-except-keys $tap-time $hold-time d lsft (q w e r t a s d f g z x c v b))
+              f (tap-hold-except-keys $tap-time $hold-time f lctl (q w e r t a s d f g z x c v b))
+              j (tap-hold-except-keys $tap-time $hold-time j rctl (y u i o p h j k l ; n m , . /))
+              k (tap-hold-except-keys $tap-time $hold-time k rsft (y u i o p h j k l ; n m , . /))
+              l (tap-hold-except-keys $tap-time $hold-time l rmet (y u i o p h j k l ; n m , . /))
+              ;; the right-option key must be set to ESC+ in iterm/terminal settings for this mapping to work                                                       
+              ; (tap-hold-except-keys $tap-time $hold-time ; ralt (y u i o p h j k l ; n m , . /))
+            )
+
+            (deflayer base
+              @cw brdn  brup  _    _    _    _   prev  pp  next  mute  vold  volu
+              @escnav @a @s @d @f _ @j @k @l @;
+            )
+
+            (deflayer navigation
+              @cw f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12
+              _ _ _ _ _ left down up rght _
+            )
+          '';
+          extraDefCfg = "process-unmapped-keys yes";
+        };
+      };
+    };
+  };
+}
