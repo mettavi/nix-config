02d1174abf81bf6e9a0e2a88326d3e8f91b1f2b6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-23 16:29:16 +1100

    Add system and user modules for firefox

---
+++ home/shared/modules/apps/firefox/default.nix
@@ -0,0 +1,253 @@
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.mettavi.apps.firefox;
+in
+{
+  options.mettavi.apps.firefox = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install and configure firefox";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.firefox = {
+      enable = true;
+      profiles = {
+        "mettavi" = {
+          id = 0;
+          name = "default";
+          isDefault = true;
+          bookmarks = [
+            {
+              name = "wikipedia";
+              tags = [ "wiki" ];
+              keyword = "wiki";
+              url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+            }
+          ];
+          extensions = {
+            packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+              bitwarden
+              sponsorblock
+            ];
+          };
+          settings = {
+            # ALPHABETICAL
+            "app.normandy.first_run" = false;
+            "app.shield.optoutstudies.enabled" = false;
+            # disable updates (pretty pointless with nix)
+            "app.update.channel" = "default";
+            "browser.aboutConfig.showWarning" = false;
+            "browser.compactmode.show" = true;
+            "browser.cache.disk.enable" = false; # Be kind to hard drive
+            "browser.ctrlTab.recentlyUsedOrder" = true;
+            "browser.download.start_downloads_in_tmp_dir" = true;
+            "browser.download.useDownloadDir" = false; # Don't ask for download dir
+            "browser.download.viewableInternally.typeWasRegistered.svg" = true;
+            "browser.download.viewableInternally.typeWasRegistered.webp" = true;
+            "browser.download.viewableInternally.typeWasRegistered.xml" = true;
+            # "browser.startup.homepage" = "https://duckduckgo.com";
+            "browser.startup.homepage" = "about:home";
+            "browser.search.defaultenginename" = "DuckDuckGo";
+            "browser.search.order.1" = "DuckDuckGo";
+            "browser.search.region" = "PL";
+            "browser.search.widget.inNavBar" = true;
+            "browser.tabs.inTitlebar" = 0;
+            "browser.tabs.loadInBackground" = true;
+            "browser.toolbars.bookmarks.visibility" = "newtab";
+            "browser.urlbar.placeholderName" = "DuckDuckGo";
+            "browser.urlbar.showSearchSuggestionsFirst" = false;
+            # disable all the annoying quick actions
+            "browser.urlbar.quickactions.enabled" = false;
+            "browser.urlbar.quickactions.showPrefs" = false;
+            "browser.urlbar.shortcuts.quickactions" = false;
+            "browser.urlbar.suggest.quickactions" = false;
+
+            "browser.contentblocking.category" = "strict"; # "standard"
+            "browser.laterrun.enabled" = false;
+            # whether user has seen the initial introductory message for the Enhanced Tracking Protection panel
+            "browser.protections_panel.infoMessage.seen" = true;
+            "browser.helperApps.deleteTempFileOnExit" = true;
+            "distribution.searchplugins.defaultLocale" = "en-AU";
+            "general.autoScroll" = true;
+            "general.useragent.locale" = "en-AU";
+
+            # DISABLE FIREFOX ACCOUNTS
+            # "identity.fxaccounts.enabled" = false;
+
+            "mousewheel.default.delta_multiplier_x" = 20;
+            "mousewheel.default.delta_multiplier_y" = 20;
+            "mousewheel.default.delta_multiplier_z" = 20;
+
+            # "print.print_footerleft" = "";
+            # "print.print_footerright" = "";
+            # "print.print_headerleft" = "";
+            # "print.print_headerright" = "";
+
+            "privacy.donottrackheader.enabled" = true;
+
+            # VERTICAL TABS
+            "sidebar.verticalTabs" = true;
+            "sidebar.revamp" = true;
+            "sidebar.main.tools" = [
+              "history"
+              "bookmarks"
+            ];
+            "signon.rememberSignons" = false; # Disable "save password" prompt
+            # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+            # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+            # then have Firefox open on some other workspace.
+            "widget.disable-workspace-management" = true;
+            "widget.use-xdg-desktop-portal.file-picker" = 1;
+
+            # DISABLE IRRITATING FIRST-RUN STUFF
+            "browser.bookmarks.restore_default_bookmarks" = false;
+            "browser.bookmarks.addedImportButton" = true;
+            "browser.disableResetPrompt" = true;
+            "browser.download.panel.shown" = true;
+            "browser.feeds.showFirstRunUI" = false;
+            "browser.messaging-system.whatsNewPanel.enabled" = false;
+            "browser.rights.3.shown" = true;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.shell.defaultBrowserCheckCount" = 1;
+            "browser.startup.homepage_override.mstone" = "ignore";
+            "browser.uitour.enabled" = false;
+            "trailhead.firstrun.didSeeAboutWelcome" = true;
+
+            # DISABLE SOME TELEMETRY
+            "browser.discovery.enabled" = false;
+            "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+            "browser.newtabpage.activity-stream.telemetry" = false;
+            "browser.ping-centre.telemetry" = false;
+            "datareporting.healthreport.service.enabled" = false;
+            "datareporting.healthreport.uploadEnabled" = false;
+            "datareporting.policy.dataSubmissionEnabled" = false;
+            "datareporting.sessions.current.clean" = true;
+            "devtools.onboarding.telemetry.logged" = false;
+            "toolkit.telemetry.archive.enabled" = false;
+            "toolkit.telemetry.bhrPing.enabled" = false;
+            "toolkit.telemetry.enabled" = false;
+            "toolkit.telemetry.firstShutdownPing.enabled" = false;
+            "toolkit.telemetry.hybridContent.enabled" = false;
+            "toolkit.telemetry.newProfilePing.enabled" = false;
+            "toolkit.telemetry.prompted" = 2;
+            "toolkit.telemetry.rejected" = true;
+            "toolkit.telemetry.reportingpolicy.firstRun" = false;
+            "toolkit.telemetry.server" = "";
+            "toolkit.telemetry.shutdownPingSender.enabled" = false;
+            "toolkit.telemetry.unified" = false;
+            "toolkit.telemetry.unifiedIsOptIn" = false;
+            "toolkit.telemetry.updatePing.enabled" = false;
+
+            # Harden true; # Force enable GPU acceleration
+            "media.ffmpeg.vaapi.enabled" = true;
+            "media.hardware-video-decoding.force-enabled" = true;
+            "widget.dmabuf.force-enabled" = true; # Required in recent Firefoxes
+
+            # DISABLE CRAPPY HOME ACTIVITY STREAM PAGE
+            "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+            "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+            "browser.newtabpage.activity-stream.feeds.snippets" = false;
+            "browser.newtabpage.activity-stream.feeds.topsites" = false;
+            "browser.newtabpage.activity-stream.showSponsored" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+            "browser.newtabpage.blocked" = lib.genAttrs [
+              # Facebook
+              "4gPpjkxgZzXPVtuEoAL9Ig=="
+              # Amazon
+              "K00ILysCaEq8+bEqV/3nuw=="
+              # Twitter
+              "T9nJot5PurhJSy8n038xGA=="
+            ] (_: 1);
+
+          };
+          search = {
+            # force replace the existing search configuration. This is recommended since Firefox will
+            # replace the symlink for the search configuration on every launch
+            force = true;
+            default = "ddg"; # DuckDuckGo
+            engines = {
+              nix-packages = {
+                name = "Nix Packages";
+                urls = [
+                  {
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
+              };
+
+              nix-options = {
+                name = "Nix Options";
+                urls = [
+                  {
+                    template = "https://search.nixos.org/options";
+                    params = [
+                      {
+                        name = "channel";
+                        value = "unstable";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }
+                ];
+                icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@no" ];
+              };
+            };
+
+            home-manager = {
+              name = "hm Options";
+              urls = [ { template = "https://home-manager-options.extranix.com/?query={searchTerms}"; } ];
+              iconMapObj."16" = "https://home-manager-options.extranix.com/images/favicon.png";
+              definedAliases = [ "@hm" ];
+            };
+
+            nixos-wiki = {
+              name = "NixOS Wiki";
+              urls = [ { template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; } ];
+              iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+              definedAliases = [ "@nw" ];
+            };
+
+            bing.metaData.hidden = true; # hide the bing search engine
+            google.metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+          order = [
+            "ddg"
+            "google"
+            "nix-packages"
+            "nix-options"
+            "nixos-wiki"
+            "home-manager"
+          ];
+          privateDefault = "ddg"; # DuckDuckGo
+        };
+      };
+    };
+  };
+}
---
+++ system/shared/modules/system/firefox/default.nix
@@ -0,0 +1,204 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  withFirefox = (
+    builtins.any (config: config.mettavi.apps.firefox.enable) (
+      builtins.attrValues config.home-manager.users
+    )
+  );
+  lock-false = {
+    Value = false;
+    Status = "locked";
+  };
+  lock-true = {
+    Value = true;
+    Status = "locked";
+  };
+  lock-empty-string = {
+    Value = "";
+    Status = "locked";
+  };
+in
+{
+  programs.firefox = {
+    enable = withFirefox;
+    languagePacks = [ "en-US" ];
+
+    # Check about:policies#documentation for options.
+    policies = {
+      DisableFirefoxStudies = true;
+      DisableTelemetry = true;
+      DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "never"
+      DisplayMenuBar = "default-off"; # (click ALT to turn on temporarily) Alternatives: "always", "never", or "default-on"
+      DisablePocket = true;
+      DontCheckDefaultBrowser = true;
+      EnableTrackingProtection = {
+        Category = "strict";
+        Cryptomining = true;
+        EmailTracking = true;
+        Fingerprinting = true;
+        SuspectedFingerprinting = true;
+        Value = true;
+      };
+      # ---- EXTENSIONS ----
+      # Check about:support for extension/add-on ID strings.
+      # Valid strings for installation_mode are "allowed", "blocked",
+      # "force_installed" and "normal_installed".
+      ExtensionUpdate = true;
+      ExtensionSettings =
+        let
+          moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+        in
+        {
+          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+          "uBlock0@raymondhill.net" = {
+            install_url = moz "ublock-origin";
+            installation_mode = "force_installed";
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = moz "bitwarden-password-manager";
+            installation_mode = "force_installed";
+          };
+          "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            install_url = moz "privacy-badger-17";
+            installation_mode = "force_installed";
+          };
+          "extension@tabliss.io" = {
+            install_url = moz "tabliss-2.6.0.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+      # Extensions settings (NB: Few extensions support this key)
+      "3rdparty".Extensions = {
+        "uBlock0@raymondhill.net".adminSettings = {
+          userSettings = rec {
+            uiTheme = "dark";
+            uiAccentCustom = true;
+            uiAccentCustom0 = "#8300ff";
+            advancedUserEnabled = true;
+            cloudStorageEnabled = lib.mkDefault true;
+            largeMediaSize = 500;
+            popupPanelSections = 31;
+
+            importedLists = [
+              "https:#filters.adtidy.org/extension/ublock/filters/3.txt"
+              "https:#github.com/DandelionSprout/adfilt/raw/master/LegitimateURLShortener.txt"
+            ];
+
+            externalLists = lib.concatStringsSep "\n" importedLists;
+          };
+
+          selectedFilterLists = [
+            "user-filters"
+            "ublock-filters"
+            "ublock-badware"
+            "ublock-privacy"
+            "ublock-quick-fixes"
+            "ublock-unbreak"
+            "easylist"
+            "adguard-generic"
+            "adguard-mobile"
+            "easyprivacy"
+            "adguard-spyware-url"
+            "block-lan"
+            "urlhaus-1"
+            "plowe-0"
+            "dpollock-0"
+            "fanboy-cookiemonster"
+            "ublock-cookies-easylist"
+            "adguard-cookies"
+            "ublock-cookies-adguard"
+            "fanboy-social"
+            "adguard-social"
+            "fanboy-thirdparty_social"
+            "easylist-chat"
+            "easylist-newsletters"
+            "easylist-notifications"
+            "easylist-annoyances"
+            "adguard-mobile-app-banners"
+            "adguard-other-annoyances"
+            "adguard-popup-overlays"
+            "adguard-widgets"
+            "ublock-annoyances"
+            "https://filters.adtidy.org/extension/ublock/filters/3.txt"
+          ];
+        };
+      };
+      FirefoxHome = {
+        Highlights = false;
+        Pocket = false;
+        Search = true;
+        Snippets = false;
+        SponsoredPocket = false;
+        SponsoredStories = false;
+        SponsoredTopSites = false;
+        Stories = false;
+        TopSites = true;
+      };
+      FirefoxSuggest = {
+        ImproveSuggest = false;
+        SponsoredSuggestions = false;
+        WebSuggestions = false;
+      };
+      HardwareAcceleration = true;
+      Homepage.StartPage = "previous-session";
+      HttpsOnlyMode = "enabled";
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      # If the value is an empty string (“”), the first run page is not displayed.
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      PasswordManagerEnabled = false;
+      PictureInPicture.Enabled = false;
+      PopupBlocking.Default = true;
+      # Check about:config for options.
+      Preferences = {
+        # Privacy settings
+        "browser.contentblocking.category" = {
+          Value = "strict";
+          Status = "locked";
+        };
+        "browser.formfill.enable" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        "browser.newtabpage.activity-stream.system.showWeather" = false;
+        "browser.newtabpage.activity-stream.topSitesRows" = 2;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+        "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+        "browser.newtabpage.pinned" = lock-empty-string;
+        "browser.search.suggest.enabled" = lock-false;
+        "browser.search.suggest.enabled.private" = lock-false;
+        "browser.tabs.firefox-view" = false;
+        "browser.tabs.tabmanager.enabled" = false;
+        "browser.topsites.contile.enabled" = lock-false;
+        "browser.urlbar.suggest.searches" = lock-false;
+        "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+        "extensions.pocket.enabled" = lock-false;
+        "extensions.screenshots.disabled" = lock-true;
+        "general.autoScroll" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+      };
+      PrimaryPassword = false;
+      PromptForDownloadLocation = false;
+      RequestedLocales = "en-AU,en-US";
+      SearchBar = "unified"; # alternative: "separate"
+      SearchSuggestEnabled = true;
+      ShowHomeButton = true;
+      SkipTermsOfUse = true;
+      TranslateEnabled = true;
+      UserMessaging = {
+        SkipOnboarding = true;
+        UrlbarInterventions = false;
+      };
+
+    };
+  };
+}
