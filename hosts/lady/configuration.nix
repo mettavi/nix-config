1397cebacc446d8fa5d10ceb9ab81829a7e8e527
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-23 16:44:29 +1100

    Disable grub to set up the refind module

--- hosts/lady/configuration.nix
+++ hosts/lady/configuration.nix
@@ -56,10 +56,14 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = false;
   # the installation process is allowed to modify EFI boot variables
   # (enabling this is not recommended on T2 macs)
   boot.loader.efi.canTouchEfiVariables = false;
   boot.loader.efi.efiSysMountPoint = "/boot";
+
+  # this is required to set up the refind module
+  boot.loader.grub.enable = false;
   boot.loader.refind = {
     enable = true;
     efiInstallAsRemovable = true;
