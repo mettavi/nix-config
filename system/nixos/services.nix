35e9efced1431dd8e240f0c1c6e99e1ea80dd849
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-24 19:18:26 +1100

    Enhance comment

--- system/nixos/services.nix
+++ system/nixos/services.nix
@@ -8,7 +8,7 @@
     # not recommended for T2 macs, see https://wiki.t2linux.org/guides/audio-config
     # pulse.enable = true;
   };
-  # used by pipewire
+  # allow pipewire to have realtime priority
   security.rtkit.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
