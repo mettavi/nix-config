7768cac60f0d0f9d4810d16374864b32f712528d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-24 12:23:45 +1100

    Move firefox module to nixos directory (firefox settings are not available for nix-darwin)

--- system/nixos/modules/system/firefox/default.nix
+++ system/nixos/modules/system/firefox/default.nix
@@ -202,10 +202,12 @@ in
 
     };
   };
-  environment.sessionVariables = lib.mkIf config.services.displayManager.gdm.wayland {
-    # for firefox to run on wayland (may be default now?)
-    MOZ_ENABLE_WAYLAND = "1";
-  };
+  environment.variables =
+    lib.mkIf (pkgs.stdenv.isLinux && config.services.displayManager.gdm.wayland)
+      {
+        # for firefox to run on wayland (may be default now?)
+        MOZ_ENABLE_WAYLAND = "1";
+      };
 
   # required for screensharing functionality
   xdg.portal = {
