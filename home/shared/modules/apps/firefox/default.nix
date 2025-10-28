5a47746bba3bdd9edc70dcc5a59f667b849b701f
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-28 17:52:48 +1100

    Remove xdg desktop portal settings, which have been moved to a nixos module

--- home/shared/modules/apps/firefox/default.nix
+++ home/shared/modules/apps/firefox/default.nix
@@ -478,24 +478,5 @@ in
         };
       };
     };
-    # set gtk as the default interface
-    xdg.portal = {
-      enable = true;
-      config = lib.mkForce {
-        common = {
-          default = [
-            "gtk"
-          ];
-        };
-      };
-      extraPortals =
-        with pkgs;
-        # required for screensharing functionality on Wayland
-        lib.optionals nixosConfig.services.displayManager.gdm.wayland [
-          xdg-desktop-portal-gtk # Use this for GNOME (may already be default)
-          # xdg-desktop-portal-wlr # Use this for Sway/wlroots
-          # xdg-desktop-portal-kde  # Use this for KDE
-        ];
-    };
   };
 }
