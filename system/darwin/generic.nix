89c776a6a42e931c8633bf5f90ac78a3c703ef5d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-28 18:40:01 +1100

    Remove temporary overlay to workaround error which is now fixed upstream

--- system/darwin/generic.nix
+++ system/darwin/generic.nix
@@ -1,5 +1,4 @@
 {
-  inputs,
   pkgs,
   ...
 }:
@@ -10,22 +9,7 @@
   };
   nixpkgs = {
     overlays = [
-      # install from PR https://github.com/NixOS/nixpkgs/pull/451386 until it is merged to nixpkgs-unstable
-      # TODO: Check status at https://nixpk.gs/pr-tracker.html?pr=451386
-      (
-        self: super:
-        (
-          let
-            nixpkgs-rubyfix = import inputs.nixpkgs-rubyfix {
-              inherit (self) system;
-            };
-          in
-          {
-            ruby = nixpkgs-rubyfix.ruby;
-          }
-        )
-      )
-    ]
-    ++ [ (import ../overlays/darwin/default.nix) ];
+      (import ../overlays/darwin/default.nix)
+    ];
   };
 }
