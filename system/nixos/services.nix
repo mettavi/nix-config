b850c64a10ebbc5f8fb172632152d9ddf6913a3b
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-28 16:40:28 +1100

    Make the wireplumber service for pipewire audio explicit

--- system/nixos/services.nix
+++ system/nixos/services.nix
@@ -1,12 +1,13 @@
 { lib, ... }:
 {
-  # Enable sound with pipewire. Wireplumber is automatically enabled if you use pipewire.
+  # Enable sound with pipewire.
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     # allow applications (like Firefox) that expect PulseAudio to function correctly using PipeWire's implementation.
     pulse.enable = true; # pulseaudio emulation
+    wireplumber.enable = true; # make the default explicit
   };
 
   # pulseaudio is not recommended for T2 macs, see https://wiki.t2linux.org/guides/audio-config
