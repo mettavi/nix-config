d5faf612ae252cb17431c30e971f343225cff4b6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-29 15:03:30 +1100

    Add vulkan graphics library and tools

--- system/nixos/hardware.nix
+++ system/nixos/hardware.nix
@@ -6,6 +6,9 @@
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       libvdpau-va-gl
+      # modern graphics API that provides improved performance and better control over graphics hardware
+      vulkan-loader
+      vulkan-tools
     ];
   };
   environment.sessionVariables = {
