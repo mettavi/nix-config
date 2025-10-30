d537d8b31b1d187cbdd29fe2863da7d464456196
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-10-30 13:54:43 +1100

    Keep comment disabled as documentation for default option

--- home/shared/modules/apps/firefox/default.nix
+++ home/shared/modules/apps/firefox/default.nix
@@ -21,7 +21,7 @@ in
 
   config = mkIf cfg.enable {
     home.sessionVariables = lib.mkIf nixosConfig.services.displayManager.gdm.wayland {
-      # MOZ_ENABLE_WAYLAND = "1"; # Explicitly enables Wayland for Firefox (may already be default)
+      # MOZ_ENABLE_WAYLAND = "1"; # Explicitly enables Wayland for Firefox (enabled by default)
       # NIXOS_OZONE_WL = "1"; # Forces Wayland backend for applications using Ozone
       # take advantage of more video codecs supported by a IGP/GPU, specifies the preferred rendering device
       MOZ_DRM_DEVICE = "/dev/dri/renderD128";
