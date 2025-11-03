1fa94de8cae690cdf522a830bf28bf82cda8accf
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-03 18:12:11 +1100

    Replace the system tray gnome extension with 'appindicator'

--- home/nixos/modules/desktop/gnome/default.nix
+++ home/nixos/modules/desktop/gnome/default.nix
@@ -17,17 +17,17 @@ in
       # GSettings editor for GNOME.
       (with pkgs; [ dconf-editor ])
       ++ (with pkgs.gnomeExtensions; [
-        app-icons-taskbar
-        # Allow Solaar to support certain features on non X11 systems
-        solaar-extension
+        # app-icons-taskbar
+        appindicator # Adds AppIndicator, KStatusNotifierItem and legacy Tray icons support to the Shell.
+        solaar-extension # Allow Solaar to support certain features on non X11 systems (eg. rules)
       ]);
     dconf.settings = {
       "org/gnome/shell" = {
         disable-user-extensions = false;
         # `gnome-extensions list` for a list
         enabled-extensions = [
-          # "aztaskbar@aztaskbar.gitlab.com" # app-icons-taskbar
           "solaar-extension@sidevesh"
+          "appindicatorsupport@rgcjonas.gmail.com"
         ];
       };
       "org/gnome/desktop/peripherals/keyboard" = {
