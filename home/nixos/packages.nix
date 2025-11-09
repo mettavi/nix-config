50f19d0415c0636371306d053b3febe51a4f3cc8
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-09 14:32:35 +1100

    Install simple image manipulation app

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    switcheroo # App for converting (and resizing) images between different formats (uses imagemagick)
     wasistlos # Unofficial WhatsApp desktop application for linux
   ];
 }
