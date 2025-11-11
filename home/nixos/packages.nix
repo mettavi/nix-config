aa14e83e849b2344dcfaf87b29657b0ec5de5313
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-11 15:22:25 +1100

    Install zoom app on linux

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -3,5 +3,6 @@
   home.packages = with pkgs; [
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
     wasistlos # Unofficial WhatsApp desktop gtk application for linux
+    zoom-us # video conferencing application
   ];
 }
