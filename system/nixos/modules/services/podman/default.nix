e4518166005d54a14c16134206f39d7678ff510e
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-18 22:48:17 +1100

    Add module to setup podman on the system

---
+++ system/nixos/modules/services/podman/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.mettavi.system.services.podman;
+in
+{
+  options.mettavi.system.services.podman = {
+    enable = lib.mkEnableOption "Install and set up nixos podman server";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Enable common container config files in /etc/containers
+    virtualisation.containers.enable = true;
+    virtualisation.podman = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        flags = [ "--all" ];
+      };
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
+  };
+}
