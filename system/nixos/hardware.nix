d1998012f3073735c85f907e42ef36d1294c03a0
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-19 15:48:22 +1100

    Add support in nixos for basic and driverless scanning

--- system/nixos/hardware.nix
+++ system/nixos/hardware.nix
@@ -1,15 +1,25 @@
 { pkgs, ... }:
 {
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      libvdpau-va-gl
-      # modern graphics API that provides improved performance and better control over graphics hardware
-      vulkan-loader
-      vulkan-tools
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        libvdpau-va-gl
+        # modern graphics API that provides improved performance and better control over graphics hardware
+        vulkan-loader
+        vulkan-tools
+      ];
+    };
+    sane = {
+      # Enable support for SANE scanners
+      enable = true;
+      extraBackends = [
+        # Apple AirScan and Microsoft WSD support, for driverless scanning (supports many vendors/devices)
+        pkgs.sane-airscan
+      ];
+    };
   };
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD"; # Force intel-media-driver
