83831f27fd69216b75d0919a22bd8015612f8e7d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-24 19:53:17 +1100

    Set the default terminal for the primary user

--- hosts/lady/configuration.nix
+++ hosts/lady/configuration.nix
@@ -186,6 +186,14 @@
         "x-scheme-handler/unknown" = "firefox.desktop";
       };
     };
+    xdg.terminal-exec = {
+      enable = true;
+      settings = {
+        default = [
+          "ghostty.desktop"
+        ];
+      };
+    };
   };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
