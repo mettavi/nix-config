f3fba8d36d0fb442223d253fda86d96501e56f2c
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-28 20:43:21 +1100

    Set the stateVersion for nixos and home-manager to the correct value

--- hosts/lady/configuration.nix
+++ hosts/lady/configuration.nix
@@ -156,7 +156,7 @@
         })
       ];
       # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-      stateVersion = "25.11";
+      stateVersion = "25.05";
     };
     mettavi = {
       apps = {
@@ -201,7 +201,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
