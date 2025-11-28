ddeecc2b70617da8bdd7b6ebf7d027e70321d1d6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-29 01:01:48 +1100

    Add comment to explain how to use the Github PAT

--- flake.nix
+++ flake.nix
@@ -57,7 +57,8 @@
     secrets = {
       # use this address with ssh keys after initial setup
       url = "git+ssh://git@github.com/mettavi/nix-secrets.git?ref=main&shallow=1";
-      # use this address with a GH personal access token for initial install
+      # use this address with a GH personal access token (PAT) for initial install
+      # NB: add the PAT directly to the nix.conf file with "access-tokens = github.com=<PAT>"
       # url = "github:mettavi/nix-secrets";
       inputs = { };
     };
