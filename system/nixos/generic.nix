d8020c54eb65615996dece40080bc8dba83cd052
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-28 16:44:57 +1100

    Move comment next to code block it refers to

--- system/nixos/generic.nix
+++ system/nixos/generic.nix
@@ -11,13 +11,13 @@
 {
   nix = {
     channel.enable = false;
-    # enable automatic garbage collection and store optimisation
     extraOptions = ''
       warn-dirty = false
       # import this file into nix.conf (set "access-tokens" key to my GitHub token to get a higher API threshold for rate-limiting)
       # see https://github.com/NixOS/nix/issues/6536 for more details
       !include ${config.home-manager.users.${username}.sops.secrets."users/${username}/github_token".path}
     '';
+    # enable automatic garbage collection and store optimisation
     gc = {
       automatic = true;
       dates = "weekly";
