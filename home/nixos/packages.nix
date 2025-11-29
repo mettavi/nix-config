699eb7fef8717843a526bb1ff986f872f3ee1300
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-11-30 01:32:48 +1100

    Install backup tool for home folder

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    deja-dup # simple backup tool
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
     wasistlos # Unofficial WhatsApp desktop gtk application for linux
     zoom-us # video conferencing application
