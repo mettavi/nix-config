be6f9b7cccb8e7582f6ffb812ad5f86c1e914f0b
Mettavihari <1978429+mettavi@users.noreply.github.com>
2025-12-30 21:43:56 +1100

    Remove moved code and provide a test for a wayland specific setting

--- home/shared/modules/apps/firefox/default.nix
+++ home/shared/modules/apps/firefox/default.nix
@@ -20,13 +20,14 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.sessionVariables = lib.mkIf nixosConfig.mettavi.system.desktops.wayland {
-      # MOZ_ENABLE_WAYLAND = "1"; # Explicitly enables Wayland for Firefox (enabled by default)
-      # NIXOS_OZONE_WL = "1"; # Forces Wayland backend for applications using Ozone
+    home.sessionVariables = {
       # take advantage of more video codecs supported by an IGP/GPU, specifies the preferred rendering device
       MOZ_DRM_DEVICE = "/dev/dri/renderD128";
       # Enable xinput2 to improve touchscreen support and enable additional touchpad gestures and smooth scrolling.
       MOZ_USE_XINPUT2 = "1";
+    }
+    // lib.mkIf nixosConfig.mettavi.system.desktops.wayland {
+      MOZ_ENABLE_WAYLAND = "1"; # Explicitly enables Wayland for Firefox (may be enabled by default)
     };
 
     # enable the firefox-gnome-theme via a flake input (also see userChrome and userContent below)
