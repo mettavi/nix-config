904a30fb28dae5418225193e82711961c3f85c5c
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-11 22:17:14 +1100

    Change keybinds in nvim-treesitter-textobjects to prevent conflict with ones in keymaps.lua (map ; to :)

--- home/shared/dots/nvim/lua/mettavi/plugins/nvim-treesitter-text-objects.lua
+++ home/shared/dots/nvim/lua/mettavi/plugins/nvim-treesitter-text-objects.lua
@@ -128,16 +128,17 @@ return {
 
     local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
 
-    -- Repeat movement with ; and ,
+    -- Repeat movement with <leader>j; and ,
     -- ensure ; goes forward and , goes backward regardless of the last direction
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+    -- (changed from ";" default for compatibility with keymaps.lua)
+    vim.keymap.set({ "n", "x", "o" }, "<leader>;", ts_repeat_move.repeat_last_move_next)
     vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
 
     -- vim way: <leader>; goes to the direction you were moving (changed from ; default).
     -- vim.keymap.set({ "n", "x", "o" }, "<leader>;", ts_repeat_move.repeat_last_move)
     -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
-    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    -- Optionally, make builtin f, F, t, T also repeatable with <leader>; and ,
     vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
     vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
     vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
