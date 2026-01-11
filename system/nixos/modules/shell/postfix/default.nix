ae6f583e115ee750978a7a00ae92f57ccbff262f
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-11 16:22:37 +1100

    Fix various syntax and logical errors in the new postfix module for nixos

--- system/nixos/modules/shell/postfix/default.nix
+++ system/nixos/modules/shell/postfix/default.nix
@@ -22,21 +22,24 @@ in
   config = mkIf cfg.enable {
     services.postfix = {
       enable = true;
-      relayHost = "smtp.gmail.com";
-      relayPort = 587;
-      config = {
-        smtp_use_tls = "yes";
+      enableSubmission = true;
+      enableSubmissions = true;
+      settings.main = {
+        hostname = "mail.${config.networking.hostName}";
+        relayhost = [ "[smtp.gmail.com]:587" ];
         smtp_sasl_auth_enable = "yes";
-        smtp_sasl_security_options = "";
         # using texthash instead of hash simplifies the setup on nixos
-        smtp_sasl_password_maps = "texthash:/etc/postfix/sasl_passwd";
-        # optional: Forward mails to root (e.g. from cron jobs, smartd) to an email address
-        virtual_alias_maps = "inline:{ {${email}} }";
+        smtp_sasl_password_maps = "texthash:${config.sops.templates."sasl_passwd".path}";
+        smtp_sasl_security_options = "";
+        smtp_use_tls = "yes";
       };
+      # optional: Forward mails to root (e.g. from cron jobs, smartd) to an email address
+      virtual = ''
+        inline:{ {root=${email}} }
+      '';
     };
     sops = {
       secrets."users/${username}/postfix_gmail-oona" = {
-        owner = config.services.postfix.user;
       };
       templates = {
         # config file to allow postfix to use my personal gmail account automatically
@@ -45,6 +48,7 @@ in
             ''
               [smtp.gmail.com]:587 ${email}:${config.sops.placeholder."users/${username}/postfix_gmail-oona"}
             '';
+          mode = "0600";
           path = "/etc/postfix/sasl_passwd";
         };
       };
