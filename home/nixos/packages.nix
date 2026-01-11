95ed7cc8b0b96753300fcc11e900ca6a5560ab78
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-11 22:32:18 +1100

    Tweak comment

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    deja-dup # simple backup tool
+    deja-dup # simple backup tool based on restic (special backup format)
     mpv # General-purpose media player, fork of MPlayer and mplayer2
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
     wasistlos # Unofficial WhatsApp desktop gtk application for linux
