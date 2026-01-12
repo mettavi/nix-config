19b7ec4f163c85ada3563c56694392964a0c0980
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-13 03:05:35 +1100

    Move osConfig variable into the home-manager scope and reorganise code layout of the calibre module

--- system/shared/modules/apps/calibre/default.nix
+++ system/shared/modules/apps/calibre/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  osConfig,
   pkgs,
   username,
   ...
@@ -17,39 +16,41 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home-manager.users.${username} = {
-      dconf.settings = lib.mkIf osConfig.mettavi.system.desktops.gnome.enable {
-        "org/gnome/desktop/app-folders" = {
-          folder-children = [
-            "Calibre"
-          ];
+    home-manager.users.${username} =
+      { osConfig, ... }:
+      {
+        dconf.settings = lib.mkIf osConfig.mettavi.system.desktops.gnome.enable {
+          "org/gnome/desktop/app-folders" = {
+            folder-children = [
+              "Calibre"
+            ];
+          };
+          "org/gnome/desktop/app-folders/folders/Calibre" = {
+            name = "Calibre";
+            apps = [
+              "calibre-gui.desktop"
+              "CaliSync.desktop"
+              "calibre-ebook-viewer.desktop"
+              "calibre-ebook-edit.desktop"
+              "calibre-lrfviewer.desktop"
+            ];
+            translate = false;
+          };
         };
-        "org/gnome/desktop/app-folders/folders/Calibre" = {
-          name = "Calibre";
-          apps = [
-            "calibre-gui.desktop"
-            "CaliSync.desktop"
-            "calibre-ebook-viewer.desktop"
-            "calibre-ebook-edit.desktop"
-            "calibre-lrfviewer.desktop"
-          ];
-          translate = false;
+        home.packages = with pkgs; [
+          # Comprehensive e-book software
+          (calibre.override {
+            # to open .cbr and .cbz files
+            unrarSupport = true;
+          })
+        ];
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "application/lrf" = "calibre-lrfviewer.desktop";
+            "application/epub+zip" = "calibre-ebook-viewer.desktop";
+          };
         };
       };
-      home.packages = with pkgs; [
-        # Comprehensive e-book software
-        (calibre.override {
-          # to open .cbr and .cbz files
-          unrarSupport = true;
-        })
-      ];
-      xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "application/lrf" = "calibre-lrfviewer.desktop";
-          "application/epub+zip" = "calibre-ebook-viewer.desktop";
-        };
-      };
-    };
   };
 }
