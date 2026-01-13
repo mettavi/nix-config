f0ad50db25c048568d8484bbc25b5c7e184eb4e6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-13 13:35:49 +1100

    Add workarounds for the kanata module on host oona (ASUS ROG laptop)

--- system/nixos/modules/shell/kanata/default.nix
+++ system/nixos/modules/shell/kanata/default.nix
@@ -1,10 +1,14 @@
 {
   config,
   lib,
+  nix_repo,
+  pkgs,
+  username,
   ...
 }:
 with lib;
 let
+  hostname = config.networking.hostName;
   cfg = config.mettavi.system.shell.kanata;
 in
 {
@@ -17,18 +21,28 @@ in
   };
 
   config = mkIf cfg.enable {
+    # set this explicitly to bypass the first-run bug, even though it is already set in the module
+    # see https://github.com/NixOS/nixpkgs/issues/317282 for details
+    hardware.uinput.enable = true;
+
     services.kanata = {
       enable = true;
       keyboards = {
-        "Asus-G14" = {
-          # An empty list, the default value, lets kanata detect which
-          # input devices are keyboards and intercept them all.
-          # devices = [
-          # Replace the paths below with the appropriate device paths for your setup.
-          # Use `ls /dev/input/by-path/` to find your keyboard devices.
-          #   "/dev/input/by-path/pci-0000:e6:00.1-usb-0:5:1.1-event-kbd"
-          #   "/dev/input/by-path/pci-0000:e6:00.1-usbv2-0:5:1.1-event-kbd"
-          # ];
+        "Asus-G14-${hostname}" = {
+          # An empty list, the default value, lets kanata detect which input devices are keyboards and intercept them all.
+          # Setting this option is necessary to automatically assign different configs to different hosts/devices
+          # NOTE: This option is not working on host oona under nixos (even as root with a minimal config - requires more testing)
+          # The workaround is to use "linux-dev-names-include" in option extraDefCfg instead
+          devices = [
+            # Replace the paths below with the appropriate device paths for your setup.
+            # Use `ls /dev/input/by-path/` to find your keyboard devices.
+            #   "/dev/input/by-path/pci-0000:e6:00.1-usb-0:5:1.1-event-kbd"
+            #   "/dev/input/by-path/pci-0000:e6:00.1-usbv2-0:5:1.1-event-kbd"
+            # Prefer `ls /dev/input/by-id/` if possible as this is more stable
+            # ASUS G24 GA403WR laptop keyboard (currently not working)
+            # "/dev/input/by-id/usb-ITE_Tech._Inc._ITE_Device_8910_-hidraw"
+            # "/dev/hidraw0"
+          ];
           config = ''
              (defsrc
               esc
@@ -67,9 +81,101 @@ in
               _ _ _ _ _ left down up rght _
             )
           '';
-          extraDefCfg = "process-unmapped-keys yes";
+          extraDefCfg = optionalString ("${hostname}" == "oona") ''
+            linux-dev-names-include (
+              "Asus Keyboard"
+            )
+            process-unmapped-keys yes
+          '';
         };
       };
     };
+    # required for preventing permissions errors when connecting to keyboard devices using the services.kanata.devices option
+    # services.udev.extraRules = ''
+    #   KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    #   SUBSYSTEM=="hidraw", ATTRS{idVendor}=="0b05", ATTRS{idProduct}=="19b6", GROUP="uinput", MODE="0660"
+    #   # KERNEL=="hidraw0", SUBSYSTEM=="hidraw", GROUP="uinput", MODE="0660"
+    # '';
+    # environment.systemPackages = with pkgs; [ kanata ];
+
+    # THIS IS A MANUAL SYSTEMD CONFIGURATION FOR TESTING
+    systemd.services.kanata-Asus-G14 = {
+      enable = false;
+      unitConfig = {
+        Description = "Kanata keyboard remapper";
+        Documentation = "https://github.com/jtroo/kanata";
+        Wants = "modprobe@uinput.service";
+        After = "modprobe@uinput.service";
+      };
+      serviceConfig = {
+        Type = "simple";
+        # User = "kanata";
+        Environment = [ "PATH=/run/current-system/sw/bin:/etc/profiles/per-user/${username}/bin" ];
+        ExecStart = "${pkgs.kanata}/bin/kanata --quiet --cfg ${
+          config.users.users.${username}.home
+        }/${nix_repo}/system/nixos/modules/shell/kanata/kanata.lsp";
+        Restart = "no";
+
+        # REMOVE ALL HARDENING OPTIONS TO TEST WITH A MINIMAL CONFIG
+        # Security
+        # CapabilityBoundingSet = "";
+        # DeviceAllow = [
+        #   "/dev/uinput rw"
+        #   "char-input"
+        #   "/dev/stdin"
+        #   "/dev/input/by-id/usb-ITE_Tech._Inc._ITE_Device_8910_-hidraw r"
+        #   "/dev/hidraw0 r"
+        # ];
+        # DevicePolicy = "strict";
+        # PrivateDevices = true;
+        # BindPaths = [ "/dev/uinput" ];
+        # BindReadOnlyPaths = [
+        #   "/dev/stdin"
+        #   "/dev/input/"
+        #   "${config.users.users.${username}.home}/${nix_repo}/"
+        # ];
+        # InaccessiblePaths = "/dev/shm";
+        # LockPersonality = true;
+        # NoNewPrivileges = true;
+        # PrivateTmp = true;
+        # PrivateNetwork = true;
+        # PrivateUsers = true;
+        # The following (ProtectClock) can not be enabled, otherwise Kanata can not open /dev/uinput.
+        # More hardening would require to explicitly list allowed system calls.
+        #ProtectClock=true
+        # ProtectHome = true;
+        # ProtectHostname = true;
+        # ProtectKernelTunables = true;
+        # ProtectKernelModules = true;
+        # ProtectKernelLogs = true;
+        # ProtectSystem = "strict";
+        # ProtectControlGroups = true;
+        # Allow only on AddressFamily and then deny it to effectively deny everything
+        # RestrictAddressFamilies = [
+        #   "AF_AX25"
+        #   "~AF_AX25"
+        # ];
+        # RestrictNamespaces = true;
+        # RuntimeDirectory = "Asus-G14";
+        # SystemCallArchitectures = "native";
+        # SystemCallErrorNumber = "EPERM";
+        # SystemCallFilter = [
+        #   "@system-service"
+        #   "~@privileged"
+        #   "~@resources"
+        # ];
+        # RemoveIPC = true;
+        # IPAddressDeny = "any";
+        # RestrictSUIDSGID = true;
+        # RestrictRealtime = true;
+        # MemoryDenyWriteExecute = true;
+        # UMask = "0077";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+    # users.users.root.extraGroups = [
+    #   "uinput"
+    #   "input"
+    # ];
   };
 }
