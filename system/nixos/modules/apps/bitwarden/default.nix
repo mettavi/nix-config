cccf8fa4462e464b534d11a697c93b44a9db76e8
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-16 16:01:26 +1100

    Fix typo

--- system/nixos/modules/apps/bitwarden/default.nix
+++ system/nixos/modules/apps/bitwarden/default.nix
@@ -83,7 +83,7 @@ in
         # install the package used for making bitwarden backups
         home.packages = with pkgs; mkIf nixosConfig.mettavi.system.apps.bitwarden.backup [ bitwarden-cli ];
 
-        sops-secrets = {
+        sops.secrets = {
           # bitwarden .env file for use with cli
           "users/${username}/bitwarden.env" = {
             sopsFile = "${secrets_path}/secrets/apps/bitwarden.yaml";
