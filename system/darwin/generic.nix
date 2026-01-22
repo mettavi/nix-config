0d28a9e36ae89bddbd720c89ed735b681a25c99e
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-22 22:14:34 +1100

    Disable channels on darwin and configure the NIX_PATH environment variable

--- system/darwin/generic.nix
+++ system/darwin/generic.nix
@@ -1,10 +1,26 @@
 {
+  config,
+  hostname,
+  inputs,
+  nix_repo,
+  options,
   pkgs,
+  username,
   ...
 }:
 {
   nix = {
     enable = true;
+    channel.enable = false;
+    # this ensures $NIX_PATH is set to an immutable location in the nix-store
+    nixPath = options.nix.nixPath.default ++ [
+      "nixpkgs=${inputs.nixpkgs}"
+      "nixpkgs-overlays=${config.users.users.${username}.home}/${nix_repo}/system/overlays/shared"
+      "darwin-config=${
+        config.users.users.${username}.home
+      }/${nix_repo}/hosts/${hostname}/configuration.nix"
+      "home-manager=${inputs.home-manager}"
+    ];
     package = pkgs.lixPackageSets.stable.lix;
     settings = {
 
