9d60d648a70b4e3239321bff7dc118a9019f4f0e
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-22 14:32:40 +1100

    Add placeholder code for adding an github access-token secret when necessary

--- system/darwin/generic.nix
+++ system/darwin/generic.nix
@@ -6,6 +6,15 @@
   nix = {
     enable = true;
     package = pkgs.lixPackageSets.stable.lix;
+    settings = {
+
+      # WARNING: This is for temporary use to authenticate while setting up the ssh keys with the sops-nix secrets system.
+      # Do not commit this to git under any circumstances!
+
+      # access-tokens = [
+      #   "github.com=<personal-access-token-here>"
+      # ];
+    };
   };
   nixpkgs = {
     overlays = [
