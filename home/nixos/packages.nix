b28ac87b1d9bd61057d64e430ced6cc7c12f05e7
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-26 01:22:55 +1100

    Install the musicbrainz picard app

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -4,6 +4,7 @@
     deja-dup # simple backup tool based on restic (special backup format)
     mpv # General-purpose media player, fork of MPlayer and mplayer2
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
+    picard # offical musicbrainz tagger
     pika-backup # Simple backups based on borg (raw files backup format)
     wasistlos # Unofficial WhatsApp desktop gtk application for linux
     zoom-us # video conferencing application
