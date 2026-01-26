67746aadea5da56c99f4e99250a7e99fec08693f
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-26 21:14:18 +1100

    Create workaround to get system authentication working in the bitwarden extension for brave browser

--- system/shared/modules/apps/brave/default.nix
+++ system/shared/modules/apps/brave/default.nix
@@ -45,6 +45,16 @@ in
         ];
         package = pkgs.brave;
       };
+      xdg.configFile = {
+        # NB: Currently, bitwarden only supports system authentication in Firefox, Chome and Edge
+        # See https://github.com/bitwarden/clients/issues/11750
+        # and https://github.com/bitwarden/clients/pull/16705
+        # Workaround: 1. create the google-chome directory to get bitwarden to create the com.8bit.bitwarden.json file
+        "google-chrome/NativeMessagingHosts/.keep".text = "";
+        # 2. link the file to the Brave directory to get system authentication working
+        "BraveSoftware/Brave-Browser/NativeMessagingHosts/com.8bit.bitwarden.json".source =
+          ./com.8bit.bitwarden.json;
+      };
     };
   };
 }
