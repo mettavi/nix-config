eb5adf2879377a1c9ef62fb36820a0efb943255a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-31 16:16:09 +1100

    Change to nixplgs-unstable to install pika-backup until bug fix is merged to nixos-unstable

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -1,11 +1,16 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
+let
+  nixpkgs-unstable = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
 {
   home.packages = with pkgs; [
     deja-dup # simple backup tool based on restic (special backup format)
     mpv # General-purpose media player, fork of MPlayer and mplayer2
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
     picard # offical musicbrainz tagger
-    pika-backup # Simple backups based on borg (raw files backup format)
+    # install from nixpkgs-unstable until pr #484174 is lands in nixos-unstable
+    # TODO: Revert when https://nixpk.gs/pr-tracker.html?pr=484174 shows the PR has landed in nixos-unstable
+    nixpkgs-unstable.pika-backup # Simple backups based on borg (raw files backup format)
     wasistlos # Unofficial WhatsApp desktop gtk application for linux
     zoom-us # video conferencing application
   ];
