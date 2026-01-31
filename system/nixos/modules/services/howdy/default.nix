4d16bcab8276edf73c148c2b963d6660969c7d00
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-01-31 23:38:08 +1100

    Add workaround to get howdy to work with polkit authentication

--- system/nixos/modules/services/howdy/default.nix
+++ system/nixos/modules/services/howdy/default.nix
@@ -17,6 +17,29 @@ in
     environment.systemPackages = with pkgs; [
       v4l-utils # video for linux (v4l) library and utilities to handle video capture, including webcams
     ];
+
+    # workaround 1 which may be superseded by the systemd.services config below
+    # see https://github.com/boltgolt/howdy/issues/1016
+    # security.pam.services = {
+    #   polkit = {
+    #     enable = true;
+    #     # https://github.com/boltgolt/howdy/issues/1016
+    #     extraConfig = ''
+    #       auth sufficient pam_howdy.so
+    #     '';
+    #   };
+    # };
+
+    # workaround 2 for problem with authentication via polkit 127
+    # see https://github.com/NixOS/nixpkgs/issues/483867
+    # and https://github.com/boltgolt/howdy/issues/1077
+    systemd.services."polkit-agent-helper@".serviceConfig = {
+      # allow rm access to video4linux group, which includes the IR camera howdy uses
+      DeviceAllow = [ "char-video4linux rw" ];
+      # allow access to physical instead of only API pseudo devices
+      PrivateDevices = "no";
+    };
+
     services.howdy = {
       enable = true;
       control = "sufficient";
