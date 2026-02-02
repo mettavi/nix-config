6f5ca3e39c6dcaaf1e3319d816abab4756965158
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-02-02 20:24:02 +1100

    Add module to decrypt and mount the luks WD SSD device when plugged in

---
+++ system/nixos/modules/devices/ssd/wdssd/default.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  secrets_path,
+  username,
+  ...
+}:
+with lib;
+let
+  cfg = config.mettavi.system.devices.wdssd;
+in
+{
+  options.mettavi.system.devices.wdssd = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Set up a luks-encrypted WD ssd to auto-unlock and automount";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.etc.crypttab = {
+      mode = "0600";
+      text = ''
+        # <volume-name> <encrypted-device> [key-file] [options]
+        nixbak UUID=c8afcf0e-1642-4b85-a74b-9b8182a7f06a ${
+          config.sops.secrets."users/${username}/wdssd.luks.key".path
+        } noauto;
+      '';
+    };
+    sops.secrets = {
+      "users/${username}/wdssd.luks.key" = {
+        format = "binary";
+        sopsFile = "${secrets_path}/secrets/wdssd.luks.key";
+      };
+    };
+  };
+}
