f0057375a05afd33ea1ddaad14ea0dbbd35ffe86
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-02-10 16:45:51 +1100

    Add module for the paperless-ngx service

---
+++ system/nixos/modules/services/paperless/default.nix
@@ -0,0 +1,68 @@
+{
+  config,
+  hostname,
+  lib,
+  secrets_path,
+  username,
+  ...
+}:
+with lib;
+let
+  cfg = config.mettavi.system.services.paperless-ngx;
+  "pngx-${hostname}Secrets".sopsFile = "${secrets_path}/secrets/hosts/${hostname}.yaml";
+in
+{
+  options.mettavi.system.services.paperless-ngx = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install and setup paperless-ngx, a documents database tool";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.paperless = {
+      enable = true;
+      address = "127.0.0.1";
+      # configure Tika and Gotenberg to process Office and e-mail files with OCR
+      configureTika = true;
+      # Configure a local PostgreSQL database server
+      database.createLocally = true;
+      dataDir = "${config.users.users.${username}.home}/.local/share/paperless";
+      environmentFile = config.sops.secrets."users/${username}/paperless-${hostname}.env".path;
+      mediaDir = "${config.users.users.${username}.home}/media/paperless";
+      # enable a workaround for document classifier timeouts
+      openMPThreadingWorkaround = true;
+      passwordFile = config.sops.secrets."users/${username}/paperless-${hostname}-pw".path;
+      port = 28981;
+      settings = {
+        # PAPERLESS_CONSUMER_ENABLE_BARCODES = true;
+        # PAPERLESS_CONSUMER_IGNORE_PATTERN = [
+        #   ".DS_STORE/*"
+        #   "desktop.ini"
+        # ];
+        # PAPERLESS_CONSUMER_RECURSIVE = true;
+        # PAPERLESS_CONSUMER_SUBDIRS_AS_TAGS = true;
+        PAPERLESS_DBHOST = "localhost";
+        PAPERLESS_DBNAME = "paperless";
+        PAPERLESS_DBUSER = "paperless";
+        # PAPERLESS_DBPASS is defined in the environmentFile (see above)
+        PAPERLESS_OCR_LANGUAGE = "eng";
+        PAPERLESS_OCR_USER_ARGS = {
+          optimize = 1;
+          pdfa_image_compression = "lossless";
+        };
+        PAPERLESS_TIME_ZONE = "Australia/Melbourne";
+        PAPERLESS_TRASH_DIR = "${config.services.paperless.mediaDir}/trash";
+        # PAPERLESS_URL = "https://paperless.example.com";
+      };
+      user = "${username}";
+    };
+    sops.secrets = {
+      "users/${username}" = {
+        "paperless-${hostname}.env" = "pngx-${hostname}Secrets";
+        "paperless-${hostname}-pw" = "pngx-${hostname}Secrets";
+      };
+    };
+  };
+}
