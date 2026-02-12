6096829d9b9a6e82ab3fe176e0c3b5f1b6f3daa4
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-02-12 14:04:52 +1100

    Prevent the paperless service from starting on boot

--- system/nixos/modules/services/paperless/default.nix
+++ system/nixos/modules/services/paperless/default.nix
@@ -63,4 +63,10 @@ in
     "users/${username}/paperless-${hostname}.env" = paperlessSecrets;
     "users/${username}/paperless-${hostname}-pw" = paperlessSecrets;
   };
+  # prevent the services from auto-starting on boot
+  systemd.services = {
+    redis-paperless.wantedBy = mkForce [ ];
+    paperless-scheduler.wantedBy = mkForce [ ];
+    # postgresql.target.wantedBy = mkForce [ ];
+  };
 }
