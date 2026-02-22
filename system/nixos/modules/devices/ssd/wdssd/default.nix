70d13490e549ff9036f0ba3945e3bcc53a8d998a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-02-22 17:22:20 +1100

    Improve documentation and syntax and add the luks disk option to the crypttab file

--- system/nixos/modules/devices/ssd/wdssd/default.nix
+++ system/nixos/modules/devices/ssd/wdssd/default.nix
@@ -12,26 +12,29 @@ in
 {
   options.mettavi.system.devices.wdssd = {
     enable = mkOption {
-      type = types.bool;
+      type = bool;
       default = false;
       description = "Set up a luks-encrypted WD ssd to auto-unlock and automount";
     };
   };
 
   config = mkIf cfg.enable {
-    # automatically unlock a luks disk with a keyfile
+    # automatically unlock the luks-encrypted disk with a keyfile
+    # NB: Linux uses the mapper name to refer to the unlocked LUKS
+    # disk (e.g. sdb)/partition (e.g. sdb1) (see /dev/mapper)
     environment.etc.crypttab = {
       mode = "0600";
       text = ''
-        # <volume-name> <encrypted-device> [key-file] [options]
-        nixbak UUID=c8afcf0e-1642-4b85-a74b-9b8182a7f06a ${
+        # <mapper-name> <encrypted-device> [key-file] [options]
+        luks-wdssd UUID=c8afcf0e-1642-4b85-a74b-9b8182a7f06a ${
           config.sops.secrets."users/${username}/wdssd.luks.key".path
-        } noauto
+        } luks noauto
       '';
     };
 
     # Create a keyfile with: dd if=/dev/urandom of=/path/to/keyfile bs=4096 count=1, and chmod 0400
     # Then add it to the device with: cryptsetup luksAddKey /dev/sdX /path/to/keyfile
+
     # NB: In order to encrypt a binary file, the example command on the sops-nix repo did not work
     # This is the successful command:
     # cat secrets/wdssd.key | sops encrypt --filename-override secrets/wdssd.luks.key \
