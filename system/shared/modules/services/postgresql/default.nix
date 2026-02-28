4c88ed5d2e5bccb97d250ccf111b7dc61c552ad7
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-02-28 12:51:38 +1100

    Add a shared system module for the postgresql db service

---
+++ system/shared/modules/services/postgresql/default.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib;
+let
+  cfg = config.mettavi.system.services.postgresql;
+in
+{
+  options.mettavi.system.services.postgresql = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install and configure the postgresql service";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      postgresql = {
+        enable = true;
+        dataDir = "/var/lib/postgresql/${config.services.postgresql.package.psqlSchema}";
+        settings = {
+          log_connections = true;
+          logging_collector = true; # Enable capturing of stderr and csvlog into log file
+          log_disconnections = true;
+          log_destination = lib.mkForce "syslog"; # stderr, csvlog, syslog, and eventlog
+          log_statement = "all"; # none, ddl, mod, all
+          log_min_messages = "debug1"; # error, warning, notice, info, debug 1..5
+        };
+      };
+    };
+  };
+}
