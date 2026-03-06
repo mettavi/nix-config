73bb84854ebf7ebcccebb742096d99830fc71372
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-06 21:47:13 +1100

    Apply a minor comment change

--- hosts/oona/hardware-config-custom.nix
+++ hosts/oona/hardware-config-custom.nix
@@ -295,7 +295,7 @@ in
   # NB: 1) Setting this by a mount option will apply the option to ALL subvolumes on the partition.
   #        This method (using systemd tmpfiles) allows it to be set per subvolume.
   #     2) This is best used on an empty directory as it only applies to NEW files.
-  #     3) Disabling COW will also disable the btrfs file integrity checksumming.
+  #     3) Disabling COW will also disable btrfs file integrity checksumming.
   systemd.tmpfiles.rules = [
     # type path mode user group (expiry)
     "h /var/lib/libvirt/images - - - - +C"
