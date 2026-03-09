56f5defb43645d4f345878ab431f0603224f6366
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-09 22:19:09 +1100

    Creat a disko option to enable/disable the disko config

--- hosts/oona/disko-config.nix
+++ hosts/oona/disko-config.nix
@@ -1,82 +1,74 @@
+{ config, lib, ... }:
+with lib;
+let
+  cfg = config.mettavi.system.devices.disko;
+in
 {
-  disko.devices.disk = {
-    nvme0n1 = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            label = "EFI";
-            name = "ESP";
-            size = "200M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/efi";
-              mountOptions = [
-                "fmask=0022"
-                "dmask=0022"
-              ];
-            };
-          };
-          nix-boot = {
-            label = "EFI";
-            name = "nix-boot";
-            size = "1024M";
-            type = "EA00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [
-                "fmask=0077"
-                "dmask=0077"
-              ];
-            };
-          };
+  options.mettavi.system.devices.disko = {
+    enable = mkEnableOption "Use disko to set up and configure this host";
+  };
 
-          cachyos = {
-            label = "cachyos";
-            name = "cachyos";
-            content = {
-              type = "filesystem";
-              format = "btrfs";
-              mountpoint = "/mnt/cachyos";
-              mountOptions = [
-                "nofail"
-                "compress=zstd"
-              ];
-            };
-          };
-
-          win11pro = {
-            label = "w11pro";
-            name = "win11pro";
-            type = "ntfs";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/mnt/win11pro";
-              mountOptions = [
-                "nofail"
-                "rw"
-                "uid=1000"
-                "windows_names"
-              ];
-            };
-          };
-
-          nixos = {
-            end = "-32G";
-            content = {
-              name = "nixos";
-              passwordFile = "/tmp/secret.key";
-              settings.allowDiscards = true;
+  config = mkIf cfg.enable {
+    disko.devices.disk = {
+      nvme0n1 = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              label = "EFI";
+              name = "ESP";
+              size = "200M";
+              type = "EF00";
               content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/efi";
+                mountOptions = [
+                  "fmask=0022"
+                  "dmask=0022"
+                ];
+              };
+            };
+            nix-boot = {
+              label = "EFI";
+              name = "nix-boot";
+              size = "1024M";
+              type = "EA00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "fmask=0077"
+                  "dmask=0077"
+                ];
+              };
+            };
+            cachyos = {
+              label = "cachyos";
+              name = "cachyos";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/mnt/cachyos";
+                mountOptions = [
+                  "nofail"
+                  "compress=zstd"
+                ];
+              };
+            };
+            nixos = {
+              # end = "-32G";
+              content = {
+                # passwordFile = "/tmp/secret.key";
+                # settings.allowDiscards = true;
                 type = "btrfs";
                 extraArgs = [ "-f" ];
+                # subvolumes = builtins.mapAttrs "subvol:subvolCfg:" {
+                #   ${subvol} = "${subvolCfg.mountpoint}";
+                # } config.mettavi.system.devices.btrfs.subvolumes;
                 subvolumes = {
                   "@root" = {
                     mountpoint = "/";
