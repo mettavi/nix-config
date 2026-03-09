ec27c4e0e4c94fda8ee9ffeca62f9cbebc2d9701
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-09 12:43:19 +1100

    Rewrite disko config for host oona to be descriptive as a starting point for a refactor

--- hosts/oona/disko-config.nix
+++ hosts/oona/disko-config.nix
@@ -9,45 +9,165 @@
           ESP = {
             label = "EFI";
             name = "ESP";
-            size = "512M";
+            size = "200M";
             type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/efi";
+              mountOptions = [
+                "fmask=0022"
+                "dmask=0022"
+              ];
+            };
+          };
+          nix-boot = {
+            label = "EFI";
+            name = "nix-boot";
+            size = "1024M";
+            type = "EA00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
               mountOptions = [
-                "defaults"
-                "umask=0077"
+                "fmask=0077"
+                "dmask=0077"
               ];
             };
           };
 
-          main = {
+          cachyos = {
+            label = "cachyos";
+            name = "cachyos";
+            content = {
+              type = "filesystem";
+              format = "btrfs";
+              mountpoint = "/mnt/cachyos";
+              mountOptions = [
+                "nofail"
+                "compress=zstd"
+              ];
+            };
+          };
+
+          win11pro = {
+            label = "w11pro";
+            name = "win11pro";
+            type = "ntfs";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/mnt/win11pro";
+              mountOptions = [
+                "nofail"
+                "rw"
+                "uid=1000"
+                "windows_names"
+              ];
+            };
+          };
+
+          nixos = {
             end = "-32G";
             content = {
-              type = "luks";
-              name = "main";
+              name = "nixos";
               passwordFile = "/tmp/secret.key";
               settings.allowDiscards = true;
               content = {
                 type = "btrfs";
-                extraArgs = ["-f"];
+                extraArgs = [ "-f" ];
                 subvolumes = {
-                  "/root" = {
+                  "@root" = {
                     mountpoint = "/";
-                    mountOptions = ["compress=zstd" "noatime"];
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
                   };
-                  "/home" = {
-                    mountpoint = "/home";
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                  "/var" = {
-                    mountpoint = "/var";
-                    mountOptions = ["compress=zstd" "noatime"];
-                  };
-                  "/nix" = {
+                  "@nix" = {
                     mountpoint = "/nix";
-                    mountOptions = ["compress=zstd" "noatime"];
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@roothome" = {
+                    mountpoint = "/root";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@vlcontainers" = {
+                    mountpoint = "/var/lib/containers";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@libvirtimgs" = {
+                    mountpoint = "/var/lib/libvirt/images";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@vlpostgres" = {
+                    mountpoint = "/var/lib/postgresql";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@varlog" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@vartmp" = {
+                    mountpoint = "/var/tmp";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@adminhome" = {
+                    mountpoint = "/home/timotheos";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@admincontainers" = {
+                    mountpoint = "/home/timotheos/.local/share/containers";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@admindownloads" = {
+                    mountpoint = "/home/timotheos/Downloads";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@adminmedia" = {
+                    mountpoint = "/home/timotheos/media";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
                   };
                 };
               };
@@ -55,7 +175,7 @@
           };
 
           swap = {
-            size = "100%";
+            size = "8G";
             content = {
               type = "swap";
               randomEncryption = true;
