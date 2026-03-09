efcab3bd8c8136b382544a5dcfadcf58495ab892
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-09 22:30:13 +1100

    Refactor fileSystems code blocks to more easily share variables

--- hosts/oona/hardware-config-custom.nix
+++ hosts/oona/hardware-config-custom.nix
@@ -7,6 +7,7 @@
   username,
   ...
 }:
+with lib;
 let
   commonOptions = [
     "defaults"
@@ -50,163 +51,167 @@ in
     "ntfs" # required to mount ntfs partitions
   ];
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-uuid/EE2C-39D6";
-      fsType = "vfat";
-      options = [
-        "fmask=0022"
-        "dmask=0022"
-      ];
+  fileSystems =
+    let
+      device = mkForce "/dev/disk/by-label/nixos";
+    in
+    {
+      "/boot" = {
+        device = mkForce "/dev/disk/by-uuid/EE2C-39D6";
+        fsType = "vfat";
+        options = [
+          "fmask=0022"
+          "dmask=0022"
+        ];
+      };
+      "/efi" = {
+        device = mkForce "/dev/disk/by-uuid/24B4-5D5C";
+        fsType = "vfat";
+        # set these permissions to prevent the "random seed file is world accessible
+        # which is a security hole" boot error
+        options = [
+          "fmask=0077"
+          "dmask=0077"
+        ];
+      };
+      # mount the HOME subvolume of the CachyOS partition
+      "/mnt/cachyos/home" = {
+        device = mkForce "/dev/disk/by-uuid/2a1020bc-0b4e-4b74-a373-8e624aec1e11";
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "nofail"
+          "subvol=@home"
+        ];
+      };
+      # mount the main Windows 11 Pro partition
+      "/mnt/win11pro" = {
+        device = "/dev/disk/by-uuid/D02CB4C42CB4A73E";
+        fsType = "ntfs-3g";
+        options = [
+          "nofail"
+          "rw"
+          "uid=1000"
+          "windows_names"
+        ];
+      };
+      "/" = {
+        inherit device;
+        fsType = "btrfs";
+        # NB: The default zstd compression level is 3.
+        # This option is used across all subvolumes on the btrfs device
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@root"
+        ];
+      };
+      "/nix" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@nix"
+        ];
+      };
+      "/root" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@roothome"
+        ];
+      };
+      "/home" = {
+        inherit device;
+        fsType = "btrfs";
+        # required on home directories for sops-nix to work with btrfs
+        # See https://github.com/Mic92/sops-nix/issues/721
+        neededForBoot = true;
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@home"
+        ];
+      };
+      "/home/${username}" = {
+        inherit device;
+        fsType = "btrfs";
+        neededForBoot = true;
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@adminhome"
+        ];
+      };
+      "/home/${username}/.local/share/containers" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@admincontainers"
+        ];
+      };
+      "/home/${username}/Downloads" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          # hide in the Nautilus devices menu
+          "x-gvfs-hide"
+          "subvol=@admindownloads"
+        ];
+      };
+      "/home/${username}/media" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          # hide in the Nautilus devices menu
+          "x-gvfs-hide"
+          "subvol=@adminmedia"
+        ];
+      };
+      "/var/lib/containers" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@vlcontainers"
+        ];
+      };
+      "/var/lib/libvirt/images" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@libvirtimgs"
+        ];
+      };
+      "/var/lib/postgresql" = {
+        inherit device;
+        fsType = "btrfs";
+        # Ensures /var/lib is mounted first
+        depends = [ "/var/lib" ];
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@vlpostgres"
+        ];
+      };
+      "/var/log" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@varlog"
+        ];
+      };
+      "/var/tmp" = {
+        inherit device;
+        fsType = "btrfs";
+        options = commonOptions ++ [
+          "compress=zstd"
+          "subvol=@vartmp"
+        ];
+      };
     };
-    "/efi" = {
-      device = "/dev/disk/by-uuid/24B4-5D5C";
-      fsType = "vfat";
-      # set these permissions to prevent the "random seed file is world accessible
-      # which is a security hole" boot error
-      options = [
-        "fmask=0077"
-        "dmask=0077"
-      ];
-    };
-    # mount the HOME subvolume of the CachyOS partition
-    "/mnt/cachyos/home" = {
-      device = "/dev/disk/by-uuid/2a1020bc-0b4e-4b74-a373-8e624aec1e11";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "nofail"
-        "subvol=@home"
-      ];
-    };
-    # mount the main Windows 11 Pro partition
-    "/mnt/win11pro" = {
-      device = "/dev/disk/by-uuid/D02CB4C42CB4A73E";
-      fsType = "ntfs-3g";
-      options = [
-        "nofail"
-        "rw"
-        "uid=1000"
-        "windows_names"
-      ];
-    };
-    "/" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      # NB: The default zstd compression level is 3.
-      # This option is used across all subvolumes on the btrfs device
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@root"
-      ];
-    };
-    "/nix" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@nix"
-      ];
-    };
-    "/root" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@roothome"
-      ];
-    };
-    "/home" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      # required on home directories for sops-nix to work with btrfs
-      # See https://github.com/Mic92/sops-nix/issues/721
-      neededForBoot = true;
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@home"
-      ];
-    };
-    "/home/${username}" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      neededForBoot = true;
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@adminhome"
-      ];
-    };
-    "/home/${username}/.local/share/containers" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@admincontainers"
-      ];
-    };
-    "/home/${username}/Downloads" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        # hide in the Nautilus devices menu
-        "x-gvfs-hide"
-        "subvol=@admindownloads"
-      ];
-    };
-    "/home/${username}/media" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        # hide in the Nautilus devices menu
-        "x-gvfs-hide"
-        "subvol=@adminmedia"
-      ];
-    };
-    "/var/lib/containers" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@vlcontainers"
-      ];
-    };
-    "/var/lib/libvirt/images" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@libvirtimgs"
-      ];
-    };
-    "/var/lib/postgresql" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      # Ensures /var/lib is mounted first
-      depends = [ "/var/lib" ];
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@vlpostgres"
-      ];
-    };
-    "/var/log" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@varlog"
-      ];
-    };
-    "/var/tmp" = {
-      device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
-      fsType = "btrfs";
-      options = commonOptions ++ [
-        "compress=zstd"
-        "subvol=@vartmp"
-      ];
-    };
-  };
 
   #fileSystems."/var/lib" = {
   #  device = "/dev/disk/by-uuid/f8d2d292-064d-403d-8578-cddd38a090e8";
