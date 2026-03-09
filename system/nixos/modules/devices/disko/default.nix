870467142ec103f1fc7988c9a8750bb2baed7ce6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-09 12:52:06 +1100

    Commit various edits to the disko module which will be replaced with a btrfs module

--- system/nixos/modules/devices/disko/default.nix
+++ system/nixos/modules/devices/disko/default.nix
@@ -26,130 +26,137 @@ in
             type = str;
             description = "The label of the physical disk";
           };
-          partScheme = mkOption {
-            type = enum [
-              "gpt"
-              "msdos"
-            ];
-            description = "The disk partitioning scheme to use";
-            default = "gpt";
-          };
-          partitions = mkOption {
-            type = attrsOf (submodule {
-              options = {
-                commonOptions = mkOption {
-                  type = listOf str;
-                  default = [
-                    "defaults"
-                    "noatime"
-                  ];
-                  description = "Specify mount options common to all partitions";
-                };
-                label = mkOption {
-                  type = str;
-                  description = "Set the LABEL of the partition";
-                };
-                name = mkOption {
-                  type = str;
-                  description = "The name of the partition";
-                };
-                partType = mkOption {
-                  type = str;
-                  # See https://wiki.archlinux.org/title/GPT_fdisk#Partition_type for a list
-                  # useful for less common and boot partition types
-                  description = "The type that is set above the partition's content block";
-                };
-                content = mkOption {
-                  type = attrsOf (submodule {
-                    options = {
-                      size = mkOption {
-                        type = str;
-                        description = "The size of the partition (eg. 512M or 100%)";
-                      };
-                      start = mkOption {
-                        type = str;
-                        description = "Where to start the partition on disk";
-                      };
-                      end = mkOption {
-                        type = str;
-                        description = "Where to end the partition on disk";
-                      };
-                      format = mkOption {
-                        type = str;
-                        description = "The partition format (eg. vfat, ext4)";
-                      };
-                      mountPoint = mkOption {
-                        type = str;
-                        description = "Define the mount point for the subvolume";
-                      };
-                      mountOptions = mkOption {
-                        type = listOf str;
-                        description = "Define any mount options specific to the mount point";
-                      };
-                      name = mkOption {
-                        type = str;
-                        description = "Define the name of the subvolume";
-                      };
-                      type = mkOption {
-                        type = str;
-                        description = "The type that is set within the partition's content block";
-                      };
-                      btrfsExtraArgs = mkOption {
-                        type = listOf str;
-                        description = "Define additional arguments for a btrfs content block";
-                        default = [ "-f" ];
-                      };
-                      commonBtrfsOptions = mkOption {
-                        type = listOf str;
-                        default = [ "compress=zstd" ];
-                        description = "Specify mount options common to all btrfs subvolumes";
-                      };
-                      btrfsSubs = mkOption {
-                        type = attrsOf (submodule {
-                          options = {
-                            mountPoint = mkOption {
-                              type = str;
-                              description = "Define the mount point for the subvolume";
-                            };
-                            mountOptions = mkOption {
-                              type = listOf str;
-                              description = "Define any mount options specific to the mount point";
-                            };
-                            subName = mkOption {
-                              type = str;
-                              description = "The btrfs subvolume name";
-                            };
-                          };
-                        });
-                      };
+          content = {
+            partScheme = mkOption {
+              type = enum [
+                "gpt"
+                "msdos"
+              ];
+              description = "The disk partitioning scheme to use";
+              default = "gpt";
+            };
+            partitions = mkOption {
+              type = attrsOf (submodule {
+                options = {
+                  commonOptions = mkOption {
+                    type = listOf str;
+                    default = [
+                      "defaults"
+                      "noatime"
+                    ];
+                    description = "Specify mount options common to all partitions";
+                  };
+                  label = mkOption {
+                    type = str;
+                    description = "Set the LABEL of the partition";
+                  };
+                  name = mkOption {
+                    type = str;
+                    description = "The name of the partition";
+                  };
+                  partType = mkOption {
+                    type = str;
+                    # See https://wiki.archlinux.org/title/GPT_fdisk#Partition_type for a list
+                    # useful for less common and boot partition types
+                    description = "The type that is set above the partition's content block";
+                    default = "8300";
+                  };
+                  size = mkOption {
+                    type = str;
+                    description = "The size of the partition (eg. 512M or 100%)";
+                  };
+                  start = mkOption {
+                    type = str;
+                    description = "Where to start the partition on disk";
+                  };
+                  end = mkOption {
+                    type = str;
+                    description = "Where to end the partition on disk";
+                  };
+                  content = {
+                    format = mkOption {
+                      type = str;
+                      description = "The partition format (eg. vfat, ext4)";
                     };
-                  });
+                    mountPoint = mkOption {
+                      type = str;
+                      description = "Define the mount point for the subvolume";
+                    };
+                    mountOptions = mkOption {
+                      type = listOf str;
+                      description = "Define any mount options specific to the mount point";
+                    };
+                    name = mkOption {
+                      type = str;
+                      description = "Define the name of the subvolume";
+                    };
+                    contentType = mkOption {
+                      type = str;
+                      description = "The type that is set within the partition's content block";
+                    };
+                    btrfsExtraArgs = mkOption {
+                      type = listOf str;
+                      description = "Define additional arguments for a btrfs content block";
+                      default = [ "-f" ];
+                    };
+                    commonBtrfsOptions = mkOption {
+                      type = listOf str;
+                      default = [ "compress=zstd" ];
+                      description = "Specify mount options common to all btrfs subvolumes";
+                    };
+                    btrfsSubs = mkOption {
+                      type = attrsOf (submodule {
+                        options = {
+                          mountPoint = mkOption {
+                            type = str;
+                            description = "Define the mount point for the subvolume";
+                          };
+                          mountOptions = mkOption {
+                            type = listOf str;
+                            description = "Define any mount options specific to the mount point";
+                          };
+                          subName = mkOption {
+                            type = str;
+                            description = "The btrfs subvolume name";
+                          };
+                        };
+                      });
+                    };
+                  };
                 };
-              };
-            });
+              });
+            };
           };
         };
       });
     };
   };
   config = mkIf cfg.enable {
-    disko.devices.disk = mapAttrs (
-      disk: diskCfg: {
-        type = "disk";
-        device = "${diskCfg.device}";
-        content = {
-          type = "${diskCfg.partScheme}";
-          partitions = mapAttrs (part: partCfg: {
-            label = optionalString (cfg.disks.name.partitions.name.label != "") "${partCfg.label}";
+    disko.devices.disk = mapAttrs (disk: diskCfg: {
+      type = "disk";
+      device = "${diskCfg.device}";
+      content = {
+        type = "${diskCfg.content.partScheme}";
+        partitions = mapAttrs (
+          part: partCfg:
+          let
+            cfg = config.mettavi.system.devices.disko;
+          in
+          {
+            label =
+              optionalString (cfg.disks.${disk}.content.partitions.${part}.label != "")
+                "${partCfg.label}";
             name = "${partCfg.name}";
-            size = optionalString (cfg.disks.name.partitions.name.size != "") "${partCfg.size}";
-            type = optionalString (cfg.disks.name.partitions.name.partType != "") "${partCfg.partType}";
+            size = optionalString (cfg.disks.${disk}.content.partitions.${part}.size != "") "${partCfg.size}";
+            type = optionalString (
+              cfg.disks.${disk}.content.partitions.${part}.partType != ""
+            ) "${partCfg.partType}";
             content =
               let
-                partContent = cfg.disks.name.partitions.name.content;
+                partContent = cfg.disks.${disk}.content.partitions.${part}.content;
               in
               {
-                type = optionalString (partContent.type != "") "${partContent.type}";
+                type = optionalString (partContent.contentType != "") "${partContent.contentType}";
                 extraArgs = optionals (partContent.extraArgs != "") "${partContent.extraArgs}";
                 format = optionalString (partContent.format != "") "${partContent.format}";
                 mountpoint = optionalString (partContent.mountPoint != "") "${partContent.mountPoint}";
@@ -164,11 +171,11 @@ in
                     mountpoint = "${subvolCfg.mountPoint}";
                     mountOptions = "${contentCfg.commonBtrfsOptions}" ++ "${subvolCfg.mountOptions}";
                   }
-                ) cfg.disks.partitions.name.content.btrfsSubs;
+                ) cfg.disks.${disk}.content.partitions.${part}.content.btrfsSubs;
               };
-          }) cfg.disks.name.partitions;
-        };
-      }
-    );
+          }
+        ) cfg.disks.${disk}.content.partitions;
+      };
+    }) cfg.disks;
   };
 }
