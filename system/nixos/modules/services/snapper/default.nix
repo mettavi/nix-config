257bf2959e88ce7b29ee0b96b4a623b1c351f307
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-16 20:58:22 +1100

    Tweak comments

--- system/nixos/modules/services/snapper/default.nix
+++ system/nixos/modules/services/snapper/default.nix
@@ -83,9 +83,9 @@ in
         };
       };
 
-    # create nested .snapshots subvolumes to store the snapshots taken by snapper
+    # create nested .snapshots btrfs subvolumes to store the snapshots taken by snapper
     # NB: The .snapshots directory must be owned by root and must not be writable (eg. r-x) by anybody else.
-    # NB 2: Ensure the root / is a btrfs subvolume for the below rules to work
+    # NB 2: Ensure the root / is a btrfs subvolume for the below "v" subvolume rules to work
     # see https://discourse.nixos.org/t/snapper-should-snapshots-subvolumes-be-created-automatically/22329/11
     systemd.tmpfiles.rules = [
       # type path mode user group (expiry)
