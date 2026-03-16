cf5e64ca0dd3db4e6c30794f932146f189ecba30
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-16 20:33:40 +1100

    Embed the text file directly in order to use a variable to point to the latest version of the bitwarden app

--- system/shared/modules/apps/brave/default.nix
+++ system/shared/modules/apps/brave/default.nix
@@ -52,8 +52,20 @@ in
         # Workaround: 1. create the google-chome directory to get bitwarden to create the com.8bit.bitwarden.json file
         "google-chrome/NativeMessagingHosts/.keep".text = "";
         # 2. link the file to the Brave directory to get system authentication working
-        "BraveSoftware/Brave-Browser/NativeMessagingHosts/com.8bit.bitwarden.json".source =
-          ./com.8bit.bitwarden.json;
+        "BraveSoftware/Brave-Browser/NativeMessagingHosts/com.8bit.bitwarden.json".text = ''
+          {
+            "name": "com.8bit.bitwarden",
+            "description": "Bitwarden desktop <-> browser bridge",
+            "path": "${pkgs.bitwarden-desktop}/libexec/desktop_proxy",
+            "type": "stdio",
+            "allowed_origins": [
+              "chrome-extension://nngceckbapebfimnlniiiahkandclblb/",
+              "chrome-extension://hccnnhgbibccigepcmlgppchkpfdophk/",
+              "chrome-extension://jbkfoedolllekgbhcbcoahefnbanhhlh/",
+              "chrome-extension://ccnckbpmaceehanjmeomladnmlffdjgn/"
+            ]
+          }
+        '';
       };
     };
   };
