562c7468ac6809f6a4ae0b9dc36bdf0b86572111
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-17 21:50:00 +1100

    Rename file to mount.nix after removing unrelated code

--- hosts/oona/configuration.nix
+++ hosts/oona/configuration.nix
@@ -10,7 +10,8 @@
   imports = [
     # customise the hardware scan config
     # NB: hardware-configuration.nix is auto-generated during install and is imported in the mkNixos function
-    ./hardware-config-custom.nix
+    # ./hardware-config-custom.nix
+    ./mount.nix
     ./kernel.nix
   ];
 
