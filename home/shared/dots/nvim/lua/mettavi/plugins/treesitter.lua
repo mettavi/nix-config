a19de8890bfc6973afe7eb63ed60537a5755105c
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-22 16:07:37 +1100

    Add back removed functionality to enable treesitter on filetypes using a plugin

--- home/shared/dots/nvim/lua/mettavi/plugins/treesitter.lua
+++ home/shared/dots/nvim/lua/mettavi/plugins/treesitter.lua
@@ -1,47 +1,38 @@
+-- this is what you previously passed to ensure_installed
+local languages = {
+  "json",
+  "javascript",
+  "typescript",
+  "tsx",
+  "yaml",
+  "html",
+  "css",
+  "markdown",
+  "markdown_inline",
+  "svelte",
+  "bash",
+  "lua",
+  "vim",
+  "dockerfile",
+  "gitignore",
+  "query",
+  "vimdoc",
+  "c",
+  "toml",
+  "regex",
+  "nix",
+}
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    branch = "main",
+    -- event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "windwp/nvim-ts-autotag",
     },
     config = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "<filetype>" },
-        callback = function()
-          -- enable syntax highlighting
-          vim.treesitter.start()
-          -- Treesitter-based indentation (experimental as at 31-12-2025)
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-
-      require("nvim-treesitter").install({
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-        "toml",
-        "regex",
-        "nix",
-      })
-
       require("nvim-ts-autotag").setup({
         opts = {
           -- Defaults
@@ -59,5 +50,16 @@ return {
         },
       })
     end,
+  },
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      ensure_installed = languages,
+      fold = { enable = true },
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = { enable = true },
+    },
   },
 }
