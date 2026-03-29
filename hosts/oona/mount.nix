6fc42c02b7481521cc32156e6ae64c249019a1e0
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-03-30 03:09:04 +1100

    Add new service to run deduplication on the btrfs file system

--- hosts/oona/mount.nix
+++ hosts/oona/mount.nix
@@ -203,6 +203,51 @@ with lib;
     fileSystems = [ "/" ];
   };
 
+  services.beesd = {
+    filesystems = {
+      root = {
+        # Filesystem to run bees dedupe on (can also be a path):
+        # Note that deduplication crosses subvolumes; one must
+        # not configure multiple instances for subvolumes of the same filesystem
+        # (or block devices which are part of the same filesystem), but only for
+        # completely independent btrfs filesystems.
+        spec = "LABEL=nixos";
+
+        # 1 GB hash table size, also maps to RAM
+        # must be a multiple of 16MB
+        hashTableSizeMB = 1024;
+
+        # This caches block fingerprints to check newly
+        # written blocks against existing. Too small and
+        # dedupe rates suffer. Too large just wastes RAM.
+
+        # unique data size |  hash table size |average dedupe extent size
+        #     1TB          |      4GB         |        4K
+        #     1TB          |      1GB         |       16K
+        #     1TB          |    256MB         |       64K
+        #     1TB          |    128MB         |      128K
+        #     1TB          |     16MB         |     1024K
+        #    64TB          |      1GB         |     1024K
+        # Source: https://github.com/Zygo/bees/blob/master/docs/config.md
+
+        # location of hash table
+        workDir = ".beeshome";
+
+        # emerg = 0; alert = 1; crit = 2; err = 3; warning = 4; notice = 5; info = 6 (default); debug = 7;
+        verbosity = "info";
+
+        extraOptions = [
+          # Max load avg target before throttling (default is no throttling)
+          "--loadavg-target"
+          "4.0"
+          # Number of worker threads to use (default is 1 per cpu core)
+          "--thread-count"
+          "6"
+        ];
+      };
+    };
+  };
+
   ######################################################
   # SET NO COPY-ON-WRITE ON SPECIAL BTRFS SUBVOLUMES
   # NB: 1) Setting this by a mount option will apply the option to ALL subvolumes on the partition.
