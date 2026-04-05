18d43326d4ffd24f770ae6e811513df39f9c0e56
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-04-05 17:09:54 +1000

    Re-enable the config file for immich and set its variables including enabling gpu acceleration

--- system/nixos/modules/services/immich/default.nix
+++ system/nixos/modules/services/immich/default.nix
@@ -59,13 +59,246 @@ in
       secretsFile = null;
       # config file /run/immich/config.json
       # Setting it to null allows configuring Immich in the web interface.
-      settings = { };
+      settings = {
+        backup = {
+          database = {
+            cronExpression = "0 02 * * *";
+            enabled = true;
+            keepLastAmount = 14;
+          };
+        };
+        ffmpeg = {
+          accel = "nvenc";
+          accelDecode = true;
+          acceptedAudioCodecs = [
+            "aac"
+            "mp3"
+            "libopus"
+          ];
+          acceptedContainers = [
+            "mov"
+            "ogg"
+            "webm"
+          ];
+          acceptedVideoCodecs = [
+            "h264"
+          ];
+          bframes = "-1";
+          cqMode = "auto";
+          crf = 23;
+          gopSize = 0;
+          maxBitrate = "0";
+          preferredHwDevice = "auto";
+          preset = "fast";
+          refs = 0;
+          targetAudioCodec = "aac";
+          targetResolution = "720";
+          targetVideoCodec = "h264";
+          temporalAQ = true;
+          threads = 0;
+          tonemap = "hable";
+          transcode = "required";
+          twoPass = true;
+        };
+        image = {
+          colorspace = "p3";
+          extractEmbedded = false;
+          fullsize = {
+            enabled = false;
+            format = "jpeg";
+            progressive = false;
+            quality = 80;
+          };
+          preview = {
+            format = "jpeg";
+            progressive = false;
+            quality = 80;
+            size = 1440;
+          };
+          thumbnail = {
+            format = "webp";
+            progressive = false;
+            quality = 80;
+            size = 250;
+          };
+        };
+        job = {
+          backgroundTask = {
+            concurrency = 5;
+          };
+          editor = {
+            concurrency = 2;
+          };
+          faceDetection = {
+            concurrency = 2;
+          };
+          library = {
+            concurrency = 5;
+          };
+          metadataExtraction = {
+            concurrency = 5;
+          };
+          migration = {
+            concurrency = 5;
+          };
+          notifications = {
+            concurrency = 5;
+          };
+          ocr = {
+            concurrency = 1;
+          };
+          search = {
+            concurrency = 5;
+          };
+          sidecar = {
+            concurrency = 5;
+          };
+          smartSearch = {
+            concurrency = 2;
+          };
+          thumbnailGeneration = {
+            concurrency = 1;
+          };
+          videoConversion = {
+            concurrency = 1;
+          };
+          workflow = {
+            concurrency = 5;
+          };
+        };
+        library = {
+          scan = {
+            cronExpression = "0 0 * * *";
+            enabled = true;
+          };
+          watch = {
+            enabled = false;
+          };
+        };
+        logging = {
+          enabled = true;
+          level = "log";
+        };
+        machineLearning = {
+          availabilityChecks = {
+            enabled = true;
+            interval = 30000;
+            timeout = 2000;
+          };
+          clip = {
+            enabled = true;
+            modelName = "ViT-B-32__openai";
+          };
+          duplicateDetection = {
+            enabled = true;
+            maxDistance = 0.01;
+            ocr = {
+              enabled = true;
+              maxResolution = 736;
+              minDetectionScore = 0.5;
+              minRecognitionScore = 0.8;
+              modelName = "PP-OCRv5_mobile";
+            };
+            urls = [
+              "http://localhost:3003"
+            ];
+          };
+          map = {
+            darkStyle = "https://tiles.immich.cloud/v1/style/dark.json";
+            enabled = true;
+            lightStyle = "https://tiles.immich.cloud/v1/style/light.json";
+          };
+          metadata = {
+            faces = {
+              import = false;
+            };
+          };
+          newVersionCheck = {
+            enabled = true;
+          };
+          nightlyTasks = {
+            clusterNewFaces = true;
+            databaseCleanup = true;
+            generateMemories = true;
+            missingThumbnails = true;
+            startTime = "00:00";
+            syncQuotaUsage = true;
+          };
+          notifications = {
+            smtp = {
+              enabled = false;
+              from = "";
+              replyTo = "";
+              transport = {
+                host = "";
+                ignoreCert = false;
+                password = "";
+                port = 587;
+                secure = false;
+                username = "";
+              };
+            };
+          };
+          oauth = {
+            autoLaunch = false;
+            autoRegister = true;
+            buttonText = "Login with OAuth";
+            clientId = "";
+            clientSecret = "";
+            defaultStorageQuota = null;
+            enabled = false;
+            issuerUrl = "";
+            mobileOverrideEnabled = false;
+            mobileRedirectUri = "";
+            profileSigningAlgorithm = "none";
+            roleClaim = "immich_role";
+            scope = "openid email profile";
+            signingAlgorithm = "RS256";
+            storageLabelClaim = "preferred_username";
+            storageQuotaClaim = "immich_quota";
+            timeout = 30000;
+            tokenEndpointAuthMethod = "client_secret_post";
+          };
+          passwordLogin = {
+            enabled = true;
+          };
+          reverseGeocoding = {
+            enabled = true;
+          };
+          server = {
+            externalDomain = "";
+            loginPageMessage = "";
+            publicUsers = true;
+          };
+          storageTemplate = {
+            enabled = false;
+            hashVerificationEnabled = true;
+            template = "{{y}}/{{y}}-{{MM}}-{{dd}}/{{filename}}";
+          };
+          templates = {
+            email = {
+              albumInviteTemplate = "";
+              albumUpdateTemplate = "";
+              welcomeTemplate = "";
+            };
+          };
+          theme = {
+            customCss = "";
+          };
+          trash = {
+            days = 30;
+            enabled = true;
+          };
+          user = {
+            deleteDelay = 7;
+          };
+        };
+      };
     };
 
     users.users.immich.extraGroups = [
       "video"
       "render"
     ];
-
   };
 }
