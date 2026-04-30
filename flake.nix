036549923e3203363351b356b28c52a9e28e60c8
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-05-01 00:00:59 +1000

    Add the nix-wrapper-modules input to flake.nix

--- flake.nix
+++ flake.nix
@@ -101,6 +101,10 @@
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    wrappers = {
+      url = "github:BirdeeHub/nix-wrapper-modules";
+      inputs.nixpkgs.follows = "nixos-pkgs";
+    };
     sops-nixos = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixos-pkgs";
