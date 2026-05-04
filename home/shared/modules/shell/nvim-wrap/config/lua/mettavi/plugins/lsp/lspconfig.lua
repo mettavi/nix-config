60d86ec25f4cbf3986922b8ab4ef05a933205762
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-05-05 01:03:45 +1000

    Move neovim config files for the nvim-wrap module into the same folder as the nix file

---
+++ home/shared/modules/shell/nvim-wrap/config/lua/mettavi/plugins/lsp/lspconfig.lua
@@ -0,0 +1,227 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+  },
+  config = function()
+    local keymap = vim.keymap -- for conciseness
+
+    -- import lspconfig plugin
+    local lspconfig = vim.lsp.config
+
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    -- enable autocompletion via nvim-cmp (assign to every lsp server config)
+    -- by extending the capabilities of lsp/neovim with nvim-cmp
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable({
+      "bashls",
+      "lua_ls",
+      "nixd",
+      "taplo",
+      "ts_ls",
+      "yamlls",
+    })
+
+    -- Server-specific settings. See `:help lsp-quickstart`
+    vim.lsp.config("lua_ls", {
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace",
+          },
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          hint = {
+            enable = true, -- necessary for inlay hints
+          },
+        },
+      },
+    })
+
+    -- local variables to setup nixd lsp
+    local hostname = vim.uv.os_gethostname()
+    local home = vim.env.HOME
+    local sysname = vim.uv.os_uname().sysname
+
+    local platform
+    if sysname == "Darwin" then
+      platform = "darwinConfigurations"
+    else
+      platform = "nixosConfigurations"
+    end
+
+    vim.lsp.config("nixd", {
+      settings = {
+        nixd = {
+          formatting = {
+            command = { "nixfmt" },
+          },
+          -- diagnostic = { suppress = { "sema-unused-def-lambda-witharg-formal" } },
+          options = {
+            sysopt = {
+              expr = string.format(
+                "(builtins.getFlake (builtins.toString %s/.nix-config)).%s.%s.options",
+                home,
+                platform,
+                hostname
+              ),
+            },
+            -- Before configuring Home Manager options, consider your setup:
+            -- Which command do you use for home-manager switching?
+            --
+            --  A. home-manager switch --flake .#... (standalone Home Manager)
+            -- expr = "(builtins.getFlake (builtins.toString ./.)).homeConfigurations.<name>.options",
+            --  B. nixos-rebuild switch --flake .#... (NixOS with integrated Home Manager)
+            -- expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.<name>.options.home-manager.users.type.getSubOptions []".
+            homeopt = {
+              expr = string.format(
+                "(builtins.getFlake (builtins.toString %s/.nix-config)).%s.%s.options.home-manager.users.type.getSubOptions []",
+                home,
+                platform,
+                hostname
+              ),
+            },
+          },
+        },
+      },
+    })
+
+    local tsinlayHints = {
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
+    }
+    vim.lsp.config("ts_ls", {
+      settings = {
+        typescript = {
+          inlayHints = {
+            tsinlayHints,
+          },
+        },
+        javascript = {
+          inlayHints = {
+            tsinlayHints,
+          },
+        },
+      },
+      implicitProjectConfiguration = {
+        checkJs = true,
+      },
+    })
+
+    lspconfig("yamlls", {
+      settings = {
+        yaml = {
+          schemaStore = {
+            -- must disable built-in schemaStore support if you want to use
+            -- the neovim schemastore plugin and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "",
+          },
+          schemas = require("schemastore").yaml.schemas(),
+          -- using yamlfmt for formatting
+          format = {
+            enable = false,
+          },
+        },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf, silent = true }
+
+        -- set keybinds
+        opts.desc = "Show LSP references"
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+        opts.desc = "Go to declaration"
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+        opts.desc = "Show LSP definitions"
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+        opts.desc = "Show LSP implementations"
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+        opts.desc = "Show LSP type definitions"
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+        opts.desc = "See available code actions"
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+        opts.desc = "Smart rename"
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+
+        opts.desc = "Show buffer diagnostics"
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+        opts.desc = "Show line diagnostics"
+        keymap.set("n", "<leader>dl", vim.diagnostic.open_float, opts) -- show diagnostics for line, j/k to close
+
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+        opts.desc = "Restart LSP"
+        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        -- enable (and toggle) inlay hints with a command or keymap
+        local bufnr = args.buf ---@type number
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client.server_capabilities.inlayHintProvider then
+          vim.api.nvim_create_user_command("ToggleInlayHints", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+          end, { desc = "Toggle Inlay Hints" })
+          vim.keymap.set("n", "<leader>i", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }), { bufnr = bufnr })
+            vim.notify("Inlay hints: " .. ((vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })) and " on" or "off"))
+          end, { buffer = bufnr, desc = "Toggle Inlay Hints" })
+        else
+          print("no inlay hints available")
+        end
+      end,
+    })
+
+    -- Change the Diagnostic symbols in the sign column (gutter)
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.INFO] = "󰋼 ",
+          [vim.diagnostic.severity.HINT] = "󰌵 ",
+        },
+        numhl = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.INFO] = "",
+        },
+      },
+    })
+  end,
+}
---
+++ home/shared/modules/shell/nvim-wrap/config/lua/mettavi/plugins/lsp/mason.lua
@@ -0,0 +1,213 @@
+return {
+  "mason-org/mason.nvim",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
+
+    -- enable autocompletion via nvim-cmp (assign to every lsp server config)
+    -- by extending the capabilities of lsp/neovim with nvim-cmp
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
+    -- code to run when lsp attaches to buffer (assign to everyl lsp server config)
+    local on_attach = function(client, bufnr)
+      -- inlay hints (experimental), need to turn it on manually
+      local function buf_command(...)
+        vim.api.nvim_buf_create_user_command(bufnr, ...)
+      end
+      if client.server_capabilities.inlayHintProvider and vim.fn.has("nvim-0.10") > 0 then
+        local inlay = function(enable)
+          if enable == "toggle" then
+            enable = not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })
+          end
+          vim.lsp.inlay_hint.enable(enable, { bufnr = bufnr })
+        end
+        vim.api.nvim_create_user_command("ToggleInlayHints", "InlayHintsToggle", {})
+        buf_command("InlayHintsToggle", function(_)
+          inlay("toggle")
+        end, { nargs = 0, desc = "Toggle inlay hints." })
+        buf_command("ToggleInlayHints", "InlayHintsToggle", {})
+        -- Toggling inlay hints: gh
+        vim.keymap.set("n", "gh", "<cmd>InlayHintsToggle<CR>", { buffer = true })
+      else
+        print("no inlay hints available")
+      end
+    end
+
+    -- import mason
+    local mason = require("mason")
+
+    -- import mason-lspconfig
+    local mason_lspconfig = require("mason-lspconfig").setup()
+
+    local mason_tool_installer = require("mason-tool-installer")
+
+    local mason_nvim_dap = require("mason-nvim-dap")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    -- mason_lspconfig.setup({ -- use mason_tool_installer instead to also autoupdate
+    -- list of servers for mason to install
+    -- ensure_installed = {
+    --   "ts_ls",
+    --   "html",
+    --   "cssls",
+    --   "lua_ls",
+    --   "pyright",
+    --   "bashls", -- uses shellcheck linter installed with brew
+    -- },
+    -- auto-install configured servers (with lspconfig)
+    -- automatic_installation = true, -- not the same as ensure_installed
+    -- })
+
+    -- mason_tool_installer.setup({
+    --   ensure_installed = {
+    --     "ts_ls", -- typescript language server
+    --     "eslint_d", -- js linter
+    --     "html", -- HTML language server
+    --     "cssls", -- CSS language server
+    --     "pyright", -- python language server
+    --     "pylint", -- python linter
+    --     "isort", -- python formatter
+    --     "black", -- python formatter
+    --     "lua_ls", -- lua language server
+    --     "stylua", -- lua formatter
+    --     "bashls", -- bash language server, uses shellcheck linter installed with brew
+    --     "shfmt", -- bash formatter
+    --     "taplo", -- toml ls and formatter
+    --     "yamlls", -- Language Server for YAML Files
+    --     "yamllint", -- Linter for YAML files
+    --     "yamlfmt", -- tool or library to format yaml files
+    --     "prettier", -- multi-purpose formatter
+    --   },
+    -- if set to true this will check each tool for updates. If updates
+    -- are available the tool will be updated. This setting does not
+    -- affect :MasonToolsUpdate or :MasonToolsInstall.
+    -- Default: false
+    -- auto_update = true,
+    -- })
+
+    -- mason_nvim_dap.setup()
+
+    -- language servers not installed with mason are configured in nvim-lspconfig
+    -- lspconfig["bashls"].setup({
+    --   capabilities = capabilities,
+    --   on_attach = on_attach,
+    --   filetypes = { "sh", "bash" },
+    -- })
+    lspconfig["lua_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          completion = {
+            callSnippet = "Replace",
+          },
+          hint = {
+            enable = true,
+          },
+        },
+      },
+    })
+    lspconfig["taplo"].setup({})
+    lspconfig["ts_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        typescript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+        javascript = {
+          inlayHints = {
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+      },
+      implicitProjectConfiguration = {
+        checkJs = true,
+      },
+    })
+    lspconfig.nixd.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      cmd = { "nixd" },
+      settings = {
+        nixd = {
+          nixpkgs = {
+            expr = "import <nixpkgs> { }",
+          },
+          formatting = {
+            command = { "nixfmt" },
+          },
+          -- diagnostic = { suppress = { "sema-unused-def-lambda-witharg-formal" } },
+          options = {
+            darwin = {
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).darwinConfigurations.mack.options',
+            },
+            -- nixd cannot get home-manager options when installed as a nix-darwin module
+            -- ( See https://github.com/nix-community/nixd/issues/608 )
+            -- home_manager = {
+            --   expr = '(builtins.getFlake  "/Users/timotheos/.nix-config").homeConfigurations."timotheos@mack".options',
+            -- },
+          },
+        },
+      },
+    })
+    lspconfig.yamlls.setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = {
+        yaml = {
+          schemaStore = {
+            -- must disable built-in schemaStore support if you want to use
+            -- the neovim schemastore plugin and its advanced options like `ignore`.
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "",
+          },
+          schemas = require("schemastore").yaml.schemas(),
+          -- using yamlfmt for formatting
+          format = {
+            enable = false,
+          },
+        },
+      },
+    })
+  end,
+}
