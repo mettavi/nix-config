f63638c0046aadb1bca5b4159e1b14a7cbd722a8
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-05-06 19:36:53 +1000

    Fix errors in nvim-wrap module and add new helper submodule

--- home/shared/modules/shell/nvim-wrap/default.nix
+++ home/shared/modules/shell/nvim-wrap/default.nix
@@ -24,62 +24,120 @@ in
       type = lib.types.attrsOf lib.types.bool;
       default = builtins.mapAttrs (_: v: v.enable) config.specs;
     };
-    neovimPlugins = lib.mkOption {
-      readOnly = true;
-      type = lib.types.attrsOf wlib.types.stringable;
-      # Makes plugins autobuilt from our inputs available with
-      # `config.mettavi.shell.nvim-lib.neovimPlugins.<name_without_prefix>`
-      default = config.mettavi.shell.nvim-lib.pluginsFromPrefix "plugins-" inputs;
-    };
-    pluginsFromPrefix = lib.mkOption {
-      type = lib.types.raw;
-      readOnly = true;
-      default =
-        prefix: inputs:
-        lib.pipe inputs [
-          builtins.attrNames
-          (builtins.filter (s: lib.hasPrefix prefix s))
-          (map (
-            input:
-            let
-              name = lib.removePrefix prefix input;
-            in
-            {
-              inherit name;
-              value = config.mettavi.shell.nvim-lib.mkPlugin name inputs.${input};
-            }
-          ))
-          builtins.listToAttrs
-        ];
-    };
   };
 
   config = mkIf cfg.enable {
-
     wrappers.neovim =
-      { pkgs, ... }:
       {
+        config,
+        pkgs,
+        wlib,
+        ...
+      }:
+      let
+        neovimPlugins = config.nvim-lib.pluginsFromPrefix "plugins-" inputs;
+      in
+      {
+        imports = [ ./extPlugins.nix ];
+
         enable = true;
-
-        settings.config_directory = ../../../dots/nvim-wrap;
-
+        env.NVIM_APPNAME = "nvim-wrap";
         # If you want to install multiple neovim derivations via home.packages or environment.systemPackages
         # in order to prevent path collisions:
         # set this to true:
         settings.dont_link = true;
         # and make sure these dont share values:
         binName = "nvw";
-        # config.settings.aliases = [ ];
+        # settings.aliases = [ ];
+
+        settings.config_directory = ./config;
+
+        specMods =
+          {
+            # When this module is run in an inner list,
+            # this will contain `config` of the parent spec
+            parentSpec ? null,
+            # and this will contain `options`
+            # otherwise they will be `null`
+            parentOpts ? null,
+            parentName ? null,
+            # and then config from this one, as normal
+            config,
+            # and the other module arguments.
+            ...
+          }:
+          {
+            options.extraPackages = lib.mkOption {
+              type = lib.types.listOf wlib.types.stringable;
+              default = [ ];
+              description = "an extraPackages spec field to put packages to suffix to the PATH";
+            };
+          };
+
+        # get extraPackages as sub-options under specs.<name>.extraPackages, rather than at the top level
+        extraPackages = config.specCollect (acc: v: acc ++ (v.extraPackages or [ ])) [ ];
+
+        specs.general = {
+          data = with pkgs.vimPlugins; [
+            auto-session
+            better-escape-nvim
+            bufferline-nvim
+            harpoon
+            # lazydev-nvim
+            lualine-nvim
+            # lze
+            # lzextras
+            nvim-tree-lua
+            SchemaStore-nvim
+            tokyonight-nvim
+            # treesitter + grammars
+            # nvim-treesitter.withAllGrammars
+            # This is for if you only want some of the grammars
+            (nvim-treesitter.withPlugins (
+              plugins: with plugins; [
+                json
+                javascript
+                typescript
+                tsx
+                yaml
+                html
+                css
+                markdown
+                markdown_inline
+                bash
+                lua
+                vim
+                dockerfile
+                gitignore
+                query
+                vimdoc
+                c
+                toml
+                regex
+                nix
+              ]
+            ))
+            treesitter-modules-nvim
+            vim-tmux-navigator
+          ];
+          extraPackages = with pkgs; [
+            lazygit
+            prettier
+            taplo
+            tree-sitter
+            typescript-language-server
+          ];
+        };
 
         # If the defaults are fine, you can just provide the `.data` field
         # In this case, a list of specs, instead of a single plugin like above
         specs.lze = [
           # if defaults is fine, you can just provide the `.data` field
-          cfg.neovimPlugins.lze
+          neovimPlugins.lze
           # but these can be specs too!
           {
             # these ones can't take lists though
-            data = cfg.neovimPlugins.lzextras;
+            data = neovimPlugins.lzextras;
             # things can target any spec that has a name.
             name = "lzextras";
             # now something else can be after = [ "lzextras" ]
@@ -96,30 +154,8 @@ in
           }
         ];
 
-        specs.general = {
-          data = with pkgs.vimPlugins; [
-            auto-session
-            better-escape-nvim
-            bufferline-nvim
-            harpoon
-            lazydev-nvim
-            lualine-nvim
-            nvim-tree-lua
-            SchemaStore-nvim
-            tokyonight-nvim
-            # treesitter + grammars
-            nvim-treesitter.withAllGrammars
-            # This is for if you only want some of the grammars
-            # (nvim-treesitter.withPlugins (
-            #   plugins: with plugins; [
-            #     nix
-            #     lua
-            #   ]
-            # ))
-            treesitter-modules-nvim
-            vim-tmux-navigator
-          ];
-        };
+        specs.vim-maximizer = neovimPlugins.vim-maximizer;
+
         specs.lazy = {
           # this would ensure any config included from nix in here will be ran after any provided by the `lze` spec
           # If we provided any from within either spec, anyway
@@ -133,67 +169,55 @@ in
           # here we chose a DAL of plugins, but we can also pass a single plugin, or null
           # plugins are of type wlib.types.stringable
           # plugins which are not loaded until you vim.cmd.packadd them ...
-          data =
-            with pkgs.vimPlugins;
-            [
-              # {
-              #   data = telescope-nvim;
-              #   # You can override defaults from the parent spec here
-              #   lazy = false;
-              # }
-              alpha-nvim
-              blink-cmp
-              blink-compat
-              comment-nvim
-              conform-nvim
-              cmp-buffer
-              cmp-cmdline
-              cmp_luasnip
-              cmp-nvim-lsp
-              cmp-path
-              colorful-menu-nvim
-              dressing-nvim # very lazy
-              fidget-nvim
-              flash-nvim # very lazy
-              friendly-snippets
-              gitsigns-nvim
-              indent-blankline-nvim
-              lazygit-nvim
-              lspkind-nvim
-              lualine-nvim
-              luasnip
-              noice-nvim # very lazy
-              nui-nvim
-              nvim-autopairs
-              nvim-cmp
-              nvim-lint
-              nvim-lspconfig
-              nvim-lsp-file-operations
-              nvim-notify
-              nvim-surround
-              nvim-treesitter-textobjects
-              nvim-ts-autotag
-              nvim-ts-context-commentstring
-              nvim-web-devicons
-              plenary-nvim
-              render-markdown-nvim
-              snacks-nvim
-              substitute-nvim
-              telescope-nvim
-              todo-comments-nvim
-              trouble-nvim
-              vim-startuptime
-              which-key-nvim # very lazy
-            ]
-            ++ [
-              cfg.neovimPlugins.vim-maximizer
-            ];
-          extraPackages = with pkgs; [
-            lazygit
-            prettier
-            taplo
-            tree-sitter
-            typescript-language-server
+          data = with pkgs.vimPlugins; [
+            # {
+            #   data = telescope-nvim;
+            #   # You can override defaults from the parent spec here
+            #   lazy = false;
+            # }
+            alpha-nvim
+            # blink-cmp
+            # blink-compat
+            comment-nvim
+            conform-nvim
+            cmp-buffer
+            cmp-cmdline
+            cmp_luasnip
+            cmp-nvim-lsp
+            cmp-path
+            # colorful-menu-nvim
+            dressing-nvim # very lazy
+            # fidget-nvim
+            flash-nvim # very lazy
+            friendly-snippets
+            gitsigns-nvim
+            indent-blankline-nvim
+            lazygit-nvim
+            lspkind-nvim
+            lualine-nvim
+            luasnip
+            noice-nvim # very lazy
+            nui-nvim
+            nvim-autopairs
+            nvim-cmp
+            nvim-lint
+            nvim-lspconfig
+            nvim-lsp-file-operations
+            nvim-notify
+            nvim-surround
+            nvim-treesitter-textobjects
+            nvim-ts-autotag
+            nvim-ts-context-commentstring
+            nvim-web-devicons
+            plenary-nvim
+            render-markdown-nvim
+            # snacks-nvim
+            substitute-nvim
+            telescope-nvim
+            todo-comments-nvim
+            trouble-nvim
+            # vim-startuptime
+            which-key-nvim # very lazy
           ];
         };
 
@@ -209,7 +233,6 @@ in
             stylua
           ];
         };
-
         specs.nix = {
           data = null;
           extraPackages = with pkgs; [
@@ -225,7 +248,6 @@ in
             shfmt
           ];
         };
-
         specs.yaml = {
           data = null;
           extraPackages = with pkgs; [
@@ -234,16 +256,16 @@ in
             yamllint
           ];
         };
-
-        # info = {
-        #   values = "for lua";
-        #   which = "will be placed in the generated info plugin for access";
-        # };
-
-        # lsps, formatters, etc...
-        # extraPackages = with pkgs; [
-        # ];
       };
+
+    #
+    #
+    #
+    # info = {
+    #   values = "for lua";
+    #   which = "will be placed in the generated info plugin for access";
+    # };
+    #
 
     home.sessionVariables =
       let
@@ -254,13 +276,8 @@ in
         EDITOR = nvimpath;
         MANPAGER = "${nvimpath} +Man!";
       };
-
-    home.shellAliases = {
-      nvw = "env NVIM_APPNAME='nvim-wrap' nvw";
-    };
-
     xdg.configFile = {
-      "nvim-wrap".source = "${config.home.homeDirectory}/${nix_repo}/home/shared/dots/nvim-wrap";
+      "nvim-wrap".source = ./config;
     };
   };
 }
---
+++ home/shared/modules/shell/nvim-wrap/extPlugins.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }: {
+  options.nvim-lib.pluginsFromPrefix = lib.mkOption {
+    type = lib.types.raw;
+    readOnly = true;
+    default =
+      prefix: inputs:
+      lib.pipe inputs [
+        builtins.attrNames
+        (builtins.filter (s: lib.hasPrefix prefix s))
+        (map (
+          input:
+          let
+            name = lib.removePrefix prefix input;
+          in
+          {
+            inherit name;
+            value = config.nvim-lib.mkPlugin name inputs.${input};
+          }
+        ))
+        builtins.listToAttrs
+      ];
+  };
+}
