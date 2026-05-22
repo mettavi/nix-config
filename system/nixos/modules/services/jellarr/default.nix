dafbe9db53a4bb803a8d523c304ff397ebdf910e
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-05-21 18:27:49 -0700

    Disable jellarr module due to error with hash mismatch

--- system/nixos/modules/services/jellarr/default.nix
+++ system/nixos/modules/services/jellarr/default.nix
@@ -46,7 +46,7 @@ in
 
     # configure the jellarr module
     services.jellarr = {
-      enable = true;
+      enable = false;
       # Bootstrap: automatically inserts API key into Jellyfin's database
       # NB: bootstrap only works if jellarr and jellyfin are on the same host
       bootstrap = {
