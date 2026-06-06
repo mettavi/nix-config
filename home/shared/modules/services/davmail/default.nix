53a6c9722f43da760d8659d0ba1d068b7ba1142c
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-06-06 00:39:27 -0700

    Add a new davmail (MS Exchange) module for home-manager

---
+++ home/shared/modules/services/davmail/default.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.mettavi.services.davmail;
+in
+{
+  options.mettavi.services.davmail = {
+    enable = mkEnableOption "davmail";
+  };
+
+  config = mkIf cfg.enable {
+    services.davmail = {
+      enable = true;
+      imitateOutlook = true;
+
+      # NOTE: https://davmail.sourceforge.net/serversetup.html
+      settings = {
+        "davmail.mode" = "O365Interactive";
+      };
+    };
+  };
+}
