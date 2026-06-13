61e8ca78e217c262f9726fd0ef22ac44e9f19000
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-06-12 23:08:20 -0700

    Add whitespace and code from nixpkgs to get the package to work on wayland

--- system/nixos/pkgs/birdtray/default.nix
+++ system/nixos/pkgs/birdtray/default.nix
@@ -17,16 +17,29 @@ stdenv.mkDerivation {
     rev = "7e35be6e3e59b252ded2eef32f7947b63b4028a9";
     hash = "sha256-RFK32dr2SGCXUsVRuqslLBsKqwwY1M86lGpgWUwUyec=";
   };
+
   buildInputs = with qt6; [
     qtbase
     qttools
   ];
+
   nativeBuildInputs = [
     cmake
     pkg-config
     qt6.wrapQtAppsHook
     wrapGAppsHook3
   ];
+
+  cmakeFlags = [
+    # CMake 4 dropped support of versions lower than 3.5,
+    # versions lower than 3.10 are deprecated.
+    (lib.cmakeFeature "CMAKE_POLICY_VERSION_MINIMUM" "3.10")
+  ];
+
+  # Wayland support is broken.
+  # https://github.com/gyunaev/birdtray/issues/113#issuecomment-621742315
+  qtWrapperArgs = [ "--set QT_QPA_PLATFORM xcb" ];
+
   meta = {
     description = "Mail system tray notification icon for Thunderbird";
     mainProgram = "birdtray";
