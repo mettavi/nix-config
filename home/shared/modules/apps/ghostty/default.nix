ec44f14294f089dc925e55c235d65dc648d0fa0d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-06-21 21:33:54 -0700

    Only enable a macOS option for ghostty on darwin

--- home/shared/modules/apps/ghostty/default.nix
+++ home/shared/modules/apps/ghostty/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
@@ -22,18 +23,24 @@ in
       # integration will work in more scenarios, such as switching shells
       enableZshIntegration = true;
       # settings will be written to $XDG_CONFIG_HOME/ghostty/config
-      settings = {
-        background-opacity = 0.8; # default is 1 (fully opaque)
-        # blur is not supported on gnome without the blur my shell extension
-        # background-blur = 20;
-        font-size = "14";
-        # make right_alt send an escape sequence in ghostty
-        macos-option-as-alt = true;
-        # start windows maximized
-        maximize = true;
-        shell-integration-features = "sudo";
-        theme = "iTerm2 Pastel Dark Background";
-      };
+      settings = mkMerge [
+        {
+          background-opacity = 0.8; # default is 1 (fully opaque)
+          # blur is not supported on gnome without the blur my shell extension
+          # background-blur = 20;
+          font-size = "14";
+          # make right_alt send an escape sequence in ghostty
+          # macos-option-as-alt = true;
+          # start windows maximized
+          maximize = true;
+          shell-integration-features = "sudo";
+          theme = "iTerm2 Pastel Dark Background";
+        }
+        (mkIf pkgs.stdenv.isDarwin {
+          # make right_alt send an escape sequence in ghostty
+          macos-option-as-alt = true;
+        })
+      ];
     };
     xdg.terminal-exec = {
       enable = true;
