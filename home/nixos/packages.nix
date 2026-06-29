cb10c25b6c84311538d0845e6cea520f796aa073
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-06-28 17:31:40 -0700

    Install Okular document viewer on nixos

--- home/nixos/packages.nix
+++ home/nixos/packages.nix
@@ -4,12 +4,13 @@
 }:
 {
   home.packages = with pkgs; [
+    kdePackages.okular # KDE document viewer
     mpv # General-purpose media player, fork of MPlayer and mplayer2
     pdfarranger # python-gtk application to merge/split/rotate/crop/rearrange PDFs
     picard # offical musicbrainz tagger
     variety # wallpaper manager
     karere # Gtk4 WhatsApp client
-    # wasistlos # no longer developed, replaced with karere 
+    # wasistlos # no longer developed, replaced with karere
     zoom-us # video conferencing application
   ];
 
