2ccc9f9f24231a7a9dd459c1f6740ed3fc71a049
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-07-11 11:19:58 -0700

    Remove the locally defined disko-btrfs module which is superfluous

--- hosts/blue/disko-config.nix
+++ hosts/blue/disko-config.nix
@@ -4,60 +4,127 @@
 with lib;
 {
   # NB: See https://wiki.archlinux.org/title/GPT_fdisk#Partition_type for a list of disk type codes
-  config = mkIf cfg.enable {
-    disko.devices.disk = {
-      sda1 = {
-        type = "disk";
-        device = "/dev/sda1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              label = "EFI";
-              name = "ESP";
-              size = "200M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/efi";
-                mountOptions = [
-                  "fmask=0022"
-                  "dmask=0022"
-                ];
-              };
+  # config = mkIf cfg.enable {
+  disko.devices.disk = {
+    sda1 = {
+      type = "disk";
+      device = "/dev/sda1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            label = "EFI";
+            name = "ESP";
+            size = "200M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/efi";
+              mountOptions = [
+                "fmask=0022"
+                "dmask=0022"
+              ];
             };
-            nixos = {
-              # end = "-32G";
-              content =
-                let
-                  cfg = config.mettavi.system.devices.disko-btrfs;
-                in
-                {
-                  # passwordFile = "/tmp/secret.key";
-                  # settings.allowDiscards = true;
-                  type = "btrfs";
-                  extraArgs = [ "-f" ]; # Override existing partition
-                  subvolumes = mapAttrs (
-                    subvol: subvolCfg:
-                    mkIf subvolCfg.enable {
-                      mountpoint = "${subvolCfg.mountpoint}";
-                      mountOptions = subvolCfg.mountOptions ++ cfg.commonMountOptions;
-                    }
-                  ) cfg.subvolumes;
+          };
+          nixos = {
+            # end = "-32G";
+            content = {
+              # passwordFile = "/tmp/secret.key";
+              # settings.allowDiscards = true;
+              type = "btrfs";
+              extraArgs = [ "-f" ]; # Override existing partition
+              subvolumes = {
+                "@root" = {
+                  enable = true;
+                  mountpoint = "/";
                 };
-            };
-            swap = {
-              size = "8G";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-                resumeDevice = true;
+                # A nested subvolume doesn't need a mountpoint as its parent is mounted
+                "@root/tmp" = {
+                  enable = true;
+                };
+                "@root/var/cache" = {
+                  enable = true;
+                };
+                "@nix" = {
+                  enable = true;
+                  mountpoint = "/nix";
+                };
+                "@roothome" = {
+                  enable = true;
+                  mountpoint = "/root";
+                };
+                "@roothome/.cache" = {
+                  enable = true;
+                };
+                "@vlcontainers" = {
+                  enable = true;
+                  mountpoint = "/var/lib/containers";
+                };
+                "@libvirtimgs" = {
+                  enable = true;
+                  mountpoint = "/var/lib/libvirt/images";
+                };
+                "@vlpostgres" = {
+                  enable = true;
+                  mountpoint = "/var/lib/postgresql";
+                };
+                "@varlog" = {
+                  enable = true;
+                  mountpoint = "/var/log";
+                };
+                "@vartmp" = {
+                  enable = true;
+                  mountpoint = "/var/tmp";
+                };
+                "@home" = {
+                  enable = true;
+                  mountpoint = "/home";
+                };
+                "@adminhome" = {
+                  enable = true;
+                  mountpoint = "/home/${username}";
+                };
+                "@adminhome/.cache" = {
+                  enable = true;
+                };
+                "@admincontainers" = {
+                  enable = true;
+                  mountpoint = "/home/${username}/.local/share/containers";
+                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
+                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
+                  ];
+                };
+                "@admindownloads" = {
+                  enable = true;
+                  mountpoint = "/home/${username}/Downloads";
+                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
+                    "x-gvfs-hide" # hide the subvolume from the Files (Nautilus) devices menu
+                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
+                  ];
+                };
+                "@adminmedia" = {
+                  enable = true;
+                  mountpoint = "/home/${username}/media";
+                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
+                    "x-gvfs-hide"
+                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
+                  ];
+                };
               };
+            };
+          };
+          swap = {
+            size = "8G";
+            content = {
+              type = "swap";
+              randomEncryption = true;
+              resumeDevice = true;
             };
           };
         };
       };
     };
   };
+  # };
 }
