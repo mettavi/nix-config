fa7cccdd9b49297f5480c70b79699a0a00e7a667
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-07-11 18:36:48 -0700

    Various updates to the disko config for host blue and the disko-btrfs system module

--- hosts/blue/disko-config.nix
+++ hosts/blue/disko-config.nix
@@ -1,10 +1,9 @@
 # NOTE: Disko is not supported on multi-boot systems.
 # It is best suited to remote servers or VMs where the whole disk can be dedicated to nixos.
-{ config, lib, ... }:
+{ lib, ... }:
 with lib;
 {
   # NB: See https://wiki.archlinux.org/title/GPT_fdisk#Partition_type for a list of disk type codes
-  # config = mkIf cfg.enable {
   disko.devices.disk = {
     sda1 = {
       type = "disk";
@@ -20,80 +19,29 @@ with lib;
             content = {
               type = "filesystem";
               format = "vfat";
-              mountpoint = "/efi";
+              mountpoint = "/boot";
               mountOptions = [
-                "fmask=0022"
-                "dmask=0022"
+                "umask=0077"
               ];
             };
           };
           nixos = {
             # end = "-32G";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ]; # Override existing partition
-              subvolumes = {
-                "@root" = {
-                  mountpoint = "/";
-                };
-                # A nested subvolume doesn't need a mountpoint as its parent is mounted
-                "@root/tmp" = {
-                };
-                "@root/var/cache" = {
-                };
-                "@nix" = {
-                  mountpoint = "/nix";
-                };
-                "@roothome" = {
-                  mountpoint = "/root";
-                };
-                "@roothome/.cache" = {
-                };
-                "@vlcontainers" = {
-                  mountpoint = "/var/lib/containers";
-                };
-                "@libvirtimgs" = {
-                  mountpoint = "/var/lib/libvirt/images";
-                };
-                "@vlpostgres" = {
-                  mountpoint = "/var/lib/postgresql";
-                };
-                "@varlog" = {
-                  mountpoint = "/var/log";
-                };
-                "@vartmp" = {
-                  mountpoint = "/var/tmp";
-                };
-                "@home" = {
-                  mountpoint = "/home";
-                };
-                "@adminhome" = {
-                  mountpoint = "/home/${username}";
-                };
-                "@adminhome/.cache" = {
-                };
-                "@admincontainers" = {
-                  mountpoint = "/home/${username}/.local/share/containers";
-                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
-                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
-                  ];
-                };
-                "@admindownloads" = {
-                  mountpoint = "/home/${username}/Downloads";
-                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
-                    "x-gvfs-hide" # hide the subvolume from the Files (Nautilus) devices menu
-                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
-                  ];
-                };
-                "@adminmedia" = {
-                  mountpoint = "/home/${username}/media";
-                  mountOptions = optionals config.mettavi.system.desktops.gnome.enable [
-                    "x-gvfs-hide"
-                    "x-gvfs-trash" # Enables trash functionality in Files (Nautilus) for the mounted filesystem
-                  ];
-                };
+            content =
+              let
+                cfg = config.mettavi.system.devices.disko-btrfs;
+              in
+              {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = mapAttrs (
+                  subvol: subvolCfg:
+                  mkIf subvolCfg.enable {
+                    mountpoint = "${subvolCfg.mountpoint}";
+                    mountOptions = subvolCfg.mountOptions ++ cfg.commonMountOptions;
+                  }
+                ) cfg.subvolumes;
               };
-            };
           };
           swap = {
             size = "8G";
@@ -107,5 +55,4 @@ with lib;
       };
     };
   };
-  # };
 }
