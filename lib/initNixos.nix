d3bc8f33a02c00fad3cf5adf96196fcf5d07183d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-07-17 00:30:36 -0700

    Refactor the initNixos module for use with other data types (such as checks)

--- lib/initNixos.nix
+++ lib/initNixos.nix
@@ -1,71 +1,88 @@
 { inputs, ... }:
-{
-  # Function for NixOS system configuration
-  mkNixosConfiguration =
-    hostname: system: username:
-    inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = {
-        inherit
-          hostname
-          inputs
-          username
-          ;
-      };
-      modules = [
-        inputs.disko.nixosModules.disko
-        ../hosts/${hostname}/configuration.nix
-        {
-          users.users.${username} = {
-            isNormalUser = true;
-            hashedPassword = "$y$j9T$AO6jdJHZ7Ep7tR2s/it0X/$1tSMbRECa1oOl.tJOeT46iKaCR9Cc0M6YkQliKZ1zsB";
-            home = "/home/${username}";
-            # allow user to configure networking and use sudo
-            extraGroups = [
-              "networkmanager"
-              "wheel"
+# Function for NixOS system configuration
+let
+  mkNixosModules = hostname: system: username: {
+    specialArgs = {
+      inherit
+        hostname
+        inputs
+        username
+        ;
+    };
+    modules = [
+      inputs.disko.nixosModules.disko
+      ../hosts/${hostname}/configuration.nix
+      {
+        users.users.${username} = {
+          isNormalUser = true;
+          home = "/home/${username}";
+          # allow user to configure networking and use sudo
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+        };
+        networking.hostName = "${hostname}";
+        nix = {
+          extraOptions = ''
+            warn-dirty = false
+          '';
+          settings = {
+            # enable flakes
+            experimental-features = [
+              "nix-command"
+              "flakes"
             ];
           };
-          networking.hostName = "${hostname}";
-          nix = {
-            extraOptions = ''
-              warn-dirty = false
-            '';
-            settings = {
-              # enable flakes
-              experimental-features = [
-                "nix-command"
-                "flakes"
-              ];
-            };
+        };
+        programs = {
+          git.enable = true;
+          vim.enable = true;
+        };
+        # Enable the OpenSSH daemon.
+        services = {
+          openssh = {
+            enable = true;
+            # create a host key
+            hostKeys = [
+              {
+                comment = "root@${hostname}";
+                path = "/etc/ssh/ssh_${hostname}_ed25519_key";
+                rounds = 100;
+                type = "ed25519";
+              }
+            ];
           };
-          nixpkgs = {
-            # Allow unfree packages
-            config.allowUnfree = true;
-            hostPlatform = "${system}";
-          };
-          programs = {
-            git.enable = true;
-            vim.enable = true;
-          };
-          # Enable the OpenSSH daemon.
-          services = {
-            openssh = {
-              enable = true;
-              # create a host key
-              hostKeys = [
-                {
-                  comment = "root@${hostname}";
-                  path = "/etc/ssh/ssh_${hostname}_ed25519_key";
-                  rounds = 100;
-                  type = "ed25519";
-                }
-              ];
-            };
-          };
-          # The Git revision of the top-level flake from which this configuration was built
-          system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-        }
-      ];
+        };
+        # The Git revision of the top-level flake from which this configuration was built
+        system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+      }
+    ];
+    # only valid when nixosSystem builds pkgs itself from scratch --
+    # pkgs.testers.runNixOSTest supplies a pre-built, read-only pkgs, so
+    # this must NOT be included in that path
+    standaloneModules = [
+      {
+        nixpkgs = {
+          config.allowUnfree = true;
+          hostPlatform = system;
+        };
+      }
+    ];
+  };
+in
+{
+  inherit mkNixosModules;
+
+  # Function for NixOS system configuration
+  mkNixosConfiguration =
+    hostname: system: username: nixinput:
+    let
+      built = mkNixosModules hostname system username;
+    in
+    inputs.${nixinput}.lib.nixosSystem {
+      inherit system;
+      inherit (built) specialArgs;
+      modules = built.modules ++ built.standaloneModules;
     };
 }
