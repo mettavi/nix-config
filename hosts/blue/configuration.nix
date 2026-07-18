908adfd3bd94a9596c1faec93f614466a964b034
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-07-17 20:51:12 -0700

    Move VPS specific config from the host to the VPS module

--- hosts/blue/configuration.nix
+++ hosts/blue/configuration.nix
@@ -46,14 +46,6 @@
     priority = 100;
   };
 
-  # Use systemd for the bootloader
-  boot.loader = {
-    # the installation process is allowed to modify EFI boot variables
-    efi.canTouchEfiVariables = true;
-    # enable the systemd-boot EFI boot manager
-    systemd-boot.enable = true;
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- system/shared/modules/profiles/vps/default.nix
+++ system/shared/modules/profiles/vps/default.nix
@@ -15,9 +15,39 @@ in
       default = false;
       description = "A profile for a virtual private/cloud server";
     };
+    useDHCP = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether the use DHCP on the VPS host";
+    };
+    netInterface = mkOption {
+      type = types.str;
+      description = "The name of the main network interface on the system";
+    };
   };
 
   config = mkIf cfg.enable {
+    boot.kernelParams = [
+      "console=tty1"
+      "console=ttyS0,115200"
+    ];
+
+    # set up a hyrbid boot, with BIOS and a UEFI fallback
+    # many VPS VMs default to a legacy BIOS mode
+    boot.loader = {
+      grub = {
+        # grub works on both BIOS and UEFI
+        enable = true;
+        device = "/dev/sda"; # Install GRUB to the MBR
+        efiSupport = true; # Support UEFI just in case
+        # Crucial for VPS: Installs bootloader to a fallback path
+        # so it boots even if NVRAM variables are reset by the host.
+        efiInstallAsRemovable = true;
+      };
+      # systemd-boot does not support BIOS
+      systemd-boot.enable = false;
+    };
+
     services.openssh =
       if pkgs.stdenv.isLinux then
         {
@@ -33,5 +63,41 @@ in
             PasswordAuthentication no
           '';
         };
+
+    # Many VPS services use static networking
+    networking.useDHCP = cfg.useDHCP;
+
+    # Explicitly configure your network interface
+    networking.interfaces.ens18 = {
+      useDHCP = cfg.useDHCP;
+      ipv4.addresses = [
+        {
+          address = "187.127.105.244";
+          prefixLength = 24; # Verify in Hostinger panel (usually 24 or 32)
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2a02:4780:5e:616::1";
+          prefixLength = 48; # Verify in Hostinger panel
+        }
+      ];
+    };
+
+    # Set your routing (FIND THESE IN THE HOSTINGER PANEL)
+    networking.defaultGateway = "187.127.105.254";
+    networking.defaultGateway6 = {
+      address = "2a02:4780:5e::1";
+      interface = "ens18";
+    };
+
+    # Set DNS resolvers
+    networking.nameservers = [
+      "8.8.8.8"
+      "1.1.1.1"
+    ];
+
+    systemd.tmpfiles.rules = [ "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware" ];
+
   };
 }
