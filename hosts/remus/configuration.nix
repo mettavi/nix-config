8934d41ecb430bdca67f683041e28dc45e9e076c
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-04 19:03:33 +0530

    Add initial config for new VPS host remus

---
+++ hosts/remus/configuration.nix
@@ -0,0 +1,92 @@
+{
+  modulesPath,
+  username,
+  ...
+}:
+{
+  imports = [
+    # imports for initial install with nixos-anywhere and disko
+    # subsequently the disko config will configure fstab
+    # NB: hardware-configuration.nix is auto-generated during install and is imported in the mkNixos function
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disko-config.nix
+    # ./hardware-configuration.nix
+  ];
+
+  # install the ghostty terminfo (which is missing from current versions of terminfo) for ssh sessions to this host
+  # environment.systemPackages = with pkgs; [ ghostty.terminfo ];
+
+  ########## IMPORTANT SETTINGS ###########
+
+  # SETUP INITIAL ACCESS
+  # set up root access to prevent initial password problems for the primary user
+  users.users.root = {
+    # hashedPassword = "$y$j9T$kD6Saa3hxt3U5YCkHGtBp/$ybiLXiDq1f6EitKgQTPB8Nyw2BzvI2QOI0IlR.TKyf1";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLefqc5FD0nZQLMUF6xfUTSZItumpd7AWPe0MP2JzoI timotheos@oona"
+    ];
+  };
+
+  users.users.${username} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILLefqc5FD0nZQLMUF6xfUTSZItumpd7AWPe0MP2JzoI timotheos@oona"
+    ];
+  };
+
+  ########## SYSTEM ARCHITECTURE ###########
+
+  # enable in-memory compressed devices and swap space
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    priority = 100;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion).
+  system.stateVersion = "26.05";
+
+  # SYSTEM MODULES SETTINGS
+  mettavi = {
+    system = {
+      devices = {
+        # create and mount btrfs subvolumes using disko
+        disko-btrfs.enable = true;
+      };
+      profiles = {
+        # hostinger vps (eg. ssh hardening)
+        vps = {
+          enable = true;
+          netInterface = "ens18";
+          ip4 = {
+            addr = "187.127.105.244";
+            prefix = 24;
+            gateway = "187.127.105.254";
+          };
+          ip6 = {
+            addr = "2a02:4780:5e:616::1";
+            prefix = 48;
+            gateway = "2a02:4780:5e::1";
+          };
+        };
+      };
+      # enable system users
+      userConfig = {
+        timotheos.enable = true;
+      };
+    };
+  };
+
+  # (HOST-SPECIFIC) HOME-MANAGER SETTINGS
+  home-manager.users.${username} = {
+    home = {
+      # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+      stateVersion = "26.05";
+    };
+  };
+}
---
+++ hosts/remus/disko-config.nix
@@ -0,0 +1,112 @@
+# NOTE: Disko is not supported on multi-boot systems.
+# It is best suited to remote servers or VMs where the whole disk can be dedicated to nixos.
+{
+  config,
+  hostname,
+  lib,
+  ...
+}:
+with lib;
+{
+  # select which btrfs subvols to exclude from the defaults set in the disko-btrfs module
+  mettavi.system.devices.disko-btrfs.subvolumes =
+    let
+      exclVols = [
+        "@adminmedia"
+        "@libvirtimgs"
+      ];
+    in
+    # {
+    builtins.listToAttrs (
+      map (vol: {
+        name = vol;
+        value = {
+          enable = false;
+        };
+      }) exclVols
+    );
+  # "@adminmedia".enable = false;
+  # not currently running VMs on host remus
+  # "@libvirtimgs".enable = false;
+  # };
+
+  disko.devices.disk = {
+    ${hostname} = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          # add a BIOS boot partition for grub, required on a GPT system without UEFI
+          biosBoot = {
+            size = "1M";
+            type = "EF02"; # Type code for BIOS boot partition
+            priority = 0;
+          };
+          ESP = {
+            # Priority of the partition, smaller values are created first
+            # Attempt to boot from this partition first
+            priority = 1;
+            # The UUID (also known as GUID) of the PARTITION. Note that this is distinct from the UUID of the filesystem.
+            # See /dev/disk/by-partuuid/. You can generate a UUID with the command `uuidgen -r`.
+            # NB: The PARTLABEL is generated with "disk-<disk-attr>-<part-attr>"
+            # (eg. "disk-<hostname>-ESP"), unless it is set directly with "label ="
+            uuid = "6feb6d11-47b2-400e-927f-a6c4e1089101";
+            size = "1G";
+            # NB: See https://wiki.archlinux.org/title/GPT_fdisk#Partition_type
+            # for a list of disk type codes
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "umask=0077"
+              ];
+            };
+          };
+          nixos = {
+            size = "100%";
+            uuid = "d679c570-aded-494f-8090-b3c465ddaad8";
+            content =
+              let
+                cfg = config.mettavi.system.devices.disko-btrfs;
+              in
+              {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # A path to mount the BTRFS filesystem to
+                mountpoint = "/";
+                # A list of options to pass to mount
+                mountOptions = [ "defaults" ];
+                # Subvolumes to define for BTRFS
+                subvolumes =
+                  (mapAttrs (
+                    subvol: subvolCfg:
+                    lib.optionalAttrs (subvolCfg.mountpoint != null) {
+                      mountpoint = subvolCfg.mountpoint;
+                      mountOptions = subvolCfg.mountOptions ++ cfg.commonMountOptions;
+                    }
+                  ) (lib.filterAttrs (subvol: subvolCfg: subvolCfg.enable) cfg.subvolumes))
+                  // {
+                    # Subvolume for the swapfile
+                    "@swap" = {
+                      mountpoint = cfg.subvolumes."@swap".mountpoint;
+                      mountOptions = [
+                        "defaults"
+                        "noatime"
+                      ];
+                      swap = {
+                        swapfile = {
+                          size = "4G";
+                        };
+                      };
+                    };
+                  };
+              };
+          };
+        };
+      };
+    };
+  };
+}
