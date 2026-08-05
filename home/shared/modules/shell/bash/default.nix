7161ead205ce589ab1cf42bc2e92e3b68be2e04a
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-05 22:36:29 +0530

    Add a prompt option to the bash module

--- home/shared/modules/shell/bash/default.nix
+++ home/shared/modules/shell/bash/default.nix
@@ -10,6 +10,15 @@ in
 {
   options.mettavi.shell.bash = {
     enable = mkEnableOption "bash configuration";
+    prompt = mkOption {
+      type = types.enum [
+        "manual"
+        "oh-my-posh"
+        "p10k"
+      ];
+      default = "manual";
+      description = "Set the shell prompt";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -17,6 +26,11 @@ in
       enable = true;
       enableCompletion = true;
       historyFile = "${config.xdg.configHome}/bash/.bash_history";
+    };
+    programs.oh-my-posh = mkIf (cfg.prompt == "oh-my-posh") {
+      enable = true;
+      configFile = ../../../dots/oh-my-posh/gruvbox.json;
+      enableBashIntegration = true;
     };
   };
 }
