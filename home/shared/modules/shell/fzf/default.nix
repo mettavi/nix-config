0b4facbfddcaa13072be2b563124651d3cd16f2d
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-15 19:59:13 +0530

    Remove the shell code for fzf, which is the same as the defaults

--- home/shared/modules/shell/fzf/default.nix
+++ home/shared/modules/shell/fzf/default.nix
@@ -84,35 +84,6 @@ in
           "Aloxaf/fzf-tab"
         ];
       };
-      completionInit = # sh
-        ''
-          # Configure fzf ** completion
-          # - The first argument to the function ($1) is the base path to start traversal
-          # - See the source code (completion.{bash,zsh}) for the details.
-          _fzf_compgen_path() {
-            fd --hidden --follow --exclude .git . "$1"
-          }
-
-          # Use fd to generate the list for directory completion
-          _fzf_compgen_dir() {
-            fd --type=d --hidden --follow --exclude .git . "$1"
-          }
-
-          # Advanced customization of fzf options via _fzf_comprun function
-          # - The first argument to the function is the name of the command.
-          # - You should make sure to pass the rest of the arguments to fzf.
-          _fzf_comprun() {
-            local command=$1
-            shift
-
-            case "$command" in
-              cd)           fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
-              export|unset) fzf --preview "eval 'echo ''${}'"         "$@" ;;
-              ssh)          fzf --preview 'dig {}'                   "$@" ;;
-              *)            fzf --preview "$show_file_or_dir_preview" "$@" ;;
-            esac
-          }
-        '';
       # source the fzf-git shell script
       initContent = (builtins.readFile "${pkgs.fzf-git-sh}/share/fzf-git-sh/fzf-git.sh");
     };
