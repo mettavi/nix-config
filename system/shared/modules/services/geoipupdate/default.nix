2440b8aea65b185031b2ca94c0ce70d704ae1025
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-18 15:26:18 +0530

    Add new shared system module for the GeoIPUpdate service using the MaxMind db

---
+++ system/shared/modules/services/geoipupdate/default.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  hostname,
+  lib,
+  secrets_path,
+  username,
+  ...
+}:
+
+let
+  cfg = config.mettavi.system.services.geoipupdate;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    ;
+in
+{
+  options.mettavi.system.services.geoipupdate = {
+    enable = mkEnableOption "Set up geoipupdate using the Maxmind database";
+  };
+
+  config = mkIf cfg.enable {
+
+    services = {
+      geoipupdate = {
+        enable = true;
+        settings = {
+          AccountID = 1395964;
+          EditionIDs = [
+            "GeoLite2-ASN"
+            "GeoLite2-City"
+            "GeoLite2-Country"
+          ];
+          LicenseKey = config.sops.secrets."users/${username}/maxmind-license-${hostname}.key".path;
+        };
+      };
+
+      sops.secrets = {
+        "users/${username}/maxmind-license-${hostname}.key" = {
+          sopsFile = "${secrets_path}/secrets/hosts/${hostname}.yaml";
+        };
+      };
+
+    };
+  };
+}
