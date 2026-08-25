b0e048c1a89c14c93bd16f4995fe9040a75a3bf3
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-25 21:51:07 +0530

    Add a newt blueprint config for a private vaultwarden service on host blue

--- system/shared/modules/services/newt/default.nix
+++ system/shared/modules/services/newt/default.nix
@@ -27,22 +27,43 @@ in
     services.newt = {
       enable = true;
       # See https://docs.pangolin.net/manage/blueprints
-      # blueprint = {
-      #   "public-resources" = {
-      #     vaultwarden = {
-      #       name = "Vaultwarden";
-      #       mode = "http";
-      #       "full-domain" = "vault.mettavi.cloud";
-      #       targets = [
-      #         {
-      #           hostname = "127.0.0.1";
-      #           port = config.services.vaultwarden.config.ROCKET_PORT;
-      #           method = "http";
-      #         }
-      #       ];
-      #     };
-      #   };
-      # };
+      blueprint = {
+        # public-resources = {
+        #   example = {
+        #     name = "Vaultwarden";
+        #     mode = "http";
+        #     "full-domain" = "vault.mettavi.cloud";
+        #     targets = [
+        #       {
+        #         hostname = "127.0.0.1";
+        #         port = config.services.vaultwarden.config.ROCKET_PORT;
+        #         method = "http";
+        #       }
+        #     ];
+        #   };
+        # };
+        private-resources = {
+          vaultwarden = {
+            enabled = true;
+            name = "Vaultwarden";
+            destination = "127.0.0.1";
+            destination-port = 8222;
+            full-domain = "vault.mettavi.cloud";
+            # the admin role is not valid here
+            roles = [ "Member" ];
+            mode = "http";
+            scheme = "https";
+            sites = [ "Services" ];
+            ssl = true;
+          };
+        };
+        sites = {
+          "Services" = {
+            name = "Services";
+            docker-socket-enabled = true;
+          };
+        };
+      };
 
       # NEWT_ID and NEWT_SECRET live here, never in settings/CLI args or the Nix store
       environmentFile = config.sops.secrets."users/${username}/newt.env".path;
