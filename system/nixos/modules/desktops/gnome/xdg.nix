dee7b62eaa22335b4a0060fb21c9a52f0c640297
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-08-26 21:21:16 +0530

    Only enable this sub-module if the parent module is also enabled

--- system/nixos/modules/desktops/gnome/xdg.nix
+++ system/nixos/modules/desktops/gnome/xdg.nix
@@ -1,36 +1,43 @@
 {
+  config,
   lib,
   pkgs,
   ...
 }:
+with lib;
+let
+  cfg = config.mettavi.system.desktops.gnome;
+in
 {
-  # set gtk as the default interface
-  xdg.portal = {
-    enable = true;
-    config = lib.mkForce {
-      common = {
-        default = [
-          "gtk"
-        ];
+  config = mkIf cfg.enable {
+    # set gtk as the default interface
+    xdg.portal = {
+      enable = true;
+      config = lib.mkForce {
+        common = {
+          default = [
+            "gtk"
+          ];
+        };
+        gnome = {
+          default = [
+            "gnome"
+            "gtk"
+          ];
+          "org.freedesktop.impl.portal.Secret" = [
+            "gnome-keyring"
+          ];
+        };
       };
-      gnome = {
-        default = [
-          "gnome"
-          "gtk"
+      extraPortals =
+        with pkgs;
+        # required for screensharing functionality on Wayland
+        [
+          xdg-desktop-portal-gnome
+          xdg-desktop-portal-gtk # Use this for GNOME (may already be default)
+          # xdg-desktop-portal-wlr # Use this for Sway/wlroots
+          # xdg-desktop-portal-kde  # Use this for KDE
         ];
-        "org.freedesktop.impl.portal.Secret" = [
-          "gnome-keyring"
-        ];
-      };
     };
-    extraPortals =
-      with pkgs;
-      # required for screensharing functionality on Wayland
-      [
-        xdg-desktop-portal-gnome
-        xdg-desktop-portal-gtk # Use this for GNOME (may already be default)
-        # xdg-desktop-portal-wlr # Use this for Sway/wlroots
-        # xdg-desktop-portal-kde  # Use this for KDE
-      ];
   };
 }
