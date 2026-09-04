1bb3e6859f15ce840f8da238564ea4f8926eb282
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-09-05 01:21:56 +0530

    Load the new local package from a default.nix file

--- system/nixos/pkgs/default.nix
+++ system/nixos/pkgs/default.nix
@@ -3,4 +3,5 @@
   tipitaka_pali_reader = (pkgs.callPackage ./tipitaka-reader { });
   birdtray = (pkgs.callPackage ./birdtray { });
   pia-wg-config = (pkgs.callPackage ./pia-wg-conf { });
+  pia-wg-config2 = (pkgs.callPackage ./pia-wg-conf2 { });
 }
