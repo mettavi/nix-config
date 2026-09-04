b3bd01cc1ffd827a999b136ccfd7daa6903d28c6
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-09-04 22:29:00 +0530

    Update the attribute name to be consisent with the package name

--- system/nixos/pkgs/default.nix
+++ system/nixos/pkgs/default.nix
@@ -2,5 +2,5 @@
 {
   tipitaka_pali_reader = (pkgs.callPackage ./tipitaka-reader { });
   birdtray = (pkgs.callPackage ./birdtray { });
-  pia-wg-conf = (pkgs.callPackage ./pia-wg-conf { });
+  pia-wg-config = (pkgs.callPackage ./pia-wg-conf { });
 }
