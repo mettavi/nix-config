1d649a893e6a6378459dad2d2d1ecdf26cfa1783
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-09-05 20:05:52 +0530

    Update comment

--- system/nixos/modules/services/gluetun/default.nix
+++ system/nixos/modules/services/gluetun/default.nix
@@ -302,7 +302,7 @@ in
               # pia-wg-refresh writes to SERVER_NAMES (bind-mounted read-write) whenever the port/server changes
               ON_PORT_CHANGE_SCRIPT = "/hooks/update-server-name.sh";
               # tradeoff: will reliably work when the server changes but with the SAME PORT
-              # (as with above var) but will cause two restarts in a row on any given regen cycle
+              # (problematic with ON_PORT_CHANGE_SCRIPT) but will cause two restarts in a row on any given regen cycle
               ON_RECOVERY_SCRIPT = "/hooks/update-server-name.sh";
               PIA_PORT_FORWARDING = "true";
               LOG_LEVEL = "info";
