81038b8f54d407e010dfadde7b8f290e8e918351
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-09-10 01:43:30 +0530

    Add weblink to PR for fixed bug for calibre dependency

--- system/shared/modules/apps/calibre/default.nix
+++ system/shared/modules/apps/calibre/default.nix
@@ -54,6 +54,7 @@ in
         home.packages = with pkgs; [
           # TODO: The override is not working as at 2/9/2026
           # See https://github.com/NixOS/nixpkgs/issues/559101 for more details
+          # NB: Fixed in https://github.com/NixOS/nixpkgs/pull/558309 on 10-09-26
 
           # Comprehensive e-book software
           # (calibre.override {
