0764c18f76705eaca3870c0b712db698454ad0b1
Mettavihari <1978429+mettavi@users.noreply.github.com>
2026-09-19 21:47:33 +0530

    Add documentation

--- system/shared/modules/apps/obsidian/default.nix
+++ system/shared/modules/apps/obsidian/default.nix
@@ -212,6 +212,8 @@ in
         vaults = {
           "Documents/VaultsTest/AABCAP 2025-26" = {
             enable = false;
+            # Settings for any attribute here will replace ALL those set through the GUI;
+            # omit them here to use the hybrid pinnedSettings (see above) instead
             settings = {
               app = {
                 attachmentFolderPath = "Utilities/Assets/";
